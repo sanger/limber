c3b2620da44ba171ccd7d038ab14d85b254311f5
Sean Dunn <sd9@sanger.ac.uk>
2011-05-26 14:35:08 +0100

    Remove static index page Replaced references to PulldownUi with PulldownPipeline

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,4 @@
-PulldownUi::Application.configure do
+PulldownPipeline::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # In the development environment your application's code is reloaded on
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-PulldownUi::Application.routes.draw do
+PulldownPipeline::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +48,7 @@ PulldownUi::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "plate#index"
 
   # See how all your routes lay out with "rake routes"
 
