87150f839d0948da8ba846a977b9ec9db3a2167b
Sean Dunn <sd9@sanger.ac.uk>
2011-06-06 17:31:04 +0100

    Add printing dummy

--- app/controllers/plates_controller.rb
+++ app/controllers/plates_controller.rb
@@ -1,9 +1,7 @@
 require "ostruct"
 class PlatesController < ApplicationController
-  before_filter :assign_api
-  before_filter :get_printers_and_lables, :on => [ :show, :update ]
   
-  attr_accessor :api
+  before_filter :get_printers_and_lables, :on => [ :show, :update ]
   
   def search
     redirect_to plate_path(:id => params[:plate_barcode]) unless params[:plate_barcode].blank?
@@ -11,7 +9,7 @@ class PlatesController < ApplicationController
   end
 
   def show
-    @plate         = api.search.find(Settings.asset_from_barcode).first(:barcode => params[:id])
+    @plate = api.search.find(Settings.asset_from_barcode).first(:barcode => params[:id])
     
     respond_to do |format|
       format.html
@@ -20,11 +18,7 @@ class PlatesController < ApplicationController
 
   def update
     @plate = api.search.find(Settings.asset_from_barcode).first(:barcode => params[:id])
-    
-    # FIXME The key name here for derefencing the params is going to
-    # change when the API stub is removed.
-    
-    # The state changes will need to be done via an api.state_change.create! call.
+
     @plate.update_attributes!(params[:sequencescape_plate])
     
     respond_to do |format|
@@ -34,14 +28,11 @@ class PlatesController < ApplicationController
 
 
   # Private Stuff...
-  def assign_api
-   self.api ||= ::Sequencescape::Api.new
-  end
-  private :assign_api
   
   def get_printers_and_lables
     # This needs to be done properly through the barcode printing API...
     @barcode_label = Sequencescape::BarcodeLabel.new({:printer => :BARCODE_PRINTER_NOT_SET})
     @printers      = { "H104_bd" => :h104_bd, "G206_bc" => :g206_bc }
   end
+  private :get_printers_and_lables
 end
