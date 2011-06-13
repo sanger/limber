efb83e72348814745928722dcb9b9182354b692c
Sean Dunn <sd9@sanger.ac.uk>
2011-06-13 11:22:33 +0100

    Added #value routing to set the currently shown plate tab.

--- app/controllers/creation_controller.rb
+++ app/controllers/creation_controller.rb
@@ -1,7 +1,7 @@
 class CreationController < ApplicationController
 
   def new
-    
+
     # This might need to use an internal uuid to class name lookup
     # for security as well as readability.
     @creation_form  = Forms.const_get(params[:plate_purpose_uuid]).new(
@@ -9,15 +9,15 @@ class CreationController < ApplicationController
       :parent_uuid        => params[:plate_id],
       :plate_purpose_uuid => params[:plate_purpose_uuid]
     )
-    
+
     respond_to do |format|
       format.html { render @creation_form.class.const_get(:PARTIAL) }
     end
   end
-  
+
   def create
     @creation_form = Forms.const_get(params[:plate][:plate_purpose_uuid]).new(params[:plate].merge(:api => api))
-    
+
     if @creation_form.save
       respond_to do |format|
         format.html { redirect_to plate_path(@creation_form.child.barcode) }
