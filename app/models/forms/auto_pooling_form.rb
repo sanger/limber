dc2a14625885522896610b459e0e3a5b89101437
Sean Dunn <sd9@sanger.ac.uk>
2011-06-22 17:21:54 +0100

    Added auto_pooling form

---
+++ app/models/forms/auto_pooling_form.rb
@@ -0,0 +1,27 @@
+module Forms
+  class AutoPoolingPlate < CreationForm
+    PARTIAL    = 'robot'
+    ATTRIBUTES = [:api, :plate_purpose_uuid, :parent_uuid, :transfer_template_uuid]
+
+    attr_accessor *ATTRIBUTES
+    attr_reader :plate_creation
+
+    def initialize(attributes = {})
+      ATTRIBUTES.each do |attribute|
+        send("#{attribute}=", attributes[attribute])
+      end
+    end
+
+    validates_presence_of *ATTRIBUTES
+    def transfer_template_uuids
+      # TODO this should do a look up on the pooling by submission uuid
+      # locally and the a direct find
+      @transfer_template_uuids ||= api.transfer_template.all.select { |transfer| transfer.name.match(/Pool/) }
+    end
+
+    def create_objects!
+      create_plate!(transfer_template_uuid)
+    end
+    private :create_objects!
+  end
+end
