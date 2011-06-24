5409eb1ac046410646f02e3fe18ef5afbe38a1ff
Sean Dunn <sd9@sanger.ac.uk>
2011-06-24 20:25:47 +0100

    Added bait previews

--- app/models/forms/baiting_form.rb
+++ app/models/forms/baiting_form.rb
@@ -1,5 +1,42 @@
 module Forms
   class BaitingForm < CreationForm
     write_inheritable_attribute :page, "baiting"
+    class_inheritable_reader :aliquot_partial
+    write_inheritable_attribute :aliquot_partial, "plates/baited_aliquot"
+
+    def bait_library_layout_preview
+      @bait_library_layout_preview ||= api.bait_library_layout.preview!(:plate => parent_uuid).layout
+    end
+
+    def create_objects!
+      create_plate! do |plate|
+        api.bait_library_layout.create!(:plate => plate.uuid)
+      end
+    end
+
+    def wells
+      wells = bait_library_layout_preview.sort.map do |well|
+        Hashie::Mash.new(
+          :location => well[0],
+          :bait     => well[1],
+          :aliquots => [:an_aliquot]
+        )
+      end
+
+      wells
+
+    end
+
+    def wells_by_row
+      @wells_by_row ||= wells.inject(Hash.new {|h,k| h[k]=[]}) do |h,well|
+        h[well.location.sub(/\d+/,'')] << well; h
+      end.sort
+
+      @wells_by_row.each do |row|
+        row.last.sort! { |a,b| a.location.sub(/\D+/,'').to_i <=> b.location.sub(/\D+/,'').to_i }
+      end
+
+      @wells_by_row
+    end
   end
 end
