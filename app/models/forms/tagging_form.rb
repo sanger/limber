f3ead6f800fdaaabf28c85876068f4a52f1fe555
Sean Dunn <sd9@sanger.ac.uk>
2011-07-19 16:58:01 +0100

    Changed TaggingForm#tag_layout_templates to use pooling size

--- app/models/forms/tagging_form.rb
+++ app/models/forms/tagging_form.rb
@@ -8,7 +8,12 @@ module Forms
     validates_presence_of *self.attributes
 
     def tag_layout_templates
-      @tag_layout_templates ||= api.tag_layout_template.all
+      return @tag_layout_template if @tag_layout_template.present?
+      maximum_pool_size = plate.pools.map(&:last).map(&:size).max
+
+      @tag_layout_templates = api.tag_layout_template.all.select do |template|
+         template.tag_group.tags.size >= maximum_pool_size
+      end
     end
 
     def create_objects!
