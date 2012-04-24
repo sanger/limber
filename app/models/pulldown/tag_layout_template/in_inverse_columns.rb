449b5b1ba67000055785d51fe6add2b3f9da0784
Matthew Denner <md12@sanger.ac.uk>
2012-04-24 13:05:29 +0100

    [fixes #28351733] Needed inverse walking for tagging

---
+++ app/models/pulldown/tag_layout_template/in_inverse_columns.rb
@@ -0,0 +1,12 @@
+module Pulldown::TagLayoutTemplate::InInverseColumns
+  def group_wells_of_plate(plate)
+    group_wells(plate) do |well_location_pool_pair|
+      (1..12).to_a.reverse.map do |column|
+        ('A'..'H').to_a.reverse.map do |row|
+          well_location_pool_pair.call(row, column)
+        end
+      end
+    end
+  end
+  private :group_wells_of_plate
+end
---
+++ app/models/pulldown/tag_layout_template/in_inverse_rows.rb
@@ -0,0 +1,11 @@
+module Pulldown::TagLayoutTemplate::InInverseRows
+  def group_wells_of_plate(plate)
+    group_wells(plate) do |well_location_pool_pair|
+      ('A'..'H').to_a.reverse.map do |row|
+        (1..12).to_a.reverse.map do |column|
+          well_location_pool_pair.call(row, column)
+        end
+      end
+    end
+  end
+end
