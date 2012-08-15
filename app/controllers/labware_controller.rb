5e991450fa676fa9362fd8bd5b638ece4210fda4
Sean Dunn <sd9@sanger.ac.uk>
2012-08-15 10:41:17 +0100

    Added login check for state changes

--- app/controllers/labware_controller.rb
+++ app/controllers/labware_controller.rb
@@ -1,6 +1,7 @@
 class LabwareController < ApplicationController
   before_filter :locate_labware, :only => [ :show ]
-  before_filter :get_printers, :only => [ :show ]
+  before_filter :get_printers, :only   => [ :show ]
+  before_filter :check_for_current_user!, :only => [ :update ]
 
   def locate_labware
      @labware = locate_labware_identified_by(params[:id])
@@ -12,9 +13,18 @@ class LabwareController < ApplicationController
   end
   private :get_printers
 
+  def check_for_current_user!
+    redirect_to(
+      search_path,
+      :alert => "Please login to change the state of any labware."
+    ) unless current_user_uuid.present?
+  end
+  private :check_for_current_user!
+
   def state_changer_for(purpose_uuid, labware_uuid)
     StateChangers.lookup_for(purpose_uuid).new(api, labware_uuid, current_user_uuid)
   end
+  private :state_changer_for
 
   def show
     @presenter = presenter_for(@labware)
@@ -36,4 +46,5 @@ class LabwareController < ApplicationController
       }
     end
   end
+
 end
