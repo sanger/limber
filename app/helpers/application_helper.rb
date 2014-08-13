1fff2f3fd5742243b2e79567def3fe30b24ea948
James Glover <james.glover@sanger.ac.uk>
2014-08-13 13:56:13 +0100

    [fixes #75419046] Non production environments are purple

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+  def environment
+    Rails.env
+  end
+
+  def non_production_class
+    Rails.env != 'production' ? 'nonproduction' : ''
+  end
+
+  def custom_theme
+    yield 'nonproduction' unless Rails.env == 'production'
+  end
 end
