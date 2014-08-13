7916b4d6dac9657721c81bea814424639c1678b4
James Glover <james.glover@sanger.ac.uk>
2014-08-13 14:04:49 +0100

    [fixes #75419046] Add version string to landing page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,24 @@
 module ApplicationHelper
 
+  module DeploymentInfo
+
+    begin
+      require './lib/deployed_version'
+    rescue LoadError
+        module Deployed
+          VERSION_ID = 'LOCAL'
+          VERSION_STRING = "Illumina Pipeline App LOCAL [#{ENV['RACK_ENV']}]"
+        end
+    end
+
+    def version_information
+      # Provides a quick means of checking the deployed version
+      Deployed::VERSION_STRING
+    end
+  end
+  include DeploymentInfo
+
+
   def environment
     Rails.env
   end
