ab19eacd5b0c2f3fb41423ad911596308e6cb41c
James Glover <james.glover@sanger.ac.uk>
2015-01-23 12:46:32 +0000

    Update tab order on qcable plates

--- app/models/presenters/qc_completable_presenter.rb
+++ app/models/presenters/qc_completable_presenter.rb
@@ -3,9 +3,9 @@ module Presenters
     include Presenters::Statemachine::QcCompletable
 
     write_inheritable_attribute :authenticated_tab_states, {
-      :pending     => [ 'labware-summary-button' ],
-      :started     => [ 'labware-summary-button' ],
-      :passed      => [ 'labware-summary-button', 'labware-state-button', 'well-failing-button', 'labware-creation-button' ],
+      :pending     => [ 'labware-summary-button', 'labware-state-button'   ],
+      :started     => [ 'labware-state-button',   'labware-summary-button' ],
+      :passed      => [ 'labware-creation-button', 'labware-summary-button', 'labware-state-button', 'well-failing-button' ],
       :qc_complete => [ 'labware-summary-button', 'labware-state-button', 'labware-creation-button' ],
       :cancelled   => [ 'labware-summary-button' ],
       :failed      => [ 'labware-summary-button' ]
