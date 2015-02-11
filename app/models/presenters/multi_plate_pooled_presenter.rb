685a9daf4550a911959557d9f0fa910d8a19905e
James Glover <james.glover@sanger.ac.uk>
2015-02-11 12:19:27 +0000

    Apply copyright message to all files

--- app/models/presenters/multi_plate_pooled_presenter.rb
+++ app/models/presenters/multi_plate_pooled_presenter.rb
@@ -1,3 +1,6 @@
+#This file is part of Illumina-B Pipeline is distributed under the terms of GNU General Public License version 3 or later;
+#Please refer to the LICENSE and README files for information on licensing and authorship of this file.
+#Copyright (C) 2015 Genome Research Ltd.
 class Presenters::MultiPlatePooledPresenter < Presenters::PooledPresenter
   write_inheritable_attribute :summary_partial, 'labware/plates/multi_pooled_plate'
   write_inheritable_attribute :printing_partial, 'labware/plates/tube_printing'
