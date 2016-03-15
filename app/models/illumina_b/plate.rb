69b029a2e479521e44c191da34c4101ca9e5c875
James Glover <james.glover@sanger.ac.uk>
2016-03-15 14:37:40 +0000

    Coerece all Cap lib pool plates

--- app/models/illumina_b/plate.rb
+++ app/models/illumina_b/plate.rb
@@ -20,6 +20,9 @@ class IlluminaB::Plate < Sequencescape::Plate
   ]
 
   TUBES_ON_PASS = [
+  ]
+
+  TUBES_ON_CREATE = [
 
     'ISCH cap lib pool'
   ]
@@ -30,10 +33,14 @@ class IlluminaB::Plate < Sequencescape::Plate
   private :is_a_final_pooling_plate?
 
   def tubes_created?
-    qc_complete? || (passed? && tubes_on_pass?)
+    qc_complete? || (passed? && tubes_on_pass?) || tubes_on_create?
   end
 
   def tubes_on_pass?
+    TUBES_ON_PASS.include?(plate_purpose.name)
+  end
+
+  def tubes_on_create?
     TUBES_ON_PASS.include?(plate_purpose.name)
   end
 
