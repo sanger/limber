e63f19c74d995a35481e550a7e4b595448df60a0
James Glover <james.glover@sanger.ac.uk>
2016-03-15 10:52:19 +0000

    Create tubes along side plate at ISCH cap lib pool
    This cap lib pool plate is actuall a tube rack. If we
    create the tubes up front it makes bed verification easier.

--- app/models/forms/auto_pooling_form.rb
+++ app/models/forms/auto_pooling_form.rb
@@ -8,5 +8,15 @@ module Forms
     write_inheritable_attribute :page, 'auto_pooling'
     write_inheritable_attribute :attributes, [:api, :user_uuid, :purpose_uuid, :parent_uuid, :transfer_template_uuid]
     write_inheritable_attribute :default_transfer_template_uuid, Settings.transfer_templates['Pool wells based on submission']
+
+    def create_plate!
+
+      super
+
+      api.transfer_template.find(Settings.transfer_templates["Transfer wells to MX library tubes by submission"]).create!(
+        :source => labware_uuid,
+        :user   => user_uuid
+      )
+    end
   end
 end
