daff6b86fa798beab8f88faafccea6c5498ba6be
James Glover <james.glover@sanger.ac.uk>
2017-01-06 14:14:50 +0000

    Provide helpers to simplify api mocking

--- spec/models/forms/pool_tubes_by_submission_form_spec.rb
+++ spec/models/forms/pool_tubes_by_submission_form_spec.rb
@@ -32,50 +32,39 @@ describe Forms::PoolTubesBySubmissionForm do
   context '#save!' do
     has_a_working_api
 
-     # Used to fetch the pools. This is the kind of thing we could pass through from a custom form
+    # Used to fetch the pools. This is the kind of thing we could pass through from a custom form
     let!(:parent_request) do
-      stub_request(:get, api_url_for(parent_uuid))
-        .to_return(status: 200, body: parent, headers: {'content-type' => 'application/json'})
+      stub_api_get(parent_uuid, body: parent)
     end
 
     let(:tube_creation_request_uuid) { SecureRandom.uuid }
 
     let!(:tube_creation_request) do
-      stub_request(:post, api_url_for('specific_tube_creations'))
-        .with(
-          headers: { 'Accept' => 'application/json', 'content-type' => 'application/json' },
-          body: { specific_tube_creation: { user: user_uuid, parent: parent_uuid, child_purposes: [purpose_uuid, purpose_uuid] } }
-        )
-        .to_return(
-          status: 201,
-          body: json(:specific_tube_creation, uuid: tube_creation_request_uuid, children_count: 2),
-          headers: {'content-type' => 'application/json'}
-        )
+      stub_api_post(
+        'specific_tube_creations',
+        payload: { specific_tube_creation: { user: user_uuid, parent: parent_uuid, child_purposes: [purpose_uuid, purpose_uuid] } },
+        body: json(:specific_tube_creation, uuid: tube_creation_request_uuid, children_count: 2)
+      )
     end
 
     # Find out what tubes we've just made!
     let!(:tube_creation_children_request) do
-      stub_request(:get, api_url_for(tube_creation_request_uuid, 'children'))
-        .to_return(status: 200, body: json(:tube_collection), headers: {'content-type' => 'application/json'})
+      stub_api_get(tube_creation_request_uuid, 'children', body: json(:tube_collection))
     end
 
     # The API needs to pull back the transfer template to know what actions it can perform
     let!(:transfer_template_request) do
-      stub_request(:get, 'http://example.com:3000/transfer-to-wells-by-submission-uuid')
-        .to_return(status: 200, body: json(:transfer_to_specific_tubes_by_submission), headers: { 'content-type' => 'application/json' })
+      stub_api_get('transfer-to-wells-by-submission-uuid', body: json(:transfer_to_specific_tubes_by_submission))
     end
 
     let!(:transfer_creation_request) do
-      stub_request(:post, 'http://example.com:3000/transfer-to-wells-by-submission-uuid')
-        .with(
-          headers: { 'Accept' => 'application/json', 'content-type' => 'application/json' },
-          body: { transfer: {
-            targets: { 'pool-1-uuid' => 'tube-0', 'pool-2-uuid' => 'tube-1' },
-            source: parent_uuid,
-            user: user_uuid
-          }}
-        )
-        .to_return(status: 200, body: '{}', headers: { 'content-type' => 'application/json' })
+      stub_api_post('transfer-to-wells-by-submission-uuid',
+        payload: { transfer: {
+          targets: { 'pool-1-uuid' => 'tube-0', 'pool-2-uuid' => 'tube-1' },
+          source: parent_uuid,
+          user: user_uuid
+        }},
+        body: '{}')
     end
 
     it 'pools by submission' do
--- spec/support/api_url_helper.rb
+++ spec/support/api_url_helper.rb
@@ -16,6 +16,49 @@ module ApiUrlHelper
       uuid = model.is_a?(String) ? model : model.uuid
       [api_root, uuid, *components].join('/')
     end
+
+    # Generate an API stub for a get request.
+    # eg. stub_api_get(plate, 'children', body: json(:plate_collection))
+    # stub_api_get(plate_uuid, body: json(:plate))
+    # @param [Api::Resource,*String] components one or more components that form the url. Models are converted to their uuid.
+    # @param [String] body: named_parameter reflecting the expected response JSON
+    # @param [Int] status: the response status, defaults to 200
+    # @return mocked_request
+    def stub_api_get(*components, status: 200, body:)
+      stub_request(:get, api_url_for(*components))
+        .to_return(
+          status: status,
+          body: body,
+          headers: { 'content-type' => 'application/json' }
+        )
+    end
+
+    # Generate an API stub for a post request.
+    # eg. stub_api_get(plate, 'children', body: json(:plate_collection))
+    # stub_api_post('transfer-to-wells-by-submission-uuid',
+    #               payload: { transfer: {
+    #                 targets: { 'pool-1-uuid' => 'tube-0', 'pool-2-uuid' => 'tube-1' },
+    #                 source: parent_uuid,
+    #                 user: user_uuid
+    #               }},
+    #               body: json(:transfer))
+    # @param [Api::Resource,*String] components one or more components that form the url. Models are converted to their uuid.
+    # @param [String] body: named_parameter reflecting the expected response JSON
+    # @param [Hash] payload: the payload of the post request. Hash strongly recommended over raw json
+    # @param [Int] status: the response status, defaults to 201
+    # @return mocked_request
+    def stub_api_post(*components, status: 201, body:, payload:)
+      stub_request(:post, api_url_for(*components))
+        .with(
+          headers: { 'Accept' => 'application/json', 'content-type' => 'application/json' },
+          body: payload
+        )
+        .to_return(
+          status: status,
+          body: body,
+          headers: { 'content-type' => 'application/json' }
+        )
+    end
   end
 end
 
