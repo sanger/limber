2f4e4e86ae7da5b3bb315bc8b60c1f133c1404f3
James Glover <james.glover@sanger.ac.uk>
2017-01-17 16:51:35 +0000

    Remove unused files
    These classes are unused. SOme of the behaviour may return in future versions.
    The MiSeqQC tube has been left in place foe now, as it is expected to be
    required shortly.

--- app/models/presenters/failable_presenter.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-module Presenters
-  class FailablePresenter < StandardPresenter
-    self.well_failure_states = [:passed]
-  end
-end
--- app/models/presenters/pcr_presenter.rb
+++ app/models/presenters/pcr_presenter.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 module Presenters
+  # Standard presenter, but renders tags.
+  # Could probably get rid of this.
   class PcrPresenter < StandardPresenter
     self.aliquot_partial = 'tagged_aliquot'
   end
