3da05cbadf022d3fc5d6df3b3c0f1b6bca26d2cb
James Glover <james.glover@sanger.ac.uk>
2017-04-13 15:24:18 +0100

    Add pooling to final tube directly from plates

--- spec/factories/transfer_factories.rb
+++ spec/factories/transfer_factories.rb
@@ -16,5 +16,36 @@ FactoryGirl.define do
       destination { associated :multiplexed_library_tube, uuid: destination_uuid }
       user { associated :user, uuid: user_uuid }
     end
+
+    factory :transfer_to_mx_tubes_by_submission do
+      transient do
+        target_tubes_count 2
+        source_wells { WellHelpers.column_order[0,target_tubes_count] }
+      end
+      source { associated :plate, uuid: source_uuid }
+      user { associated :user, uuid: user_uuid }
+
+      # Transfers to tubes have a hash, which acts as a minimal representation
+      # of the tubes and their source wells.
+      transfers do
+        transfer_hash = {}
+        source_wells.each_with_index do |well, i|
+          transfer_hash[well] = {
+          'uuid' => "child-tube-#{i}",
+          'name' => "Child tube #{i}",
+          'state' => "pending",
+          'label' => {"text":"Example purpose","prefix":"prefix"},
+          'barcode' => {
+            "number" => "#{i+1}",
+            "prefix" => "NT",
+            "two_dimensional" => nil,
+            "ean13" => SBCF::SangerBarcode.new(prefix:'NT',number:i+1).machine_barcode.to_s,
+            "type" => 2
+          }
+        }
+        end
+        transfer_hash
+      end
+    end
   end
 end
---
+++ spec/models/labware_creators/final_tube_from_plate_spec.rb
@@ -0,0 +1,98 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+require 'labware_creators/base'
+
+# CreationForm is the base class for our forms
+describe LabwareCreators::FinalTubeFromPlate do
+  subject do
+    LabwareCreators::FinalTubeFromPlate.new(form_attributes)
+  end
+
+  # Set up our templates
+  before(:each) do
+    LabwareCreators::FinalTubeFromPlate.default_transfer_template_uuid = 'transfer-to-mx-tubes-on-submission'
+  end
+
+  let(:user_uuid)    { SecureRandom.uuid }
+  let(:user)         { json :user, uuid: user_uuid }
+  let(:purpose_uuid) { SecureRandom.uuid }
+  let(:purpose)      { json :purpose, uuid: purpose_uuid }
+  let(:parent_uuid)  { SecureRandom.uuid }
+  let(:parent)       { json :plate, uuid: parent_uuid, pool_sizes: [3, 3] }
+
+  let(:form_attributes) do
+    {
+      user_uuid: user_uuid,
+      purpose_uuid: purpose_uuid,
+      parent_uuid: parent_uuid,
+      api: api
+    }
+  end
+
+  context '#save!' do
+    has_a_working_api
+
+    # Used to fetch the pools. This is the kind of thing we could pass through from a custom form
+    let!(:parent_request) do
+      stub_api_get(parent_uuid, body: parent)
+    end
+
+    # The API needs to pull back the transfer template to know what actions it can perform
+    let!(:transfer_template_request) do
+      stub_api_get('transfer-to-mx-tubes-on-submission', body: json(:transfer_wells_to_mx_library_tubes_by_submission))
+    end
+
+    let!(:transfer_creation_request) do
+      stub_api_post('transfer-to-mx-tubes-on-submission',
+                    payload: { transfer: {
+                      source: parent_uuid,
+                      user: user_uuid
+                    } },
+                    body: json(:transfer_to_mx_tubes_by_submission))
+    end
+
+    let!(:tube_state_change_request_0) do
+      stub_api_post(
+        'state_changes',
+        payload: {
+          'state_change' => {
+            user: user_uuid,
+            target: 'child-tube-0',
+            target_state: 'passed'
+          }
+        },
+        body: '{}' # We don't care about the response
+      )
+    end
+      let!(:tube_state_change_request_1) do
+      stub_api_post(
+        'state_changes',
+        payload: {
+          'state_change' => {
+            user: user_uuid,
+            target: 'child-tube-1',
+            target_state: 'passed'
+          }
+        },
+        body: '{}' # We don't care about the response
+      )
+    end
+
+    it 'pools by submission' do
+      expect(subject.save!).to be_truthy
+      expect(transfer_creation_request).to have_been_made.once
+    end
+
+    it 'passes the tubes automatically' do
+      subject.save!
+      expect(tube_state_change_request_0).to have_been_made.once
+      expect(tube_state_change_request_1).to have_been_made.once
+    end
+
+    it 'redirects to the parent plate' do
+      subject.save!
+      expect(subject.child.uuid).to eq(parent_uuid)
+    end
+  end
+end
