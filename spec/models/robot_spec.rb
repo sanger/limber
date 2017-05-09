18d617b289841f592d8b59b754c34dea9082092b
James Glover <james.glover@sanger.ac.uk>
2017-05-09 12:59:38 +0100

    Extend robot specs

--- spec/models/robot_spec.rb
+++ spec/models/robot_spec.rb
@@ -5,11 +5,27 @@ require 'rails_helper'
 describe Robots::Robot do
   include FeatureHelpers
 
+  has_a_working_api
+
   let(:api)                         { Sequencescape::Api.new(Limber::Application.config.api_connection_options) }
   let(:settings)                    { YAML.load_file(Rails.root.join('spec', 'data', 'settings.yml')).with_indifferent_access }
   let(:user_uuid)                   { SecureRandom.uuid }
   let(:plate_uuid)                  { SecureRandom.uuid }
-  let(:plate)                       { json :stock_plate, uuid: plate_uuid }
+  let(:source_barcode)              { ean13(1) }
+  let(:source_purpose_name)         { 'Limber Cherrypicked' }
+  let(:source_purpose_uuid)         { SecureRandom.uuid }
+  let(:plate)                       do
+    json :plate,
+         uuid: plate_uuid,
+         barcode_number: 1,
+         purpose_name: source_purpose_name,
+         purpose_uuid: source_purpose_uuid,
+         state: 'passed'
+  end
+  let(:target_barcode)              { ean13(2) }
+  let(:target_purpose_name)         { 'target_plate_purpose' }
+  let(:target_purpose_uuid)         { SecureRandom.uuid }
+  let(:target_plate)                { json :plate, purpose_name: target_purpose_name, purpose_uuid: target_purpose_uuid, barcode_number: 2 }
   let(:metadata_uuid)               { SecureRandom.uuid }
   let(:custom_metadatum_collection) { json :custom_metdatum_collection, uuid: metadata_uuid }
 
@@ -19,9 +35,12 @@ describe Robots::Robot do
 
       before(:each) do
         Settings.robots['robot_id'] = settings[:robots][:robot_id]
-      end
+        Settings.purpose_uuids[source_purpose_name] = source_purpose_uuid
+        Settings.purpose_uuids[target_purpose_name] = target_purpose_uuid
 
-      has_a_working_api
+        stub_asset_search(source_barcode, plate)
+        stub_asset_search(target_barcode, target_plate)
+      end
 
       it 'returns an error if there is an invalid bed' do
         stub_search_and_single_result('Find assets by barcode', { 'search' => { 'barcode' => 'dodgy_barcode' } }, nil)
@@ -29,14 +48,42 @@ describe Robots::Robot do
       end
 
       it 'returns an error if the plate is not on the right bed' do
-        stub_search_and_single_result('Find assets by barcode', { 'search' => { 'barcode' => 'plate_barcode' } }, plate)
-        expect(robot.verify(settings[:robots][:robot_id][:beds].keys.first => ['plate_barcode'])[:valid]).to be_falsey
+        expect(robot.verify('bed3_barcode' => [source_barcode])[:valid]).to be_falsey
+      end
+
+      it 'passes if everything is tickety-boo' do
+        stub_search_and_single_result('find-source-assets-by-destination-asset-barcode', { 'search' => { 'barcode' => target_barcode } }, plate)
+        expect(robot.verify('bed1_barcode' => [source_barcode], 'bed2_barcode' => [target_barcode])[:valid]).to be_truthy
+      end
+
+      it 'fails if the plates aren\'t related' do
+        stub_search_and_single_result('find-source-assets-by-destination-asset-barcode', { 'search' => { 'barcode' => target_barcode } }, json(:plate))
+        expect(robot.verify('bed1_barcode' => [source_barcode], 'bed2_barcode' => [target_barcode])[:valid]).to be_falsey
+      end
+
+      context 'with grand children' do
+        let(:robot) { Robots::Robot.find(id: 'grandparent_robot', api: api, user_uuid: user_uuid) }
+        let(:grandchild_purpose_name) { 'target2_plate_purpose' }
+        let(:grandchild_purpose_uuid) { SecureRandom.uuid }
+        let(:grandchild_barcode)      { ean13(3) }
+        let(:grandchild_plate)        { json :plate, uuid: plate_uuid, purpose_name: grandchild_purpose_name, purpose_uuid: grandchild_purpose_uuid, barcode_number: 3 }
+
+        before(:each) do
+          Settings.robots['grandparent_robot'] = settings[:robots][:grandparent_robot]
+          Settings.purpose_uuids[grandchild_purpose_name] = grandchild_purpose_uuid
+          stub_asset_search(grandchild_barcode, grandchild_plate)
+        end
+
+        it 'passes if everything is tickety-boo' do
+          stub_search_and_single_result('find-source-assets-by-destination-asset-barcode', { 'search' => { 'barcode' => target_barcode } }, plate)
+          stub_search_and_single_result('find-source-assets-by-destination-asset-barcode', { 'search' => { 'barcode' => grandchild_barcode } }, target_plate)
+          v = robot.verify('bed1_barcode' => [source_barcode], 'bed2_barcode' => [target_barcode], 'bed3_barcode' => [grandchild_barcode])
+          expect(v[:valid]).to be_truthy
+        end
       end
     end
 
     describe 'robot barcode' do
-      has_a_working_api
-
       let(:robot) { Robots::Robot.find(id: 'robot_id_2', api: api, user_uuid: user_uuid) }
 
       before(:each) do
@@ -103,8 +150,6 @@ describe Robots::Robot do
   end
 
   describe '#perform_transfer' do
-    has_a_working_api
-
     let(:robot) { Robots::Robot.find(id: 'bravo-lb-end-prep', api: api, user_uuid: user_uuid) }
 
     let(:state_change_request) do
