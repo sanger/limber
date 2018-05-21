9be4ad7531343fc817612f19dc1e0e6933a4b286
James Glover <james.glover@sanger.ac.uk>
2018-05-21 14:43:30 +0100

    Update well json to match reality

--- spec/views/exports/concentrations.csv.erb_spec.rb
+++ spec/views/exports/concentrations.csv.erb_spec.rb
@@ -6,8 +6,8 @@ describe 'exports/concentrations.csv.erb' do
   context 'with a full plate' do
     has_a_working_api
 
-    let(:well_a1) { create(:well_v2, position: 'A1', qc_results: create_list(:qc_result, 1)) }
-    let(:well_b1) { create(:well_v2, position: 'B1', qc_results: create_list(:qc_result, 1)) }
+    let(:well_a1) { create(:well_v2, position: { 'name' => 'A1' }, qc_results: create_list(:qc_result, 1)) }
+    let(:well_b1) { create(:well_v2, position: { 'name' => 'B1' }, qc_results: create_list(:qc_result, 1)) }
     let(:labware) { create(:v2_plate, wells: [well_a1, well_b1], pool_sizes: [1, 1]) }
 
     before do
