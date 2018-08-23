d7dd5bddeff8eec354984a91e89bc6ef50eb240f
James Glover <james.glover@sanger.ac.uk>
2018-08-23 10:50:18 +0100

    Try and resolve travis issues

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -42,7 +42,7 @@ end
 Capybara.register_driver :headless_chrome do |app|
   options = Selenium::WebDriver::Chrome::Options.new
   options.add_argument('--headless')
-  # options.add_argument('--disable_gpu')
+  options.add_argument('--disable_gpu')
   options.add_argument('--window-size=1600,3200')
   driver = Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
   # enable_chrome_headless_downloads(driver, DownloadHelpers::PATH.to_s)
