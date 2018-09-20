b5fe4abd0d10e9fa9a0f2ca0c09b7d637b90c0f3
Stephen Inglis <si5@sanger.ac.uk>
2018-09-20 14:59:57 +0100

    Reverted print job to output ean13 barcode for default print job.

--- app/models/labels/plate_label.rb
+++ app/models/labels/plate_label.rb
@@ -7,7 +7,7 @@ class Labels::PlateLabel < Labels::Base
       bottom_left: "#{labware.barcode.prefix} #{labware.barcode.number}",
       top_right: "#{labware.stock_plate.barcode.prefix}#{labware.stock_plate.barcode.number}",
       bottom_right: "#{labware.label.prefix} #{labware.label.text}",
-      barcode: labware.human_barcode
+      barcode: labware.barcode.ean13
     }
   end
 
