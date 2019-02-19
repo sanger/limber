b8e1cd9afd0ccc9f31a92a8e61f2128ebff34e03
James Glover <james.glover@sanger.ac.uk>
2019-02-19 11:18:18 +0000

    Extend comments

--- app/models/limber/tag_layout_template/in_columns.rb
+++ app/models/limber/tag_layout_template/in_columns.rb
@@ -10,8 +10,18 @@ module Limber::TagLayoutTemplate::InColumns
   end
   private :group_wells_of_plate
 
-  # Returns the tag index for the primary tag
+  # Returns the tag index for the primary (i7) tag
   # That is the one laid out in columns with four copies of each
+  # i5 follows the same pattern, but isn't actually shown in limber
+  #
+  # @param row [Integer] Zero indexed row co-ordinate of the well
+  # @param column [Integer] Zero-indexed column co-ordinate of the well
+  # @param scale [Integer] The number of times each tag is repeated in a given row/column.
+  #                        eg. 2 for quad stamps.
+  # @param height [Integer] The number of rows on a plate
+  # @param _width [Integer] The number of columns on a plate (unused)
+  #
+  # @return [Integer] The index of the tag to use for the well
   def primary_index(row, column, scale, height, _width)
     tag_col = (column / scale)
     tag_row = (row / scale)
