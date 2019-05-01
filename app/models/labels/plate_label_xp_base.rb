8ace3fd3acf3f15566e9c6458441927fc1ac2b1f
Luca Da Rin Fioretto <ldrf@sanger.ac.uk>
2019-05-01 10:15:50 +0100

    Refactored labels. Added plate_split label

---
+++ app/models/labels/plate_label_xp_base.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+class Labels::PlateLabelXpBase < Labels::PlateLabelBase
+  def attributes
+    super.merge({ barcode: labware.barcode.human })
+  end
+
+  def qc_attributes
+    {
+      top_left: date_today,
+      bottom_left: "#{labware.barcode.human} QC",
+      top_right: labware.stock_plate&.barcode&.human,
+      barcode: "#{labware.barcode.human}-QC"
+    }
+  end
+end
