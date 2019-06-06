69e744f4894413c2467716063bf0a03ade204293
Andrew Sparkes <as28@sanger.ac.uk>
2019-06-06 10:19:00 +0100

    Added robot config for hamilton lbc aggregate to lbc cherrypick

--- config/robots.rb
+++ config/robots.rb
@@ -1036,4 +1036,80 @@ ROBOT_CONFIG = RobotConfiguration::Register.configure do
     from 'GnT scDNA', bed(4)
     to 'GnT MDA Norm', car('2,3')
   end
+
+  # For Chromium 10x pipeline
+  custom_robot('hamilton-lbc-aggregate-to-lbc-cherrypick',
+               name: 'hamilton LBC Aggregate => LBC Cherrypick',
+               beds: {
+                 bed(1).barcode => {
+                   purpose: 'LBC Aggregate',
+                   states: %w[passed qc_complete],
+                   child: bed(13).barcode,
+                   label: 'Bed 1' },
+                 bed(2).barcode => {
+                   purpose: 'LBC Aggregate',
+                   states: %w[passed qc_complete],
+                   child: bed(13).barcode,
+                   label: 'Bed 2' },
+                 bed(3).barcode => {
+                   purpose: 'LBC Aggregate',
+                   states: %w[passed qc_complete],
+                   child: bed(13).barcode,
+                   label: 'Bed 3' },
+                 bed(4).barcode => {
+                   purpose: 'LBC Aggregate',
+                   states: %w[passed qc_complete],
+                   child: bed(13).barcode,
+                   label: 'Bed 4' },
+                 bed(5).barcode => {
+                   purpose: 'LBC Aggregate',
+                   states: %w[passed qc_complete],
+                   child: bed(13).barcode,
+                   label: 'Bed 5' },
+                 bed(6).barcode => {
+                   purpose: 'LBC Aggregate',
+                   states: %w[passed qc_complete],
+                   child: bed(13).barcode,
+                   label: 'Bed 6' },
+                 bed(7).barcode => {
+                   purpose: 'LBC Aggregate',
+                   states: %w[passed qc_complete],
+                   child: bed(13).barcode,
+                   label: 'Bed 7' },
+                 bed(8).barcode => {
+                   purpose: 'LBC Aggregate',
+                   states: %w[passed qc_complete],
+                   child: bed(13).barcode,
+                   label: 'Bed 8' },
+                 bed(9).barcode => {
+                   purpose: 'LBC Aggregate',
+                   states: %w[passed qc_complete],
+                   child: bed(13).barcode,
+                   label: 'Bed 9' },
+                 bed(10).barcode => {
+                   purpose: 'LBC Aggregate',
+                   states: %w[passed qc_complete],
+                   child: bed(13).barcode,
+                   label: 'Bed 10' },
+                 bed(13).barcode => {
+                   purpose: 'LBC Cherrypick',
+                   states: %w[pending started],
+                   parents: [
+                     bed(1).barcode,
+                     bed(2).barcode,
+                     bed(3).barcode,
+                     bed(4).barcode,
+                     bed(5).barcode,
+                     bed(6).barcode,
+                     bed(7).barcode,
+                     bed(8).barcode,
+                     bed(9).barcode,
+                     bed(10).barcode
+                   ],
+                   target_state: 'passed',
+                   label: 'Bed 13'
+                 }
+               },
+               destination_bed: bed(13).barcode,
+               class: 'Robots::PoolingRobot')
 end
