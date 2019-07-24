68c3ce50fd1dd58f47b0e5fc7d8673965b4a435d
Luca Da Rin Fioretto <ldrf@sanger.ac.uk>
2019-07-24 13:22:46 +0100

    Add stashed files form fix_multi_stamp_transfers

---
+++ app/models/labware_creators/quadrant_stamp_base.rb
@@ -0,0 +1,75 @@
+# frozen_string_literal: true
+
+module LabwareCreators
+  # Basic quadrant stamp behaviour, applies no special request filters
+  # See MultiStamp for further documentation
+  #
+  # Handles the generation of 384 well plates from 1-4 96 well plates.
+  #
+  # Briefly, 96 well plates get stamped onto 384 plates in an interpolated pattern
+  # eg.
+  # +--+--+--+--+--+--+--~
+  # |P1|P3|P1|P3|P1|P3|P1
+  # |A1|A1|A2|A2|A3|A3|A4
+  # +--+--+--+--+--+--+--~
+  # |P2|P4|P2|P4|P2|P4|P1
+  # |A1|A1|A2|A2|A3|A3|A4
+  # +--+--+--+--+--+--+--~
+  # |P1|P3|P1|P3|P1|P3|P1
+  # |B1|B1|B2|B2|B3|B3|B4
+  # +--+--+--+--+--+--+--~
+  # |P2|P4|P2|P4|P2|P4|P1
+  # |B1|B1|B2|B2|B3|B3|B4
+  #
+  # The transfers layout 'quadrant' descibed above is implemented client side.
+  #
+  class QuadrantStampBase < MultiStamp
+    self.transfers_layout = 'quadrant'
+    self.target_rows = 16
+    self.target_columns = 24
+    self.source_plates = 4
+    self.attributes += [
+      {
+        transfers: [
+          [:source_plate, :source_asset, :outer_request, :pool_index, { new_target: :location }]
+        ]
+      }
+    ]
+    private
+
+    def create_labware!
+      super do |child|
+        PlateMetadata.new(
+          api: api,
+          user: user_uuid,
+          plate: child
+        ).update!(stock_barcodes_by_quadrant)
+        yield(child) if block_given?
+      end
+    end
+
+    def source_plates_by_quadrant
+      source_plates_uuids = Array.new(4)
+      transfers.each do |transfer|
+        target_well_location = transfer.dig(:new_target, :location)
+        target_well_quadrant = WellHelpers.well_quadrant(target_well_location)
+        if source_plates_uuids[target_well_quadrant].nil?
+          source_plates_uuids[target_well_quadrant] = transfer[:source_plate]
+        end
+      end
+      source_plates_uuids
+    end
+
+    def stock_barcodes_by_quadrant
+      quadrants = {}
+      source_plates_by_quadrant.each_with_index do |uuid, index|
+        next if uuid.nil?
+
+        source_plate = Sequencescape::Api::V2::Plate.find_by(uuid: uuid)
+        stock_barcode = source_plate&.stock_plate&.barcode&.human
+        quadrants["stock_barcode_q#{index}".to_sym] = stock_barcode unless stock_barcode.nil?
+      end
+      quadrants
+    end
+  end
+end
---
+++ app/models/labware_creators/ten_stamp.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+# Handle the generation of 10x 96-wells plates.
+# In the client, the user will be able to scan 10 96-wells plate barcodes to
+# create a single 96-well plate. Transfer requests will be collected in column
+# order starting from the first of the 10 plates. The destination wells will be
+# laid down in column order.
+#
+# Eg.
+#
+# Plate 1        Plate 2         Dest. Plate
+# +--+--+--~     +--+--+--~      +----+----+----~
+# |A1|  |A3      |A1|  |         |P1A1|P1A3|P2D2
+# +--+--+--~     +--+--+--~      +----+----+----~
+# |  |  |        |  |B2|B3       |P1C1|P1D3|P2B3
+# +--+--+--~  +  +--+--+--~  =>  +----+----+----~
+# |C1|C2|        |  |  |         |P1D1|P2A1|P2D3
+# +--+--+--~     +--+--+--~      +----+----+----~
+# |D1|  |D3      |  |D2|D3       |P1C2|P2B2|
+#
+# The user must specify a volume value which will be recorded on each
+# transfer request. The transfer creator logic 'with-volume' is implemented
+# client side.
+
+module LabwareCreators
+  class TenStamp < MultiStamp
+    self.transfers_layout = 'sequential'
+    self.transfers_creator = 'with-volume'
+    self.attributes += [
+      {
+        transfers: [
+          [:source_plate, :source_asset, :outer_request, :pool_index, { new_target: :location }, :volume]
+        ]
+      }
+    ]
+    self.target_rows = 8
+    self.target_columns = 12
+    self.source_plates = 10
+
+    private
+
+    def request_hash(transfer, *args)
+      # We might want to add the 'volume' key into a nested hash called 'metadata'
+      super.merge('volume' => transfer[:volume])
+    end
+  end
+end
