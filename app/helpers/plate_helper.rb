da8dba37770191c73a8aa1e99ca1c9d812ff3c9f
Andrew Sparkes <as28@sanger.ac.uk>
2019-10-30 12:07:31 +0000

    Added sort on order id to pools helper method

--- app/helpers/plate_helper.rb
+++ app/helpers/plate_helper.rb
@@ -19,30 +19,40 @@ module PlateHelper
     WellFailingPresenter.new(form, presenter)
   end
 
-  # Returns an array of all pre-capture pools sorted in column order based on the first
-  # well in the pool.
+  # Returns an array of all pre-capture pools for a plate, with wells sorted into plate well column order.
   # We rely on the fact that hashes maintain insert order, and walk the wells in column
-  # order. Each time we see a pre-capture pool for the first time, it gets inserted into the hash.
-  # This gets passed to our javascript via ajax.
+  # order. Each time we see a pre-capture pool for the first time, it gets inserted into the hash along with the
+  # request Order id.
+  # We then sort the pre-capture pool hashes within the array by their Order ids, to get the pools into the right
+  # sequence. Requests are grouped into Orders, each pre-capture pool has a different Order relating to the asset group
+  # name entered on the submission manifest. Asset groups are created in the sequence they are presented in the manifest.
+  # (Pre-capture pools are not created sequentially in the same order as the asset groups on the manifest).
+  # This sorted array gets passed to our javascript via ajax.
   # @note Javascript objects are not explicitly ordered, hence the need to pass an array here.
   #
   # @param current_plate [Sequencescape::Api::V2::Plate] The plate from which to extract the pre-cap pools
   #
   # @return [Array] An array of basic pool information
   # @example Example output
-  #   [{ pool_id: '123', wells: ['A1','B1','D1'] }, { pool_id: '122', wells: ['C1','E1','F1'] }]
+  #   [
+  #    { pool_id: 123, order_id: 401, wells: ['A1','B1','D1'] },
+  #    { pool_id: 122, order_id: 402, wells: ['C1','E1','F1'] }
+  #   ]
   def sorted_pre_cap_pool_json(current_plate)
-    current_plate.wells_in_columns.each_with_object({}) do |well, pool_store|
+    unsorted = current_plate.wells_in_columns.each_with_object({}) do |well, pool_store|
       next unless well.passed?
 
       well.incomplete_requests.each do |request|
         next unless request.pre_capture_pool
 
         pool_id = request.pre_capture_pool.id
-        pool_store[pool_id] ||= { pool_id: pool_id, wells: [] }
+        pool_store[pool_id] ||= { pool_id: pool_id, order_id: request.order_id, wells: [] }
         pool_store[pool_id][:wells] << well.location
       end
-    end.values.to_json.html_safe
+    end.values
+    # sort the pool hashes by Order id
+    sorted = unsorted.sort_by { |k| k[:order_id] }
+    sorted.to_json.html_safe
   end
 
   def pools_by_id(pools)
