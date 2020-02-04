e530e82efe26d59efa5defbc06c0c8c6912117b1
Andrew Sparkes <as28@sanger.ac.uk>
2020-02-04 16:04:38 +0000

    Added request state filter to well filter allowing partials Renamed variables.

--- app/models/labware_creators/well_filter_allowing_partials.rb
+++ app/models/labware_creators/well_filter_allowing_partials.rb
@@ -2,24 +2,34 @@
 
 #
 # Version of WellFilter allowing partial submissions from a plate.
+# i.e. not all wells need to match the well filter.
+# Also adds a check on request state.
 #
 class LabwareCreators::WellFilterAllowingPartials < LabwareCreators::WellFilter
+
+  attr_accessor :request_state
+
   private
+
+  def filter_by_state(requests)
+    requests.select { |r| @request_state.blank? || @request_state.include?(r.state) }
+  end
 
   def filter_requests(requests, well)
     return extract_submission(well) if well.requests_as_source.empty?
 
-    filtered_requests = filter_by_request_type(requests)
-    filtered_requests_by_library_type = filter_requests_by_library_type(filtered_requests)
+    filtered_requests_by_rt = filter_by_request_type(requests)
+    filtered_requests_by_lt = filter_by_library_type(filtered_requests_by_rt)
+    filtered_requests_by_state = filter_by_state(filtered_requests_by_lt)
 
-    num_requests = filtered_requests_by_library_type.count
+    num_requests = filtered_requests_by_state.count
 
     if num_requests.zero?
       # likely a partial submission, and this well is not required and has no matching request
       nil
     elsif num_requests == 1
       #  valid, one matching request found
-      { 'outer_request' => filtered_requests_by_library_type.first.uuid }
+      { 'outer_request' => filtered_requests_by_state.first.uuid }
     else
       # too many matching requests, cannot disentangle
       errors.add(:base, "found #{num_requests} eligible requests for #{well.location}, possible overlapping submissions")
@@ -32,7 +42,7 @@ class LabwareCreators::WellFilterAllowingPartials < LabwareCreators::WellFilter
 
       filtered_requests = filter_requests(well.active_requests, well)
 
-      # don't add wells to the transfers list if they have no filtered requests
+      # don't add wells to the transfers list if they have no filtered requests, i.e. only those submitted for library prep
       transfers << [well, filtered_requests] unless filtered_requests.nil?
     end
   end
