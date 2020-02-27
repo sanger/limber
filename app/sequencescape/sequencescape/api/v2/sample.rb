c235e48109ea70218b3d34f31fa17025d9e88839
Katy Taylor <kt17@sanger.ac.uk>
2020-02-27 10:54:15 +0000

    Add a relationship between Sample and Sample Metadata in the Limber v2 API Sample model. - Was getting an error on this line: aliquots.first.sample.first.sample_metadata.supplier_name - because it couldn't find the sample_metadata on the sample

--- app/sequencescape/sequencescape/api/v2/sample.rb
+++ app/sequencescape/sequencescape/api/v2/sample.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
 
 class Sequencescape::Api::V2::Sample < Sequencescape::Api::V2::Base
+  has_one :sample_metadata, class_name: 'Sequencescape::Api::V2::SampleMetadata'
 end
