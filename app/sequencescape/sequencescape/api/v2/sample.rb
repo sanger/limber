a73c1221c6a9393498ea8fdefb102176ef07e227
Katy Taylor <kt17@sanger.ac.uk>
2020-02-28 14:50:00 +0000

    Add extra columns in csv (blank, for faculty to fill in) - moved well_spec test to api tests folder to be with the similar plate one

--- app/sequencescape/sequencescape/api/v2/sample.rb
+++ app/sequencescape/sequencescape/api/v2/sample.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# Represents a sample in Limber via the Sequencescape API
 class Sequencescape::Api::V2::Sample < Sequencescape::Api::V2::Base
   has_one :sample_metadata, class_name: 'Sequencescape::Api::V2::SampleMetadata'
 end
