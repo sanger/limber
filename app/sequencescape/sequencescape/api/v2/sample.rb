576314ed61caedd1188e2432911c3f499442ec3b
Katy Taylor <kt17@sanger.ac.uk>
2020-03-02 15:23:56 +0000

    forgot to change this back when we added the inflections

--- app/sequencescape/sequencescape/api/v2/sample.rb
+++ app/sequencescape/sequencescape/api/v2/sample.rb
@@ -2,5 +2,5 @@
 
 # Represents a sample in Limber via the Sequencescape API
 class Sequencescape::Api::V2::Sample < Sequencescape::Api::V2::Base
-  has_one :sample_metadata, class_name: 'Sequencescape::Api::V2::SampleMetadatum'
+  has_one :sample_metadata, class_name: 'Sequencescape::Api::V2::SampleMetadata'
 end
