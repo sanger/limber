f40f41a0ff82d3c2691c0ac3fdca40b0bf90e3fe
Andrew Sparkes <as28@sanger.ac.uk>
2020-03-08 19:12:09 +0000

    Added binning calculator

--- app/models/utility/pcr_cycles_binning_calculator.rb
+++ app/models/utility/pcr_cycles_binning_calculator.rb
@@ -5,18 +5,16 @@ module Utility
   # Used by the PCR Cycles Binned Plate class to handle the binning processing.
   class PcrCyclesBinningCalculator
     include ActiveModel::Model
+    include Utility::CommonDilutionCalculations
 
-    # def initialize(config)
-    #   @config = Utility::PcrCyclesDilutionsConfig.new(config)
-    # end
+    def initialize(well_details)
+      @well_details = well_details
+    end
 
-    # TODO: do we need to pass in anything here? is any configuration required?
-    def initialize; end
-
-    def compute_well_transfers_hash(well_details, number_of_rows, number_of_columns)
-      pcr_cycle_bins = pcr_cycle_bins(well_details)
-      compression_reqd = compression_required?(pcr_cycle_bins, number_of_rows, number_of_columns)
-      build_transfers_hash(pcr_cycle_bins, number_of_rows, compression_reqd)
+    def compute_well_transfers(parent_plate)
+      bins_hash = pcr_cycle_bins
+      compression_reqd = compression_required?(bins_hash, parent_plate.number_of_rows, parent_plate.number_of_columns)
+      build_transfers_hash(bins_hash, parent_plate.number_of_rows, compression_reqd)
     end
 
     # Used by the plate presenter. Uses the well metadata to work out the well bin colour and number of PCR cycles.
@@ -37,9 +35,24 @@ module Utility
     # end
 
     # Sorts well locations into bins based on their number of pcr cycles.
-    def pcr_cycle_bins(well_details)
-      # TODO: Need to work out how many bins we have ( = distinct pcr_cycle values)
-      # TODO: then cycle through adding wells to bins
+    def pcr_cycle_bins
+      pcr_bins = calculate_bins
+      binned_wells = pcr_bins.each_with_object({}) { |bin_pcr_cycles_num, bins_hash| bins_hash[bin_pcr_cycles_num] = [] }
+      @well_details.each do |well_locn, details|
+        pcr_cycles_num = details['pcr_cycles']
+        binned_wells[pcr_cycles_num] << { 'locn' => well_locn, 'sample_volume' => details['sample_volume'] }
+      end
+      binned_wells
+    end
+
+    def calculate_bins
+      bins = []
+      @well_details.each do |well_locn, details|
+        pcr_cycles = details['pcr_cycles']
+        bins << pcr_cycles unless bins.include? pcr_cycles
+      end
+      # want pcr cycle bins in reverse order, highest first e.g. [16,14,12]
+      bins.sort.reverse
     end
 
     # Build the transfers hash, cycling through the bins and their wells and locating them onto the
@@ -47,15 +60,11 @@ module Utility
     def build_transfers_hash(bins, number_of_rows, compression_reqd)
       binner = Binner.new(compression_reqd, number_of_rows)
       bins.values.each_with_object({}).with_index do |(bin, transfers_hash), bin_index_within_bins|
-        next if bin.length.zero?
-
         bin.each_with_index do |well, well_index_within_bin|
-          # TODO: what is the structure of this? what fields are needed and do we need to add more fields on transfer requests?
           src_locn = well['locn']
-          details = well['details']
           transfers_hash[src_locn] = {
             'dest_locn' => WellHelpers.well_name(binner.row, binner.column),
-            'volume' => details['source_volume'].to_s
+            'volume' => well['sample_volume'].to_s
           }
           # work out what the next row and column will be
           finished = ((bin_index_within_bins == bins.size - 1) && (well_index_within_bin == bin.size - 1))
