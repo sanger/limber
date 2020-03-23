0f1a5d6f9d714c12f2b6202ee80cb7a12952ac53
Harriet Craven <hc6@sanger.ac.uk>
2020-03-23 13:13:07 +0000

    forgot to add the new bed class

---
+++ app/models/robots/bed/heron.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+module Robots::Bed
+  class Heron < Robots::Bed::Base
+    validate :test_validation    
+    
+    def test_validation
+        # debugger
+    end
+
+    # private
+
+  end
+end
