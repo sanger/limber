a481f6a6c4cc795fa2a1ce5de2cd739bd1403d69
James Glover <JamesGlover@users.noreply.github.com>
2020-04-22 14:41:22 +0100

    Fix spelling of pipeline
    Co-Authored-By: KatyTaylor <kt17@sanger.ac.uk>

--- spec/factories/pipeline_factories.rb
+++ spec/factories/pipeline_factories.rb
@@ -3,7 +3,7 @@
 FactoryBot.define do
   # Construct a Pipeline
   # Use create to automatically push it onto the pipelines array
-  # build just returns the pipleine object
+  # build just returns the pipeline object
   factory :pipeline do
     to_create do |instance, evaluator|
       evaluator.pipeline_list << instance
