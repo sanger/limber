9fc79ec01a7f4f42471bc0a49e2fc0620524d8cd
Harriet Craven <hc6@sanger.ac.uk>
2020-11-10 15:39:20 +0000

    rubocopped

--- spec/models/print_job_spec.rb
+++ spec/models/print_job_spec.rb
@@ -153,12 +153,12 @@ RSpec.describe PrintJob do
   describe 'print_to_sprint' do
     let(:labels_sprint) do
       {
-        'sprint'=> {
-         'right_text'=>'DN9000003B',
-         'left_text'=>'DN9000003B',
-         'barcode'=>'DN9000003B',
-         'extra_right_text'=>'DN9000003B  LTHR-384 RT',
-         'extra_left_text'=>'10-NOV-2020'
+        'sprint' => {
+          'right_text' => 'DN9000003B',
+          'left_text' => 'DN9000003B',
+          'barcode' => 'DN9000003B',
+          'extra_right_text' => 'DN9000003B  LTHR-384 RT',
+          'extra_left_text' => '10-NOV-2020'
         }
       }
     end
