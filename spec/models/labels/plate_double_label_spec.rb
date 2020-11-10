6020e45dce45a8988c05c3047ebf423b3f4ec874
Harriet Craven <hc6@sanger.ac.uk>
2020-11-10 14:11:18 +0000

    Finished the tests

--- spec/models/labels/plate_double_label_spec.rb
+++ spec/models/labels/plate_double_label_spec.rb
@@ -3,4 +3,38 @@
 require 'rails_helper'
 
 RSpec.describe Labels::PlateDoubleLabel, type: :model do
+  it { expect(described_class).to be < Labels::Base }
+
+  context 'when creating the label of a plate' do
+    let(:labware) { create :v2_plate }
+    let(:label) { Labels::PlateDoubleLabel.new(labware) }
+
+    context '#attributes' do
+      it 'has the correct attributes' do
+        attributes = label.attributes
+        expect(attributes[:right_text]).to eq labware.workline_identifier
+        expect(attributes[:left_text]).to eq labware.barcode.human
+        expect(attributes[:barcode]).to eq labware.barcode.machine
+      end
+    end
+
+    context '#extra_attributes' do
+      it 'has the correct attributes' do
+        extra_attributes = label.extra_attributes
+        expect(extra_attributes[:right_text]).to eq "#{labware.workline_identifier} #{labware.role} #{labware.purpose.name}"
+        expect(extra_attributes[:left_text]).to eq Time.zone.today.strftime('%e-%^b-%Y')
+      end
+    end
+
+    context '#sprint_attributes' do
+      it 'has the correct attributes' do
+        sprint_attributes = label.sprint_attributes
+        expect(sprint_attributes[:right_text]).to eq labware.workline_identifier
+        expect(sprint_attributes[:left_text]).to eq labware.barcode.human
+        expect(sprint_attributes[:barcode]).to eq labware.barcode.machine
+        expect(sprint_attributes[:extra_right_text]).to eq "#{labware.workline_identifier} #{labware.role} #{labware.purpose.name}"
+        expect(sprint_attributes[:extra_left_text]).to eq Time.zone.today.strftime('%e-%^b-%Y')
+      end
+    end
+  end
 end
