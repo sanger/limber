ec5563576ddf0f4429f702c7e46639695dd7710d
James Glover <james.glover@sanger.ac.uk>
2020-11-26 08:48:21 +0000

    Add uncreatable creator
    The original stock plates behave badly
    if created as part of the pipeline, so
    we disable their creation.
    
    Note: This isn't the case for *All* stock
    plates, and depends on the presence
    of downstream requests. As a result
    I've been conservative with adding it.

---
+++ app/models/labware_creators/uncreatable.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+module LabwareCreators
+  # Purposes with the Uncreatable creator can't be created, so will not appear
+  # in the 'Other plates' dropdown. This should be reserved for labware which
+  # will fail to function correctly if created through Limber.
+  # In practice this class behaves just like the base creator, but has been
+  # sub-classed to better communicate its intent.
+  class Uncreatable < Base
+    # This creator is invalid for all parents.
+    # @note This actually duplicates the behaviour on the base class, so is not
+    #       required for correct functionality. However I've decided to include
+    #       it here for reasons of clarity.
+    def self.support_parent?(_parent)
+      false
+    end
+  end
+end
