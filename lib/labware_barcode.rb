aa6516d8bd9aec4a725cca401215200d955975fb
James Glover <james.glover@sanger.ac.uk>
2021-01-12 15:12:37 +0000

    Extend documentation

--- lib/labware_barcode.rb
+++ lib/labware_barcode.rb
@@ -22,6 +22,17 @@ class LabwareBarcode
     @human
   end
 
+  #
+  # Match operator. Checks to see ith the other barcode, is a match for this one.
+  # - In the case of foreign barcodes we match only when the other barcode is equal to the machine_barcode
+  # - In the case of SBCF formatted barcodes (eg DN1234K/1220001234757) we delegate to the matcher in SBCF::SangerBarcode
+  #   This allows us to match either ean13 or code39 formatted barcodes (or the machine barcode to human readable
+  #   version in the case of older plates).
+  #
+  # @param other [String] The barcode to check
+  #
+  # @return [Bool] True is the barcodes match, false otherwise.
+  #
   def =~(other)
     if sbcf.valid?
       sbcf =~ other
