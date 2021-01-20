4952c341c9bd73460cb40b0f2b0fbc012a6f7070
Eduardo Martin Rojo <emr@sanger.ac.uk>
2021-01-20 07:47:19 +0000

    Rubocop

--- app/models/sequencescape_submission.rb
+++ app/models/sequencescape_submission.rb
@@ -102,7 +102,7 @@ class SequencescapeSubmission
   end
 
   def asset_groups_for_orders_creation
-    return asset_groups unless asset_groups.length == 1 and extra_barcodes
+    return asset_groups unless (asset_groups.length == 1) && extra_barcodes
 
     [{ assets: [assets, extra_assets].flatten.compact, autodetect_studies_projects: true }]
   end
