8ad41760452dfd9ac9bec817d475b28f77cbaac2
James Glover <james.glover@sanger.ac.uk>
2021-03-11 16:36:04 +0000

    Fix state changer using wrong reasons
    Previously the state changer was trying to use javascript
    to disable the reason fields for the opposing state change.
    However this had actually been implemented incorrectly, and also
    ignored the customer accepts responsibility checkbox.
    
    Generally I felt this approach was brittle, and could result in
    unexpected behaviour if the javascript failed, or a developer added new
    fields.
    
    Instead we group each set of fields with 'fields_for' and select them
    in the controller.
    
    This greatly simplifies the javascript, and lets us remove some helper
    methods. Their is a slight cost increase in the controller.

--- app/assets/javascripts/state_change_reasons.js
+++ app/assets/javascripts/state_change_reasons.js
@@ -1,23 +1,15 @@
-// Global SCAPE.message method
+
 (function($, window, undefined){
   "use strict";
 
   var updateDisplay = function(new_state, speed) {
-    if ($('.reason:visible').length === 0) {
-      $('#' + new_state + '_reasons').slideDown(speed).find('select:disabled').prop('disabled', false);
-    }
-    else {
-      $('.reason').not('#' + new_state + '_reasons').slideUp(speed, function () {
-        $('#' + new_state + '_reasons').slideDown(speed).find('select:disabled').prop('disabled', false);
-      }).prop('disabled', true);
-    }
+    $('.reason').not('#' + new_state + '_reasons').slideUp(speed)
+    $('#' + new_state + '_reasons').slideDown(speed)
   }
 
-  var displayReason = function() {
-    updateDisplay(this.value, 'fast')
-  };
+  var displayReason = function () { updateDisplay(this.value, 'fast') }
 
   $(function(){
-    $('#state-changer').find('input[type=radio]').on('change', displayReason);
-  });
+    $('#state-changer').find('input[type=radio]').on('change', displayReason)
+  })
 })(jQuery,window);
