2ffb24050330b13a00aa6ecd788837eca2df870a
Katy Taylor <kt17@sanger.ac.uk>
2021-04-29 15:56:07 +0100

    bit of cleaning up

--- app/controllers/pipeline_work_in_progress_controller.rb
+++ app/controllers/pipeline_work_in_progress_controller.rb
@@ -7,12 +7,13 @@ class PipelineWorkInProgressController < ApplicationController
   # Filter out ones that are complete, in the final stage of the pipeline
 
   # Questions:
-  # How quick can we load the page?
-  # How many plates will this be?
+  # How quick can we load the page? ~30 seconds so far
+  # How many plates will this be? ~542 plates
   # Does it display the same plates as the Trello board?
   def index
     puts "*** index **"
     @pipeline = '"Heron"'
+    page_size = 500
 
     p = Sequencescape::Api::V2::Labware
       .select(
@@ -24,46 +25,34 @@ class PipelineWorkInProgressController < ApplicationController
       .includes(:purpose)
       .where(
         without_children: true,
-        purpose_name: [ # cherrypick id 395
+        purpose_name: [ # LTHR Cherrypick id 395
           "LTHR Cherrypick","LTHR-384 RT-Q","LTHR-384 PCR 1","LTHR-384 PCR 2","LTHR-384 Lib PCR 1","LTHR-384 Lib PCR 2","LTHR-384 Lib PCR pool","LTHR-384 Pool XP"
         ]
       )
-      .per(500)
-
-    # TODO: do tubes as well
-    # p = Sequencescape::Api::V2::Plate.select("uuid","labware_barcode","purpose","state", {purposes: "name"})
-    #   # .includes(:state_changes)
-    #   .includes(:purpose)
-    #   .where(
-    #     without_children: true,
-    #     purpose_name: [ # cherrypick id 395
-    #       "LTHR Cherrypick","LTHR-384 RT-Q","LTHR-384 PCR 1","LTHR-384 PCR 2","LTHR-384 Lib PCR 1","LTHR-384 Lib PCR 2","LTHR-384 Lib PCR pool","LTHR-384 Pool XP"
-    #     ]
-    #   )
-
-    # binding.pry
+      .per(page_size)
 
     all_records = []
     page_num = 1
-    page_size = 100
-    while page_size == 100
+    num_retrieved = page_size
+    while num_retrieved == page_size
       puts "page_num: #{page_num}"
 
-      page_size = p.page(page_num).to_a.size
-      puts "page size: #{page_size}"
+      num_retrieved = p.page(page_num).to_a.size
+      puts "num_retrieved: #{num_retrieved}"
 
       all_records += p.page(page_num).to_a
       page_num += 1
     end
 
     records = all_records
-    # records = p.page(1).to_a
+    # records = p.page(1).to_a # use instead of line above to just display the first page
     @num_records = records.size
 
     @grouped = {}
     records.each do |rec|
       # puts rec.inspect
       # binding.pry
+
       purpose_name = rec.purpose&.name
       if @grouped.key? purpose_name
         @grouped[purpose_name] << rec
@@ -71,9 +60,5 @@ class PipelineWorkInProgressController < ApplicationController
         @grouped[purpose_name] = [rec]
       end
     end
-
-    # puts "*** grouped: #{grouped.class} ***"
-    # puts "*** grouped: #{grouped} ***"
-
   end
 end
