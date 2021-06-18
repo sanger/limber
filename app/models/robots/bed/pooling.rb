c4a9a1685ad49d4f5dd38ebfe72843575fddae03
Eduardo Martin Rojo <emr@sanger.ac.uk>
2021-06-18 11:56:46 +0100

    Redefine load of barcodes for pooling beds so they query for plates only

--- app/models/robots/bed/pooling.rb
+++ app/models/robots/bed/pooling.rb
@@ -12,6 +12,18 @@ module Robots::Bed
       end
     end
 
+    def load(barcodes)
+      # Ensure we always deal with an array, and any accidental duplicate scans are squashed out
+      @barcodes = Array(barcodes).map(&:strip).uniq.reject(&:blank?)
+
+      @labwares = if @barcodes.present?
+                    Sequencescape::Api::V2::Plate.find_all({ barcode: @barcodes }, includes: labware_includes)
+                  else
+                    []
+                  end
+    end
+
+
     private
 
     def parent_labwares
