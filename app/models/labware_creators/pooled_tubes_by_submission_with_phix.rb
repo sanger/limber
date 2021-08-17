de7dd483db7c1390041e57c8b6f60e55402d8343
Katy Taylor <kt17@sanger.ac.uk>
2021-08-17 16:56:55 +0100

    Make adding PhiX optional but encouraged

--- app/models/labware_creators/pooled_tubes_by_submission_with_phix.rb
+++ app/models/labware_creators/pooled_tubes_by_submission_with_phix.rb
@@ -2,7 +2,7 @@
 
 module LabwareCreators
   # Includes the addition of PhiX, a specific type of control sample.
-  # User scans SpikedBuffer tube containining PhiX into an interstitial page, on tube creation.
+  # User scans SpikedBuffer tube containing PhiX into an interstitial page, on tube creation.
   # SpikedBuffer tube is recorded as a parent of the newly created tube(s).
   class PooledTubesBySubmissionWithPhiX < PooledTubesBySubmission
     include LabwareCreators::CustomPage
@@ -15,11 +15,15 @@ module LabwareCreators
     attr_accessor :spikedbuffer_tube_barcode
 
     def create_child_stock_tubes
-      spiked_buffer_tube = Sequencescape::Api::V2::Tube.find_by(barcode: spikedbuffer_tube_barcode)
+      parents = [parent_uuid]
+
+      if spikedbuffer_tube_barcode.present?
+        parents << Sequencescape::Api::V2::Tube.find_by(barcode: spikedbuffer_tube_barcode).uuid
+      end
 
       tubes = api.specific_tube_creation.create!(
         user: user_uuid,
-        parents: [parent_uuid, spiked_buffer_tube.uuid],
+        parents: parents,
         child_purposes: [purpose_uuid] * pool_uuids.length,
         tube_attributes: tube_attributes
       ).children.index_by(&:name)
