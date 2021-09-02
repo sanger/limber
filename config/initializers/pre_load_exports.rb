46ab34e3671cc7c775dd16a4f2ae23347727f223
James Glover <james.glover@sanger.ac.uk>
2021-09-02 20:23:16 +0100

    Extract exports from controller to config
    There's still a lot of potential for further
    refactoring here, but this is a good first
    step.

---
+++ config/initializers/pre_load_exports.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+# It isn't entirely necessary that we pre-load our exports, however it lets
+# us know of invalid configurations upfront, rather than at runtime.
+Export.loader
