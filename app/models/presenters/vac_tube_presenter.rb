b281ccc33fff66e1346ab2552a528e2e5ff11419
Andrew Sparkes <as28@sanger.ac.uk>
2021-09-07 14:59:25 +0100

    Refactor of configs and added VacTubePresenter

---
+++ app/models/presenters/vac_tube_presenter.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+module Presenters
+  # A stock tube presenter is used for tubes just entering the pipeline.
+  # It shows a preview of the tube, but prevents well failure and state changes.
+  # In addition it also detects common scenarios which may indicate problems
+  # with the submission.
+  class VacTubePresenter < TubePresenter
+    include Presenters::Statemachine::StateAllowsChildCreation
+    include Presenters::Statemachine::DoesNotAllowLibraryPassing
+    include Presenters::StateChangeless
+
+    def state
+      "passed"
+    end
+  end
+end
