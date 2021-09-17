1b9d52a5393f790735355590177e934db65564e4
Stuart McHattie <sm49@sanger.ac.uk>
2021-09-17 17:34:30 +0100

    Update test for CSV generator

--- spec/views/exports/hamilton_pooling_plate_pbmc.erb_spec.rb
+++ spec/views/exports/hamilton_pooling_plate_pbmc.erb_spec.rb
@@ -6,9 +6,12 @@ RSpec.describe 'exports/hamilton_pooling_plate_pbmc.erb' do
   has_a_working_api
 
   let(:ancestor_plate_barcode) { 'ANCESTOR_PLATE' }
-  let(:ancestor_well_a1) { create(:v2_well, plate_barcode: ancestor_plate_barcode, location: 'A1') }
-  let(:ancestor_well_b1) { create(:v2_well, plate_barcode: ancestor_plate_barcode, location: 'B1') }
-  let(:ancestor_well_c1) { create(:v2_well, plate_barcode: ancestor_plate_barcode, location: 'C1') }
+  let(:live_cell_count_a1) { create(:qc_result, key: 'live_cell_count', value: '1000000', units: 'cells/ml') }
+  let(:live_cell_count_b1) { create(:qc_result, key: 'live_cell_count', value: '1250000', units: 'cells/ml') }
+  let(:live_cell_count_c1) { create(:qc_result, key: 'live_cell_count', value: '1850000', units: 'cells/ml') }
+  let(:ancestor_well_a1) { create(:v2_well, plate_barcode: ancestor_plate_barcode, location: 'A1', qc_results: [live_cell_count_a1]) }
+  let(:ancestor_well_b1) { create(:v2_well, plate_barcode: ancestor_plate_barcode, location: 'B1', qc_results: [live_cell_count_b1]) }
+  let(:ancestor_well_c1) { create(:v2_well, plate_barcode: ancestor_plate_barcode, location: 'C1', qc_results: [live_cell_count_c1]) }
   let(:ancestor_labware) { create(:v2_plate, wells: [ancestor_well_a1, ancestor_well_b1, ancestor_well_c1], pool_sizes: [1, 1, 1]) }
 
   let(:transfer_request_from_a1) do
@@ -57,9 +60,9 @@ RSpec.describe 'exports/hamilton_pooling_plate_pbmc.erb' do
   let(:expected_content) do
     [
       %w[SourcePlate SourceWell DestinationPlate DestinationWell SampleVolume ResuspensionVolume],
-      [ancestor_plate_barcode, 'A1', labware.labware_barcode.human, 'A1', '1', '6.25'],
-      [ancestor_plate_barcode, 'B1', labware.labware_barcode.human, 'A1', '2', '6.25'],
-      [ancestor_plate_barcode, 'C1', labware.labware_barcode.human, 'B1', '3', '3.125']
+      [ancestor_plate_barcode, 'A1', labware.labware_barcode.human, 'A1', '20', '6.25'],
+      [ancestor_plate_barcode, 'B1', labware.labware_barcode.human, 'A1', '16', '6.25'],
+      [ancestor_plate_barcode, 'C1', labware.labware_barcode.human, 'B1', '11', '3.125'] # rounded from 10.8
     ]
   end
 
