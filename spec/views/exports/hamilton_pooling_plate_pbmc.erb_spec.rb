f1820940dd6ab2f4599d5ab9b9aa6e3b8919c391
Stuart McHattie <sm49@sanger.ac.uk>
2021-09-17 17:54:10 +0100

    Add tests for missing qc_results

--- spec/views/exports/hamilton_pooling_plate_pbmc.erb_spec.rb
+++ spec/views/exports/hamilton_pooling_plate_pbmc.erb_spec.rb
@@ -6,10 +6,11 @@ RSpec.describe 'exports/hamilton_pooling_plate_pbmc.erb' do
   has_a_working_api
 
   let(:ancestor_plate_barcode) { 'ANCESTOR_PLATE' }
+  let(:concentration_result) { create(:qc_result_concentration) }
   let(:live_cell_count_a1) { create(:qc_result, key: 'live_cell_count', value: '1000000', units: 'cells/ml') }
   let(:live_cell_count_b1) { create(:qc_result, key: 'live_cell_count', value: '1250000', units: 'cells/ml') }
   let(:live_cell_count_c1) { create(:qc_result, key: 'live_cell_count', value: '1850000', units: 'cells/ml') }
-  let(:ancestor_well_a1) { create(:v2_well, plate_barcode: ancestor_plate_barcode, location: 'A1', qc_results: [live_cell_count_a1]) }
+  let(:ancestor_well_a1) { create(:v2_well, plate_barcode: ancestor_plate_barcode, location: 'A1', qc_results: [live_cell_count_a1, concentration_result]) }
   let(:ancestor_well_b1) { create(:v2_well, plate_barcode: ancestor_plate_barcode, location: 'B1', qc_results: [live_cell_count_b1]) }
   let(:ancestor_well_c1) { create(:v2_well, plate_barcode: ancestor_plate_barcode, location: 'C1', qc_results: [live_cell_count_c1]) }
   let(:ancestor_labware) { create(:v2_plate, wells: [ancestor_well_a1, ancestor_well_b1, ancestor_well_c1], pool_sizes: [1, 1, 1]) }
@@ -33,6 +34,7 @@ RSpec.describe 'exports/hamilton_pooling_plate_pbmc.erb' do
   end
   let(:labware) { create(:v2_plate, wells: [well_a1, well_b1], pool_sizes: [2, 1]) }
 
+
   before do
     assign(:ancestor_plate, ancestor_labware)
     assign(:plate, labware)
@@ -49,5 +51,29 @@ RSpec.describe 'exports/hamilton_pooling_plate_pbmc.erb' do
 
   it 'renders the expected content' do
     expect(CSV.parse(render)).to eq(expected_content)
+  end
+
+  it 'removes entries with no qc results' do
+    ancestor_labware.wells_in_columns[0].qc_results = []
+
+    expected = [
+      %w[SourcePlate SourceWell DestinationPlate DestinationWell SampleVolume ResuspensionVolume],
+      [ancestor_plate_barcode, 'B1', labware.labware_barcode.human, 'A1', '16', '3.125'],
+      [ancestor_plate_barcode, 'C1', labware.labware_barcode.human, 'B1', '11', '3.125']
+    ]
+
+    expect(CSV.parse(render)).to eq(expected)
+  end
+
+  it 'removes entries with no cell count results' do
+    ancestor_labware.wells_in_columns[0].qc_results.shift
+
+    expected = [
+      %w[SourcePlate SourceWell DestinationPlate DestinationWell SampleVolume ResuspensionVolume],
+      [ancestor_plate_barcode, 'B1', labware.labware_barcode.human, 'A1', '16', '3.125'],
+      [ancestor_plate_barcode, 'C1', labware.labware_barcode.human, 'B1', '11', '3.125']
+    ]
+
+    expect(CSV.parse(render)).to eq(expected)
   end
 end
