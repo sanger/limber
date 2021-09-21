5917a0cd23a48e12b3cbb485b22585ac4d997333
Katy Taylor <kt17@sanger.ac.uk>
2021-09-21 14:05:03 +0100

    Get rid of this commented out code - there will be no requests coming out of the tubes, so it doesn't make sense - we are transferring all the aliquots from the tubes regardless of whether they have requests or not (expecting one aliquot per tube)

--- app/javascript/shared/requestHelpers.js
+++ app/javascript/shared/requestHelpers.js
@@ -1,5 +1,4 @@
 import { requestsForWell } from './wellHelpers'
-// import { requestsForTube } from './tubeHelpers'
 
 const requestIsActive = function(request) {
   return request.state !== 'passed' &&
@@ -23,18 +22,4 @@ const requestsFromPlates = function(plateObjs) {
   return requestsArray
 }
 
-// const requestsFromTubes = function(tubeObjs) {
-//   const requestsArray = []
-//   for (let p = 0; p < tubeObjs.length; p++) {
-//     const tubeObj = tubeObjs[p]
-//     const tube = tubeObj.tube
-//     const requests = requestsForTube(tube)
-//     for (let r = 0; r < requests.length; r++) {
-//       requestsArray.push({ request: requests[r], tube: tube, tubeObj: tubeObj })
-//     }
-//   }
-//   return requestsArray
-// }
-
-// export { requestIsActive, requestsFromPlates, requestsFromTubes }
 export { requestIsActive, requestsFromPlates }
