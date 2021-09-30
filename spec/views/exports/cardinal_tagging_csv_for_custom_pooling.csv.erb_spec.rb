ec88890cc4fa786b10c07c90e18f1ebf9d951332
Stuart McHattie <sm49@sanger.ac.uk>
2021-09-30 10:46:17 +0100

    Refine tests for CSV expectations
    in different aliquot quantities and tag situations

--- spec/views/exports/cardinal_tagging_csv_for_custom_pooling.csv.erb_spec.rb
+++ spec/views/exports/cardinal_tagging_csv_for_custom_pooling.csv.erb_spec.rb
@@ -40,6 +40,25 @@ RSpec.describe 'exports/cardinal_tagging_csv_for_custom_pooling.csv.erb' do
     expect(get_column(parsed_csv, 5)).to eq([well_a1.aliquots[0].tag2_index.to_s, well_b1.aliquots[0].tag2_index.to_s])
   end
 
+  context "well a1's aliquot has no tags" do
+    let(:aliquot) { create(:v2_aliquot) }
+    let(:well_a1) { create(:v2_well, position: { 'name' => 'A1' }, aliquots: [aliquot]) }
+
+    it "gives nil values for a1's tag indices" do
+      parsed_csv = CSV.parse(render)
+      expect(parsed_csv.size).to eq 3
+      expect(parsed_csv[0]).to eq expected_headers
+
+      barcode = labware.labware_barcode.human
+      expect(get_column(parsed_csv, 0)).to eq(["#{barcode}:A1", "#{barcode}:B1"])
+      expect(get_column(parsed_csv, 1)).to eq([nil, nil])
+      expect(get_column(parsed_csv, 2)).to eq([nil, nil])
+      expect(get_column(parsed_csv, 3)).to all(satisfy { |val| val.match(/^\d+$/) })
+      expect(get_column(parsed_csv, 4)).to eq([nil, well_b1.aliquots[0].tag_index.to_s])
+      expect(get_column(parsed_csv, 5)).to eq([nil, well_b1.aliquots[0].tag2_index.to_s])
+    end
+  end
+
   context 'well a1 is empty' do
     let(:well_a1) { create(:v2_well, position: { 'name' => 'A1' }, aliquot_count: 0) }
 
@@ -57,10 +76,49 @@ RSpec.describe 'exports/cardinal_tagging_csv_for_custom_pooling.csv.erb' do
     end
   end
 
-  context 'well a1 has more than 1 aliquot' do
-    let(:well_a1) { create(:v2_well, position: { 'name' => 'A1' }, aliquot_count: 2) }
+  context 'well a1 has more than 1 aliquot but with matching tag indices' do
+    let(:well_a1) { create(:v2_well, position: { 'name' => 'A1' }, aliquot_count: 2, aliquot_factory: :v2_tagged_aliquot) }
 
-    it 'skips wells with more than 1 aliquot' do
+    it 'includes all wells, still' do
+      parsed_csv = CSV.parse(render)
+      expect(parsed_csv.size).to eq 3
+      expect(parsed_csv[0]).to eq expected_headers
+
+      barcode = labware.labware_barcode.human
+      expect(get_column(parsed_csv, 0)).to eq(["#{barcode}:A1", "#{barcode}:B1"])
+      expect(get_column(parsed_csv, 1)).to eq([nil, nil])
+      expect(get_column(parsed_csv, 2)).to eq([nil, nil])
+      expect(get_column(parsed_csv, 3)).to all(satisfy { |val| val.match(/^\d+$/) })
+      expect(get_column(parsed_csv, 4)).to eq([well_a1.aliquots[0].tag_index.to_s, well_b1.aliquots[0].tag_index.to_s])
+      expect(get_column(parsed_csv, 5)).to eq([well_a1.aliquots[0].tag2_index.to_s, well_b1.aliquots[0].tag2_index.to_s])
+    end
+  end
+
+  context 'well a1 has more than 1 aliquot with different tag_index values' do
+    let(:aliquot_1) { create(:v2_aliquot, tag_oligo: 'CAT', tag_index: 5, tag2_oligo: 'GAG', tag2_index: 10) }
+    let(:aliquot_2) { create(:v2_aliquot, tag_oligo: 'TAT', tag_index: 7, tag2_oligo: 'GAG', tag2_index: 10) }
+    let(:well_a1) { create(:v2_well, position: { 'name' => 'A1' }, aliquots: [aliquot_1, aliquot_2]) }
+
+    it 'does not include well a1 in the CSV file' do
+      parsed_csv = CSV.parse(render)
+      expect(parsed_csv.size).to eq 2
+      expect(parsed_csv[0]).to eq expected_headers
+
+      expect(get_column(parsed_csv, 0)).to eq(["#{labware.labware_barcode.human}:B1"])
+      expect(get_column(parsed_csv, 1)).to eq([nil])
+      expect(get_column(parsed_csv, 2)).to eq([nil])
+      expect(get_column(parsed_csv, 3)).to all(satisfy { |val| val.match(/^\d+$/) })
+      expect(get_column(parsed_csv, 4)).to eq([well_b1.aliquots[0].tag_index.to_s])
+      expect(get_column(parsed_csv, 5)).to eq([well_b1.aliquots[0].tag2_index.to_s])
+    end
+  end
+
+  context 'well a1 has more than 1 aliquot with different tag2_index values' do
+    let(:aliquot_1) { create(:v2_aliquot, tag_oligo: 'CAT', tag_index: 5, tag2_oligo: 'GAG', tag2_index: 10) }
+    let(:aliquot_2) { create(:v2_aliquot, tag_oligo: 'CAT', tag_index: 5, tag2_oligo: 'TAC', tag2_index: 8) }
+    let(:well_a1) { create(:v2_well, position: { 'name' => 'A1' }, aliquots: [aliquot_1, aliquot_2]) }
+
+    it 'does not include well a1 in the CSV file' do
       parsed_csv = CSV.parse(render)
       expect(parsed_csv.size).to eq 2
       expect(parsed_csv[0]).to eq expected_headers
