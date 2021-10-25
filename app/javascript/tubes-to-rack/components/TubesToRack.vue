67801fc0e0fef40a1c3e246610bb6dcc36c2c16d
Stuart McHattie <sm49@sanger.ac.uk>
2021-10-25 12:35:41 +0100

    Add JavaScript files for TubesToRack

---
+++ app/javascript/tubes-to-rack/components/TubesToRack.vue
@@ -0,0 +1,19 @@
+<template>
+  <lb-page>
+    <lb-loading-modal
+      v-if="loading"
+      :message="progressMessage"
+    />
+    <lb-main-content>
+      <b-h1>Hello</b-h1>
+    </lb-main-content>
+    <lb-sidebar>
+    </lb-sidebar>
+  </lb-page>
+</template>
+
+<script>
+export default {
+  name: 'TubesToRack'
+}
+</script>
