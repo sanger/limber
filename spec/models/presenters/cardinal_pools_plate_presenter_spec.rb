6c9414aa1f9ed9946524a80ad43f35cbbfbf4be9
Harriet Craven <hc6@sanger.ac.uk>
2021-10-26 16:21:12 +0100

    remove CardinalPoolsPlatePresenter

--- spec/models/presenters/cardinal_pools_plate_presenter_spec.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require 'rails_helper'
-# require 'presenters/concentration_binned_plate_presenter'
-require_relative 'shared_labware_presenter_examples'
-
-RSpec.describe Presenters::CardinalPoolsPlatePresenter, cardinal: true do
-  has_a_working_api
-end
