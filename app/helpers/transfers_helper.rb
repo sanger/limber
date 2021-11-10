41756dca83cf2ce6efb0f2d3342fd9d139ca1c8f
Stuart McHattie <sm49@sanger.ac.uk>
2021-11-10 16:49:36 +0000

    Stub a method for the pick volumes calculation

---
+++ app/helpers/transfers_helper.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+# Helper methods for calculating volumes and concentrations in sample transfers.
+module TransfersHelper
+  # Calculates the pick volume and the buffer volume to reach a target molarity
+  # and volume given a source molarity.
+  #
+  # Params:
+  #  - target_molarity:  The target molarity in the same units as source_molarity.
+  #  - target_volume:    The intended volume after the transfer is complete.
+  #  - source_molarity:  The source molarity of the sample being transfered.
+  #                      The units should match those in the target_molarity.
+  #  - minimum_pick:     The minimum pick possible for both the sample and the buffer.
+  #                      Units should match the target_volume.
+  #
+  # Returns:
+  #  - A hash containing keys :pick_volume and :buffer_volume for the transfer.
+  #    Volumes in the return hash use the same units as the target_volume parameter.
+  def calculate_pick_volumes(target_molarity, target_volume, source_molarity, minimum_pick)
+    return { pick_volume: 0, buffer_volume: 0 }
+  end
+end
