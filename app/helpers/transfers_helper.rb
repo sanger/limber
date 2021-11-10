55a06fbc00cfd241805cf8573e90aa9c86c1970b
Stuart McHattie <sm49@sanger.ac.uk>
2021-11-10 17:38:11 +0000

    Create tests for calculation method

--- app/helpers/transfers_helper.rb
+++ app/helpers/transfers_helper.rb
@@ -2,7 +2,7 @@
 
 # Helper methods for calculating volumes and concentrations in sample transfers.
 module TransfersHelper
-  # Calculates the pick volume and the buffer volume to reach a target molarity
+  # Calculates the source volume and the buffer volume to reach a target molarity
   # and volume given a source molarity.
   #
   # Params:
@@ -10,21 +10,21 @@ module TransfersHelper
   #  - target_volume:    The intended volume after the transfer is complete.
   #  - source_molarity:  The source molarity of the sample being transfered.
   #                      The units should match those in the target_molarity.
-  #  - minimum_pick:     The minimum pick possible for both the sample and the buffer.
-  #                      Units should match the target_volume.
+  #  - minimum_pick:     The minimum pick volume possible for both the sample
+  #                      and the buffer. Units should match the target_volume.
   #
   # Returns:
-  #  - A hash containing keys :pick_volume and :buffer_volume for the transfer.
+  #  - A hash containing keys :sample_volume and :buffer_volume for the transfer.
   #    Volumes in the return hash use the same units as the target_volume parameter.
   def calculate_pick_volumes(target_molarity, target_volume, source_molarity, minimum_pick)
-    pick_volume = (target_molarity / source_molarity) * target_volume
-    pick_volume = [pick_volume, minimum_pick].max
-    buffer_volume = target_volume - pick_volume
+    sample_volume = (target_molarity.to_f / source_molarity) * target_volume
+    sample_volume = [sample_volume, minimum_pick].max
+    buffer_volume = target_volume - sample_volume
 
     if buffer_volume < minimum_pick
-      { pick_volume: target_volume, buffer_volume: 0 }
+      { sample_volume: target_volume, buffer_volume: 0 }
     else
-      { pick_volume: pick_volume, buffer_volume: buffer_volume }
+      { sample_volume: sample_volume, buffer_volume: buffer_volume }
     end
   end
 end
