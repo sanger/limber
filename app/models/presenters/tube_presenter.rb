5079c0eeddd9bb359e81f748d990c9b884401d2f
Stuart McHattie <sm49@sanger.ac.uk>
2021-11-10 15:33:33 +0000

    Fix linting issues

--- app/models/presenters/tube_presenter.rb
+++ app/models/presenters/tube_presenter.rb
@@ -62,14 +62,12 @@ module Presenters
     end
 
     def qc_summary
-      labware.receptacle.qc_results.sort_by { |result| result.key }.each do |result|
+      labware.receptacle.qc_results.sort_by(&:key).each do |result|
         yield result.key.titleize, result.unit_value
       end
       nil
     end
 
-    def qc_footer
-
-    end
+    def qc_footer; end
   end
 end
