0e1c927ebcbcb2ae95a2e58cbd972d6032625359
Andrew Sparkes <as28@sanger.ac.uk>
2021-11-15 15:16:00 +0000

    Added find by method to Sample api resource

--- app/sequencescape/sequencescape/api/v2/sample.rb
+++ app/sequencescape/sequencescape/api/v2/sample.rb
@@ -3,4 +3,10 @@
 # Represents a sample in Limber via the Sequencescape API
 class Sequencescape::Api::V2::Sample < Sequencescape::Api::V2::Base
   has_one :sample_metadata, class_name: 'Sequencescape::Api::V2::SampleMetadata'
+
+  DEFAULT_INCLUDES = [].freeze
+
+  def self.find_by(options, includes: DEFAULT_INCLUDES)
+    Sequencescape::Api::V2::Sample.includes(*includes).find(options).first
+  end
 end
