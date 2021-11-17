8d4e7541d9970450d8c4f8096d8c7c6f331c816d
Stuart McHattie <sm49@sanger.ac.uk>
2021-11-17 12:23:16 +0000

    Convert method documentation into YARD friendly tags

--- app/helpers/transfers_helper.rb
+++ app/helpers/transfers_helper.rb
@@ -5,17 +5,15 @@ module TransfersHelper
   # Calculates the source volume and the buffer volume to reach a target molarity
   # and volume given a source molarity.
   #
-  # Params:
-  #  - target_molarity:  The target molarity in the same units as source_molarity.
-  #  - target_volume:    The intended volume after the transfer is complete.
-  #  - source_molarity:  The source molarity of the sample being transfered.
-  #                      The units should match those in the target_molarity.
-  #  - minimum_pick:     The minimum pick volume possible for both the sample
-  #                      and the buffer. Units should match the target_volume.
+  # @param target_molarity [Float] The target molarity in the same units as +source_molarity+.
+  # @param target_volume [Float] The intended volume after the transfer is complete.
+  # @param source_molarity [Float] The source molarity of the sample being transfered.
+  #        The units should match those in the +target_molarity+.
+  # @param minimum_pick [Float] The minimum pick volume possible for both the sample and the buffer.
+  #        Units should match the +target_volume+.
   #
-  # Returns:
-  #  - A hash containing keys :sample_volume and :buffer_volume for the transfer.
-  #    Volumes in the return hash use the same units as the target_volume parameter.
+  # @return [Hash] A hash containing keys +:sample_volume+ and +:buffer_volume+ for the transfer.
+  #         Volumes in the return hash use the same units as the +target_volume+ parameter.
   def calculate_pick_volumes(target_molarity:, target_volume:, source_molarity:, minimum_pick:)
     sample_volume = (target_molarity.to_f / source_molarity) * target_volume
     sample_volume = [sample_volume, minimum_pick].max
