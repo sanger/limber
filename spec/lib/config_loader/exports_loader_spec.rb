64d0a5ad4d034fc8e4f9d8278a35b17b273438bf
Andrew Sparkes <as28@sanger.ac.uk>
2021-12-13 16:48:01 +0000

    Added targeted nanoseq export files

--- spec/lib/config_loader/exports_loader_spec.rb
+++ spec/lib/config_loader/exports_loader_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe ConfigLoader::ExportsLoader, type: :model, loader: true do
     let(:selected_files) { nil }
 
     it 'loads purposes from all files' do
-      expect(loader.config.length).to eq 21
+      expect(loader.config.length).to eq 25
       expect(loader.config).to be_a(Hash)
     end
   end
@@ -23,7 +23,7 @@ RSpec.describe ConfigLoader::ExportsLoader, type: :model, loader: true do
     let(:selected_files) { 'exports' }
 
     it 'loads purposes from specified files' do
-      expect(loader.config.length).to eq 21
+      expect(loader.config.length).to eq 25
       expect(loader.config).to be_a(Hash)
     end
   end
