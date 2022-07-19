482a3d9d4f1c0cc0ce0017044cfb808c82274270
Andrew Sparkes <as28@sanger.ac.uk>
2022-07-19 17:06:38 +0100

    Refactoring of creator to use selective submission of subset of samples based on customer file contents

--- app/models/labware_creators/pcr_cycles_binned_plate.rb
+++ app/models/labware_creators/pcr_cycles_binned_plate.rb
@@ -4,11 +4,13 @@ module LabwareCreators
   # Handles the generation of a plate with wells binned according to the number of
   # PCR cycles that has been determined by the customer.
   # Uploads a file supplied by the customer that has a row per each well and
-  # includes Sample Volume, Diluent Volume, PCR Cycles, Sub-Pool and Coverage columns.
+  # includes Sample Volume, Diluent Volume, PCR Cycles, Sub-Pool, Coverage and Hyb Panel columns.
   # Uses the PCR Cycles column to determine the binning arrangement of the wells,
   # and the Sample Volume and Diluent Volume columns in the well transfers.
-  # Sub-Pool and Coverage need to be stored for a later step downstream in the pipeline,
-  # at the point where custom pooling is performed.
+  # Rows with a Sample Volume of zero mean the customer does not want that sample
+  # to go forward at this time, and it should not be transferred into the dilution plate.
+  # Sub-Pool, Coverage and Hyb Panel need to be stored for a later step downstream in the
+  # pipeline, at the point where custom pooling is performed.
   # Wells in the bins are applied to the destination by column order.
   # If there is enough space on the destination plate each new bin will start in a new
   # column. Otherwise bins will run consecutively without gaps.
@@ -27,15 +29,15 @@ module LabwareCreators
   # |E1| pcr_cycles = 12 (bin 2)  |  |  |  |
   # +--+--+--~                    +--+--+--~
   # |G1| pcr_cycles = 12 (bin 2)  |  |  |  |
-  class PcrCyclesBinnedPlate < StampedPlate
+  class PcrCyclesBinnedPlate < Base
     include LabwareCreators::CustomPage
+    include SupportParent::PlateOnly
+    self.default_transfer_template_name = 'Custom pooling'
 
     MISSING_WELL_DETAIL = 'is missing a row for well %s, all wells with content must have a row in the uploaded file.'
-    PENDING_WELL =
-      # rubocop:todo Layout/LineLength
-      'contains at least one pending well %s, the plate and all wells in it should be passed before creating the child plate.'
-
-    # rubocop:enable Layout/LineLength
+    PENDING_WELL = 'contains at least one pending well %s, the plate and all wells in it '\
+      'should be passed before creating the child plate.'
+    NO_WELLS_TO_TRANSFER = 'has no well rows suitable for transfer (check sample volumes)'
 
     self.page = 'pcr_cycles_binned_plate'
     self.attributes += [:file]
@@ -43,16 +45,17 @@ module LabwareCreators
     attr_accessor :file
 
     # delegate method to return well values to csv file handler class
-    delegate :well_details, to: :csv_file
+    delegate :well_details, :skipped_wells, to: :csv_file
 
     validates :file, presence: true
     validates_nested :csv_file, if: :file
     validate :wells_have_required_information?
+    validate :some_wells_are_being_transferred?
 
-    PARENT_PLATE_INCLUDES =
-      'wells.aliquots,wells.qc_results,wells.requests_as_source.request_type,wells.aliquots.request.request_type'
-
+    PARENT_PLATE_INCLUDES = 'wells.aliquots,wells.qc_results,wells.requests_as_source.request_type,'\
+      'wells.aliquots.request.request_type,wells.aliquots.study'
     CHILD_PLATE_INCLUDES = 'wells.aliquots'
+    WELL_METADATA_FIELDS = %w[diluent_volume pcr_cycles submit_for_sequencing sub_pool coverage hyb_panel].freeze
 
     def parent
       @parent ||= Sequencescape::Api::V2.plate_with_custom_includes(PARENT_PLATE_INCLUDES, uuid: parent_uuid)
@@ -62,13 +65,18 @@ module LabwareCreators
       @parent_v1 ||= api.plate.find(parent_uuid)
     end
 
-    # Configurations from the plate purpose.
+    #
+    # csv file configuration from the plate purpose.
+    #
     def csv_file_upload_config
       @csv_file_upload_config ||= purpose_config.fetch(:csv_file_upload)
     end
 
-    def dilutions_config
-      @dilutions_config ||= purpose_config.fetch(:dilutions)
+    #
+    # submission configuration from the plate purpose.
+    #
+    def submission_options
+      @submission_options ||= purpose_config.fetch(:submission_options)
     end
 
     def save
@@ -76,13 +84,15 @@ module LabwareCreators
       super && upload_file && true
     end
 
+    #
+    # Called as part of the 'super' call in the 'save' method
+    # retrieve child plate through v2 api, using uuid got through v1 api
+    #
     def after_transfer!
-      # called as part of the 'super' call in the 'save' method
-      # retrieve child plate through v2 api, using uuid got through v1 api
       child_v2 = Sequencescape::Api::V2.plate_with_custom_includes(CHILD_PLATE_INCLUDES, uuid: child.uuid)
 
       # update fields on each well with various metadata
-      fields_to_update = %w[diluent_volume pcr_cycles submit_for_sequencing sub_pool coverage]
+      fields_to_update = WELL_METADATA_FIELDS
 
       child_wells_by_location = child_v2.wells.index_by(&:location)
 
@@ -94,29 +104,165 @@ module LabwareCreators
       end
     end
 
+    #
+    # Update metadata on a well to pass values to descendants
+    #
     def update_well_with_metadata(well, metadata, fields_to_update)
       options = fields_to_update.index_with { |field| metadata[field] }
       well.update(options)
     end
 
-    def wells_have_required_information?
-      filtered_wells.each do |well|
-        next if well.aliquots.empty?
-
-        errors.add(:csv_file, format(MISSING_WELL_DETAIL, well.location)) unless well_details.include? well.location
-        errors.add(:csv_file, format(PENDING_WELL, well.location)) if well.pending?
-      end
-    end
-
+    #
+    # The dilutions calculator works out how the samples will we rearranged when
+    # transferred into the child plate
+    #
     def dilutions_calculator
       @dilutions_calculator ||= Utility::PcrCyclesBinningCalculator.new(well_details)
     end
 
+    def labware_wells
+      parent.wells
+    end
+
     private
 
-    # Returns the parent wells selected to be taken forward.
-    def filtered_wells
-      well_filter.filtered.each_with_object([]) { |well_filter_details, wells| wells << well_filter_details[0] }
+    #
+    # Well filter handles the selection of wells for transfer based on submission id
+    #
+    def well_filter
+      @well_filter ||= WellFilterBySubmission.new(creator: self, submission_id: @submission.id)
+    end
+
+    #
+    # Validation to check that at least some wells should be transferred
+    # to catch situation where customer file has no valid rows (e.g. all zero sample volume)
+    #
+    def some_wells_are_being_transferred?
+      return true unless well_details.size.zero?
+
+      errors.add(:csv_file, format(NO_WELLS_TO_TRANSFER))
+      false
+    end
+
+    def check_for_well_missing_detail(well)
+      return if well_details.include? well.location
+      errors.add(:csv_file, format(MISSING_WELL_DETAIL, well.location))
+    end
+
+    def check_for_well_pending(well)
+      return unless well.pending?
+      errors.add(:csv_file, format(PENDING_WELL, well.location))
+    end
+
+    #
+    # Validation to check wells have the required information and are in the right state
+    #
+    def wells_have_required_information?
+      labware_wells.each do |well|
+        # skip validation on wells empty or chosen not to go forward (those where user has set zero sample vol)
+        next if well.aliquots.empty? || skipped_wells.include?(well.location)
+
+        check_for_well_missing_detail(well)
+        check_for_well_pending(well)
+      end
+    end
+
+    #
+    # Creates and builds the submission, creates the child plate labware and
+    # transfers samples from the parent plate to the child plate
+    #
+    def create_labware!
+      create_and_build_submission
+      return if errors.size.positive?
+
+      plate_creation = create_plate_from_parent!
+      @child = plate_creation.child
+
+      child_v2 = Sequencescape::Api::V2.plate_with_wells(@child.uuid)
+
+      transfer_material_from_parent!(child_v2)
+
+      yield(@child) if block_given?
+      after_transfer!
+      true
+    end
+
+    #
+    # Creates and builds the submission for the dilution and cleanup step
+    #
+    def create_and_build_submission
+      submission_created = create_submission_from_parent_plate
+      unless submission_created
+        errors.add(:base, 'Failed to create submission')
+        return
+      end
+
+      errors.add(:base, 'Failed to build submission') unless submission_built?
+    end
+
+    #
+    # Checks that the submission has been built, waiting if not
+    #
+    def submission_built?
+      counter = 1
+      while counter <= 6
+        @submission = Sequencescape::Api::V2::Submission.where(uuid: @submission_uuid).first
+        return true unless @submission.building_in_progress?
+
+        sleep(5)
+        counter += 1
+      end
+      false
+    end
+
+    #
+    # Fetches the dilution and cleanup submission options from the purpose
+    # configuration and calls the method to create the submission
+    #
+    def create_submission_from_parent_plate
+      # there should only be one submission option
+      if submission_options.count > 1
+        errors.add(:base, 'Expected only one submission')
+        return
+      end
+
+      # create a submission with params specified in the config
+      configured_params = submission_options.values.first
+
+      create_submission(configured_params)
+    end
+
+    #
+    # Fetch the parent well uuids for those wells being transferred
+    #
+    def parent_asset_uuids
+      labware_wells.filter_map do |well|
+        well.uuid unless well.empty? || skipped_wells.include?(well.position['name'])
+      end
+    end
+
+    #
+    # Creates the dilution and cleanup submission
+    #
+    def create_submission(configured_params)
+      sequencescape_submission_parameters = {
+        template_name: configured_params[:template_name],
+        request_options: configured_params[:request_options],
+        asset_groups: [{ assets: parent_asset_uuids, autodetect_studies_projects: true }],
+        api: api,
+        user: user_uuid
+      }
+
+      ss = SequencescapeSubmission.new(sequencescape_submission_parameters)
+      submission_created = ss.save
+
+      if submission_created
+        @submission_uuid = ss.submission_uuid
+        return true
+      end
+
+      errors.add(:base, ss.errors.full_messages)
+      false
     end
 
     #
@@ -126,12 +272,48 @@ module LabwareCreators
       parent_v1.qc_files.create_from_file!(file, 'pcr_cycles_binned_plate_customer_file.csv')
     end
 
-    # Create class that will parse and validate the uploaded file
+    #
+    # Create class that will parse and validate the uploaded customer csv file
+    #
     def csv_file
       @csv_file ||= CsvFile.new(file, csv_file_upload_config, parent.human_barcode)
     end
 
-    # Override this method in sub-class if required.
+    #
+    # Call the api to create the transfer request collection
+    #
+    def transfer_material_from_parent!(child_plate)
+      api.transfer_request_collection.create!(
+        user: user_uuid,
+        transfer_requests: transfer_request_attributes(child_plate)
+      )
+    end
+
+    #
+    # Generates the transfer request attributes
+    # e.g [
+    #   {
+    #     'source_asset': 'auuid',
+    #     'target_asset': 'anotheruuid',
+    #     'volume': '5.2'
+    #   },
+    #   { etc. }
+    # ]
+    #
+    def transfer_request_attributes(child_plate)
+      well_filter.filtered.filter_map do |well, additional_parameters|
+        request_hash(well, child_plate, additional_parameters)
+      end
+    end
+
+    #
+    # Generates a request hash for a specific well
+    # e.g {
+    #   'source_asset': 'auuid',
+    #   'target_asset': 'anotheruuid',
+    #   'volume': '5.2'
+    # }
+    #
     def request_hash(source_well, child_plate, additional_parameters)
       {
         'source_asset' => source_well.uuid,
@@ -144,7 +326,21 @@ module LabwareCreators
       }.merge(additional_parameters)
     end
 
+    #
     # Uses the calculator to generate the hash of transfers to be performed on the parent plate
+    # e.g.
+    # {
+    #   'A1' => {
+    #     'dest_locn' => 'A1',
+    #     'volume' => '5.0'
+    #   },
+    #   'B1' => {
+    #     'dest_locn' => 'B1',
+    #     'volume' => '5.0'
+    #   },
+    #   etc.
+    # }
+    #
     def transfer_hash
       @transfer_hash ||= dilutions_calculator.compute_well_transfers(parent)
     end
--- spec/models/labware_creators/pcr_cycles_binned_plate_spec.rb
+++ spec/models/labware_creators/pcr_cycles_binned_plate_spec.rb
@@ -13,6 +13,33 @@ RSpec.describe LabwareCreators::PcrCyclesBinnedPlate, with: :uploader do
     expect(described_class.page).to eq 'pcr_cycles_binned_plate'
   end
 
+  # user
+  let(:user_uuid) { 'user-uuid' }
+
+  # dilution cleanup submission setup
+  let(:library_type_name) { 'Test Library Type' }
+  let(:submission_uuid) { 'sub-uuid' }
+  let(:submission_for_cleanup_id) { '1' }
+  let(:submission_for_cleanup) do
+    create :v2_submission,
+    id: submission_for_cleanup_id,
+    uuid: submission_uuid
+  end
+
+  let(:requests_for_cleanup) do
+    Array.new(10) do |i|
+      create :library_request,
+      state: 'pending',
+      uuid: "request-#{i}",
+      library_type: library_type_name,
+      submission_id: submission_for_cleanup_id,
+      submission: submission_for_cleanup
+    end
+  end
+
+  let(:example_submission_template_uuid) { SecureRandom.uuid }
+
+  # parent
   let(:parent_uuid) { 'example-plate-uuid' }
   let(:plate_size) { 96 }
 
@@ -21,7 +48,7 @@ RSpec.describe LabwareCreators::PcrCyclesBinnedPlate, with: :uploader do
       :v2_well,
       location: 'A1',
       qc_results: create_list(:qc_result_concentration, 1, value: 1.0),
-      requests_as_source: [requests[0]],
+      requests_as_source: [requests_for_cleanup[0]],
       outer_request: nil
     )
   end
@@ -30,7 +57,7 @@ RSpec.describe LabwareCreators::PcrCyclesBinnedPlate, with: :uploader do
       :v2_well,
       location: 'B1',
       qc_results: create_list(:qc_result_concentration, 1, value: 1.0),
-      requests_as_source: [requests[1]],
+      requests_as_source: [requests_for_cleanup[1]],
       outer_request: nil
     )
   end
@@ -39,7 +66,7 @@ RSpec.describe LabwareCreators::PcrCyclesBinnedPlate, with: :uploader do
       :v2_well,
       location: 'D1',
       qc_results: create_list(:qc_result_concentration, 1, value: 1.0),
-      requests_as_source: [requests[2]],
+      requests_as_source: [requests_for_cleanup[2]],
       outer_request: nil
     )
   end
@@ -48,7 +75,7 @@ RSpec.describe LabwareCreators::PcrCyclesBinnedPlate, with: :uploader do
       :v2_well,
       location: 'E1',
       qc_results: create_list(:qc_result_concentration, 1, value: 1.0),
-      requests_as_source: [requests[3]],
+      requests_as_source: [],
       outer_request: nil
     )
   end
@@ -57,7 +84,7 @@ RSpec.describe LabwareCreators::PcrCyclesBinnedPlate, with: :uploader do
       :v2_well,
       location: 'F1',
       qc_results: create_list(:qc_result_concentration, 1, value: 1.0),
-      requests_as_source: [requests[4]],
+      requests_as_source: [requests_for_cleanup[3]],
       outer_request: nil
     )
   end
@@ -66,7 +93,7 @@ RSpec.describe LabwareCreators::PcrCyclesBinnedPlate, with: :uploader do
       :v2_well,
       location: 'H1',
       qc_results: create_list(:qc_result_concentration, 1, value: 1.0),
-      requests_as_source: [requests[5]],
+      requests_as_source: [requests_for_cleanup[4]],
       outer_request: nil
     )
   end
@@ -75,7 +102,7 @@ RSpec.describe LabwareCreators::PcrCyclesBinnedPlate, with: :uploader do
       :v2_well,
       location: 'A2',
       qc_results: create_list(:qc_result_concentration, 1, value: 1.0),
-      requests_as_source: [requests[6]],
+      requests_as_source: [requests_for_cleanup[5]],
       outer_request: nil
     )
   end
@@ -84,7 +111,7 @@ RSpec.describe LabwareCreators::PcrCyclesBinnedPlate, with: :uploader do
       :v2_well,
       location: 'B2',
       qc_results: create_list(:qc_result_concentration, 1, value: 1.0),
-      requests_as_source: [requests[7]],
+      requests_as_source: [],
       outer_request: nil
     )
   end
@@ -93,7 +120,7 @@ RSpec.describe LabwareCreators::PcrCyclesBinnedPlate, with: :uploader do
       :v2_well,
       location: 'C2',
       qc_results: create_list(:qc_result_concentration, 1, value: 1.0),
-      requests_as_source: [requests[8]],
+      requests_as_source: [requests_for_cleanup[6]],
       outer_request: nil
     )
   end
@@ -102,7 +129,7 @@ RSpec.describe LabwareCreators::PcrCyclesBinnedPlate, with: :uploader do
       :v2_well,
       location: 'D2',
       qc_results: create_list(:qc_result_concentration, 1, value: 1.0),
-      requests_as_source: [requests[9]],
+      requests_as_source: [requests_for_cleanup[7]],
       outer_request: nil
     )
   end
@@ -111,7 +138,7 @@ RSpec.describe LabwareCreators::PcrCyclesBinnedPlate, with: :uploader do
       :v2_well,
       location: 'E2',
       qc_results: create_list(:qc_result_concentration, 1, value: 1.0),
-      requests_as_source: [requests[10]],
+      requests_as_source: [],
       outer_request: nil
     )
   end
@@ -120,7 +147,7 @@ RSpec.describe LabwareCreators::PcrCyclesBinnedPlate, with: :uploader do
       :v2_well,
       location: 'F2',
       qc_results: create_list(:qc_result_concentration, 1, value: 1.0),
-      requests_as_source: [requests[11]],
+      requests_as_source: [],
       outer_request: nil
     )
   end
@@ -129,7 +156,7 @@ RSpec.describe LabwareCreators::PcrCyclesBinnedPlate, with: :uploader do
       :v2_well,
       location: 'G2',
       qc_results: create_list(:qc_result_concentration, 1, value: 1.0),
-      requests_as_source: [requests[12]],
+      requests_as_source: [requests_for_cleanup[8]],
       outer_request: nil
     )
   end
@@ -138,7 +165,7 @@ RSpec.describe LabwareCreators::PcrCyclesBinnedPlate, with: :uploader do
       :v2_well,
       location: 'H2',
       qc_results: create_list(:qc_result_concentration, 1, value: 1.0),
-      requests_as_source: [requests[13]],
+      requests_as_source: [requests_for_cleanup[9]],
       outer_request: nil
     )
   end
@@ -164,27 +191,39 @@ RSpec.describe LabwareCreators::PcrCyclesBinnedPlate, with: :uploader do
              well_g2,
              well_h2
            ],
-           outer_requests: requests
+           outer_requests: requests_for_cleanup
   end
 
-  let(:parent_plate_v1) { json :plate, uuid: parent_uuid, stock_plate_barcode: 2, qc_files_actions: %w[read create] }
-
-  let(:child_plate) do
-    create :v2_plate, uuid: 'child-uuid', barcode_number: '3', size: plate_size, outer_requests: requests
+  let(:parent_plate_v1) do
+    json :plate,
+    uuid: parent_uuid,
+    stock_plate_barcode: 2,
+    qc_files_actions: %w[read create]
   end
 
-  let(:library_type_name) { 'Test Library Type' }
-
-  let(:requests) do
-    Array.new(10) do |i|
-      create :library_request, state: 'pending', uuid: "request-#{i}", library_type: library_type_name
+  let(:expected_skipped_wells) { %w[E1 B2 E2 F2] }
+  let(:filtered_parent_asset_uuids) do
+    parent_plate.wells.filter_map do |well|
+      well.uuid unless expected_skipped_wells.include?(well.position['name'])
     end
   end
 
+  # child
+  let(:child_uuid) { 'child-uuid' }
+  let(:child_v2_plate) do
+    create :v2_plate,
+    uuid: child_uuid,
+    barcode_number: '3',
+    size: plate_size,
+    outer_requests: requests_for_cleanup
+  end
+
+  # purpose config
   let(:child_purpose_uuid) { 'child-purpose' }
   let(:child_purpose_name) { 'Child Purpose' }
+  let!(:purpose_config) do
+ create :pcr_cycles_binned_plate_purpose_config, name: child_purpose_name, uuid: child_purpose_uuid end
 
-  let(:user_uuid) { 'user-uuid' }
 
   context 'on new' do
     has_a_working_api
@@ -227,31 +266,36 @@ RSpec.describe LabwareCreators::PcrCyclesBinnedPlate, with: :uploader do
         )
     end
 
-    let(:stub_parent_request) { stub_api_get(parent_uuid, body: parent_plate_v1) }
+    let(:stub_get_parent_v1) { stub_api_get(parent_uuid, body: parent_plate_v1) }
+
+    let(:bait_library) { create :bait_library, name: 'HybPanel1' }
 
     before do
-      stub_parent_request
+      stub_get_parent_v1
 
-      create :pcr_cycles_binned_plate_customer_csv_file_upload_purpose_config,
-             uuid: child_purpose_uuid,
-             name: child_purpose_name,
-             library_type_name: library_type_name
+      Settings.submission_templates = { 'example' => example_submission_template_uuid }
 
       stub_v2_plate(
         parent_plate,
         stub_search: false,
         custom_includes:
-          'wells.aliquots,wells.qc_results,wells.requests_as_source.request_type,wells.aliquots.request.request_type,wells.aliquots.study'
+          'wells.aliquots,wells.qc_results,wells.requests_as_source.request_type,'\
+          'wells.aliquots.request.request_type,wells.aliquots.study'
       )
-      stub_v2_plate(child_plate, stub_search: false)
+      # this child stub is for after creation of the child plate
+      stub_v2_plate(child_v2_plate, stub_search: false, custom_query: [:plate_with_wells, child_v2_plate.uuid])
 
-      stub_v2_plate(
-        child_plate,
-        stub_search: false,
-        custom_includes: 'wells.aliquots'
-      )
+      # this child stub is for after transfer
+      stub_v2_plate(child_v2_plate, stub_search: false, custom_includes: 'wells.aliquots')
 
       stub_upload_file_creation
+
+      # set up a stub for the hyb panel field lookup (bait library)
+      stub_v2_bait_library(bait_library.name, bait_library)
+
+      allow('Sequencescape::Api::V2::Submission'.constantize).to receive(:where).with(uuid: submission_uuid).and_return(
+        [submission_for_cleanup]
+      )
     end
 
     context 'with an invalid file' do
@@ -286,68 +330,67 @@ RSpec.describe LabwareCreators::PcrCyclesBinnedPlate, with: :uploader do
 
       let!(:api_v2_post) { stub_api_v2_post('Well') }
 
-      # ['E1', 'B2', 'E2', 'F2']
       let(:transfer_requests) do
         [
           {
             'volume' => '5.0',
             'source_asset' => well_a1.uuid,
             'target_asset' => '3-well-A2',
-            'outer_request' => requests[0].uuid
+            'outer_request' => requests_for_cleanup[0].uuid
           },
           {
             'volume' => '5.0',
             'source_asset' => well_b1.uuid,
             'target_asset' => '3-well-B2',
-            'outer_request' => requests[1].uuid
+            'outer_request' => requests_for_cleanup[1].uuid
           },
           {
             'volume' => '5.0',
             'source_asset' => well_d1.uuid,
             'target_asset' => '3-well-A1',
-            'outer_request' => requests[2].uuid
+            'outer_request' => requests_for_cleanup[2].uuid
           },
           {
             'volume' => '4.0',
             'source_asset' => well_f1.uuid,
             'target_asset' => '3-well-A3',
-            'outer_request' => requests[3].uuid
+            'outer_request' => requests_for_cleanup[3].uuid
           },
           {
             'volume' => '5.0',
             'source_asset' => well_h1.uuid,
             'target_asset' => '3-well-B3',
-            'outer_request' => requests[4].uuid
+            'outer_request' => requests_for_cleanup[4].uuid
           },
           {
             'volume' => '3.2',
             'source_asset' => well_a2.uuid,
             'target_asset' => '3-well-C3',
-            'outer_request' => requests[5].uuid
+            'outer_request' => requests_for_cleanup[5].uuid
           },
           {
             'volume' => '5.0',
             'source_asset' => well_c2.uuid,
             'target_asset' => '3-well-D3',
-            'outer_request' => requests[6].uuid
+            'outer_request' => requests_for_cleanup[6].uuid
           },
           {
             'volume' => '5.0',
             'source_asset' => well_d2.uuid,
             'target_asset' => '3-well-E3',
-            'outer_request' => requests[7].uuid
+            'outer_request' => requests_for_cleanup[7].uuid
           },
           {
             'volume' => '5.0',
             'source_asset' => well_g2.uuid,
             'target_asset' => '3-well-C2',
-            'outer_request' => requests[8].uuid
+            'outer_request' => requests_for_cleanup[8].uuid
           },
           {
             'volume' => '3.621',
             'source_asset' => well_h2.uuid,
             'target_asset' => '3-well-B1',
-            'outer_request' => requests[9].uuid
+            'outer_request' => requests_for_cleanup[9].uuid
           }
         ]
       end
@@ -365,16 +408,49 @@ RSpec.describe LabwareCreators::PcrCyclesBinnedPlate, with: :uploader do
         )
       end
 
-      let(:bait_library) { create :bait_library, name: 'HybPanel1' }
-
-      before do
-        stub_v2_bait_library(bait_library.name, bait_library)
+      let!(:order_request) do
+        stub_api_get(example_submission_template_uuid,
+body: json(:submission_template, uuid: example_submission_template_uuid))
+        stub_api_post(
+          example_submission_template_uuid,
+          'orders',
+          payload: {
+            order: {
+              assets: filtered_parent_asset_uuids,
+              request_options: purpose_config[:submission_options]['Test Dilution and Cleanup']['request_options'],
+              user: user_uuid,
+              autodetect_studies_projects: true
+            }
+          },
+          body: '{"order":{"uuid":"order-uuid"}}'
+        )
       end
+
+      let(:order_id) { 'order-uuid' }
+
+      let!(:submission_request) do
+        stub_api_post(
+          'submissions',
+          payload: {
+            submission: {
+              orders: [order_id],
+              user: user_uuid
+            }
+          },
+          body: json(:submission, uuid: submission_uuid, orders: [{ uuid: order_id }])
+        )
+      end
+
+      let!(:submission_submit) { stub_api_post(submission_uuid, 'submit') }
 
       it 'makes the expected transfer requests to bin the wells' do
         expect(subject.save!).to eq true
+        expect(subject.skipped_wells).to match(expected_skipped_wells)
         expect(plate_creation_request).to have_been_made
         expect(transfer_creation_request).to have_been_made
+        expect(order_request).to have_been_made.once
+        expect(submission_request).to have_been_made.once
+        expect(submission_submit).to have_been_made.once
       end
     end
   end
