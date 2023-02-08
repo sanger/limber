1406206e0510d5acd1eb0bdb47b3bd05f7051292
Andrew Sparkes <as28@sanger.ac.uk>
2023-02-08 16:05:16 +0000

    added indifferent access to fix bug in determining pools

--- app/models/labware_creators/pooled_tubes_by_submission.rb
+++ app/models/labware_creators/pooled_tubes_by_submission.rb
@@ -16,18 +16,18 @@ module LabwareCreators
     def determine_pools
       # pools are already defined on the parent plate, since they are set at the point of submission
       # filter for just those that are marked for multiplexing
-      pools_for_multiplexing = parent.pools.select { |_submission_id, pool_info| pool_info['for_multiplexing'] }
+      pools_for_multiplexing = parent.pools.select { |_submission_id, pool_info| pool_info.with_indifferent_access['for_multiplexing'] }
 
       # filter for just those where the source wells for the pool have't been failed
       pools_for_multiplexing.transform_values { |pool_info| all_wells_in_pool_passed?(pool_info) }
     end
 
     def all_wells_in_pool_passed?(pool_info)
-      pool_info.fetch('wells', []).select { |location| pick?(location) }
+      pool_info.with_indifferent_access.fetch('wells', []).select { |location| pick?(location) }
     end
 
     def pick?(location)
-      well_locations.fetch(location).passed?
+      well_locations.with_indifferent_access.fetch(location).passed?
     end
   end
 end
