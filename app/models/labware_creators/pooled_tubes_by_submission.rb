2ea0fb6324c801b9da194a1b6326901267af3ae0
Andrew Sparkes <as28@sanger.ac.uk>
2023-02-09 09:04:46 +0000

    rubocop

--- app/models/labware_creators/pooled_tubes_by_submission.rb
+++ app/models/labware_creators/pooled_tubes_by_submission.rb
@@ -16,7 +16,8 @@ module LabwareCreators
     def determine_pools
       # pools are already defined on the parent plate, since they are set at the point of submission
       # filter for just those that are marked for multiplexing
-      pools_for_multiplexing = parent.pools.select { |_submission_id, pool_info| pool_info.with_indifferent_access['for_multiplexing'] }
+      pools_for_multiplexing = parent.pools.select do |_submission_id, pool_info|
+ pool_info.with_indifferent_access['for_multiplexing'] end
 
       # filter for just those where the source wells for the pool have't been failed
       pools_for_multiplexing.transform_values { |pool_info| all_wells_in_pool_passed?(pool_info) }
