04f1cf86bc746788c2ad557c7cfa592a6bc5e0c6
Eduardo Martin Rojo <emr@sanger.ac.uk>
2023-07-04 11:47:31 +0100

    Test the ending tag

--- spec/views/exports/bioscan_mbrave.tsv.erb_spec.rb
+++ spec/views/exports/bioscan_mbrave.tsv.erb_spec.rb
@@ -16,6 +16,9 @@ RSpec.describe 'exports/bioscan_mbrave.tsv.erb' do
     meta2 =
       create(:v2_sample_metadata_for_mbrave, supplier_name: 'meta2', cohort: 'cohort2', sample_description: 'desc2')
     sample2 = create(:v2_sample, sample_metadata: meta2)
+    meta3 =
+      create(:v2_sample_metadata_for_mbrave, supplier_name: 'meta3', cohort: 'cohort3', sample_description: 'desc3')
+    sample3 = create(:v2_sample, sample_metadata: meta3)
     aliquots = [
       create(
         :v2_tagged_aliquot_for_mbrave,
@@ -43,6 +46,15 @@ RSpec.describe 'exports/bioscan_mbrave.tsv.erb' do
         sample: sample0,
         tag: create(:v2_tag, tag_group: create(:v2_tag_group, name: 'Bioscan_forward_96_v2')),
         tag2: create(:v2_tag, tag_group: create(:v2_tag_group, name: 'Bioscan_reverse_4_1_v2'))
+      ),
+      create(
+        :v2_tagged_aliquot_for_mbrave,
+        well_location: 'H12',
+        library_state: 'pending',
+        outer_request: outer_request,
+        sample: sample3,
+        tag: create(:v2_tag, tag_group: create(:v2_tag_group, name: 'Bioscan_forward_96_v2')),
+        tag2: create(:v2_tag, tag_group: create(:v2_tag_group, name: 'Bioscan_reverse_4_24_v2'))
       )
     ]
     create(:v2_tube, aliquots: aliquots)
@@ -57,7 +69,8 @@ RSpec.describe 'exports/bioscan_mbrave.tsv.erb' do
         ['Forward Labels', 'Reverse Labels', 'Label', 'Group', 'UMI plate ID', 'Sample Plate ID'],
         %w[PB1F_bc1001 PB1R_bc1097_rc meta0 cohort0 1 desc0],
         %w[PB1F_bc1001 PB1R_bc1121_rc meta2 cohort2 7 desc2],
-        %w[PB1F_bc1075 PB1R_bc1139_rc meta1 cohort1 11 desc1]
+        %w[PB1F_bc1075 PB1R_bc1139_rc meta1 cohort1 11 desc1],
+        %w[PB1F_bc1096 PB1R_bc1192_rc meta3 cohort3 24 desc3]
       ]
     )
   end
