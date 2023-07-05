475013ac6081d72606530e4606f131179b686c75
yoldas <ay6@sanger.ac.uk>
2023-07-05 15:40:54 +0100

    Updated warning with printing info

--- app/models/presenters/tag_plate_384_presenter.rb
+++ app/models/presenters/tag_plate_384_presenter.rb
@@ -6,5 +6,13 @@ module Presenters
     def label
       Labels::Plate384SingleLabel.new(labware)
     end
+
+    def add_unknown_plate_warnings
+      errors.add(
+        :plate,
+        "type '#{labware.purpose_name}' is not a limber plate. " \
+        'You can still use this page to print labels.'
+      )
+    end
   end
 end
