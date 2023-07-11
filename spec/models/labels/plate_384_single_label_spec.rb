0607f76ca47882586f9a688f023cbd3c5034902e
yoldas <ay6@sanger.ac.uk>
2023-07-11 15:17:36 +0100

    Removed explicit purpose name from test

--- spec/models/labels/plate_384_single_label_spec.rb
+++ spec/models/labels/plate_384_single_label_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Labels::Plate384SingleLabel, type: :model do
   it { expect(described_class).to be < Labels::Base }
 
   context 'when creating the label of a plate' do
-    let(:labware) { build :v2_plate, purpose_name: 'LBSN-384 PCR 1', size: 384 }
+    let(:labware) { build :v2_plate, size: 384 }
     let(:label) { Labels::Plate384SingleLabel.new(labware) }
     let(:date_format) { /\A\s?\d{1,2}-[A-Z]{3}-\d{4}\z/ } # e.g., ' 4 JUL 2023' or '24 JUL 2023'
 
