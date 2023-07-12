aa186c041ed8e24c76b8db2dcaa094713c5df33a
yoldas <ay6@sanger.ac.uk>
2023-07-12 02:00:41 +0100

    First line changes for tubes

--- app/models/labels/tube_label_traction_compatible.rb
+++ app/models/labels/tube_label_traction_compatible.rb
@@ -4,7 +4,7 @@
 # instead of the machine barcode.
 class Labels::TubeLabelTractionCompatible < Labels::TubeLabel
   def attributes
-    {
+    d = {
       first_line: first_line,
       second_line: second_line,
       third_line: labware.purpose_name,
@@ -13,5 +13,24 @@ class Labels::TubeLabelTractionCompatible < Labels::TubeLabel
       round_label_bottom_line: labware.barcode.number,
       barcode: labware.barcode.human
     }
+    binding.pry
+    return d
+  end
+
+  # Parent barcode for LBSN-9216 Lib PCR Pool XP tube
+  # Empty for LBSN-9216 Lib PCR Pool Tube
+  # 
+  def first_line
+    # Parent barcode for LBSN-384 PCR 2 Pool tube
+    return labware.parents[0].barcode.human if labware.parents.size = 1
+    # Name for LBSN-9216 Lib PCR Pool XP tube
+    return labware.name if labware.name&.match? /^.+?\s[A-Z]\d+:[A-Z]\d+$/
+    # Parent barcode for LBSN-9216 Lib PCR Pool XP tube
+  end
+
+  def second_line
+    pools_size = @options[:pool_size] || labware.aliquots.count
+    "#{labware.barcode.number}, P#{pools_size}"
   end
 end
+
