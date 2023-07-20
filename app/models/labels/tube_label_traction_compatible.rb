12504d85d9e6c869a5a8e7ade77a69353b66f3c4
yoldas <ay6@sanger.ac.uk>
2023-07-20 01:07:13 +0100

    Bioscan XP tube label class inherits from tube 1d

--- app/models/labels/tube_label_traction_compatible.rb
+++ app/models/labels/tube_label_traction_compatible.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# This is the same as a standard Tube label but has a human readable barcode
-# instead of the machine barcode.
-class Labels::TubeLabelTractionCompatible < Labels::TubeLabel
+# This tube label uses human readable barcode instead of the machine barcode
+# for Bioscan XP tube for Traction compatibility.
+class Labels::TubeLabelTractionCompatible < Labels::Tube1dLabel
   def attributes
     {
       first_line: first_line,
@@ -17,26 +17,8 @@ class Labels::TubeLabelTractionCompatible < Labels::TubeLabel
 
   # Parent barcode
   def first_line
-    # Parent barcode for LBSN-384 PCR 2 Pool tube.
-    # This is the asset name with well range, which corresponds to plate barcode and well range.
-    return labware.name if labware.name&.match?(/^.+?\s[A-Z]\d{1,2}:[A-Z]\d{1,2}$/)
-
-    # Parent barcode for LBSN-9216 Lib PCR Pool tube.
-    # There are up to 24 parent tubes for this tube. Show the first parent.
-
     # Parent barcode for LBSN-9216 Lib PCR Pool XP tube.
     # This is the previous tube barcode.
-    return labware.parents[0].barcode.human if labware.parents.size == 1
-  end
-
-  # Tube ID without prefix followed by number of samples
-  def second_line
-    pools_size = @options[:pool_size] || labware.aliquots.count
-    "#{barcode_human_without_prefix}, P#{pools_size}"
-  end
-
-  #
-  def barcode_human_without_prefix
-    labware.barcode.human.sub(/\A#{Regexp.escape(labware.barcode.prefix)}/, '')
+    labware.parents[0].barcode.human
   end
 end
