c8a652a169a3e0a64801ad886d746d6e90573466
yoldas <ay6@sanger.ac.uk>
2023-07-22 10:44:57 +0100

    Added code comments to plate label class

--- app/models/labels/plate_384_single_label.rb
+++ app/models/labels/plate_384_single_label.rb
@@ -1,7 +1,31 @@
 # frozen_string_literal: true
 
-# Prints labels for 384-well plates (single label).
-# Stock plate barcode comes from the first stock plate of configured or last purpose
+# This label class provides attributes for Bioscan 384-well plate labels,
+# which have only a single sticker. The placement of barcode and text fields
+# is similar to 96-well plate labels. Note that other 384-well plate labels
+# have double stickers.
+#
+# The label contains a barcode image in the middle and text on the corners.
+# * The barcode image in the middle contains human barcode in code128, which
+#   is more compact and readable than code39. It needs empty space on both
+#   sides for readabilty.
+# * Top-left contains date of printing
+# * Bottom-left contains human barcode of the plate
+# * Top-right contains the barcode of the an ancestor stock plate. For Bioscan
+#   384-well plates, we show the first LILYS barcode, or the first LYSATE
+#   barcode; not last.
+#
+# We show an ancestor stock plate barcode; not the parent. The algorithm
+# below is generic to handle the cases when ancestor plates with the same
+# purpose can be stock plates or non stock plates because of alternative
+# pipeline paths and/or when ancestor plates from different paths are used
+# together to make a plate. We show the first of the configured alternative
+# or the first of closest stock plate ancestors.
+#
+# Only Squix printers are used for printing 384-well plate labels. We do not
+# send print requests to PMB service, instead we send them directly to SPrint
+# service, which talks to Squix printers.
+#
 class Labels::Plate384SingleLabel < Labels::PlateLabelBase
   def attributes
     {
@@ -49,6 +73,10 @@ class Labels::Plate384SingleLabel < Labels::PlateLabelBase
 
     labware.ancestors.where(purpose_name: [last_purpose_name]).order(id: :asc).first
   end
+
+  # There are no defaults configured for this label in the label_templates config.
+  # It is configured using its section in there. The following methods are to make
+  # sure that inherited defaults are not captured accidentially.
 
   def default_printer_type
     default_printer_type_for(:plate_384_single)
