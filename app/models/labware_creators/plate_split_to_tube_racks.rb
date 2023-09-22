ac6a02c2c588d0d4e335c7f1b08654d521657a6d
Andrew Sparkes <as28@sanger.ac.uk>
2023-09-22 11:22:04 +0100

    Initial version of labware creator for plate to fluidx tubes - wip

---
+++ app/models/labware_creators/plate_split_to_tube_racks.rb
@@ -0,0 +1,424 @@
+# frozen_string_literal: true
+
+require_dependency 'form'
+require_dependency 'labware_creators/base'
+
+module LabwareCreators
+  # Handles the creation of up to 2 child racks of tubes from a single parent 96-well plate.
+  # Intended for use in a PBMC cell extraction pipeline.
+  #
+  # There will typically be one rack of tubes for 'contingency' and one for 'sequencing' (this rack is
+  # optional).
+  #
+  # The parent plate contains multiple copies of material prepared from the same sample. One well instance
+  # of each sample will go into a tube in the 'sequencing' rack (if present), and any remaining copies
+  # will go into tubes in the 'contingency' rack.
+  #
+  # If after the initial preparation the users feel they need more contingency tubes, then they will go
+  # back to prepare more material for that sample from an earlier step in the pipeline and create just
+  # 'contingency' tubes at this step.
+  #
+  # Inputs:
+  # 1) The parent plate - contains a number of wells containing material prepared from the same sample.
+  #    The first of these parent wells will be transferred into a tube in the 'sequencing' rack if it is
+  #    present, and any remaining parent wells for the same sample will be transferred into tubes in the
+  #    'contingency' rack.
+  # 2) Child tube rack scan CSV files - these are scans of racks of 2D tube barcodes from the 'contingency'
+  #    and 'sequencing' tube racks, to allow us to know the position and barcode of each available tube.
+  #    On the upload screen displays counts of how many tubes are needed (e.g. To perform this transfer you
+  #    will either need 20 sequencing and 40 contingency tubes, or 60 contingency tubes.)
+  #
+  # Validations - Error message to users if any of these are not met:
+  # 1) The user must always upload a scan file for the 'contingency' rack tube barcodes, whereas the
+  #    'sequencing' rack file is optional.
+  # 2) The scanned child tube barcodes must be unique and must not already exist in the system i.e. they are
+  #    new unused empty tubes. List any that do already exist with rack type and location.
+  # 3) The number of tubes available in the racks must be sufficient for the number of parent wells being
+  #    transferred. e.g. if there are 20 distinct samples in the parent and 40 additional copies (60 wells
+  #    total), then there must be at least 20 tubes in the first rack and at least 40 in the second rack.
+  #    List any racks that do not have enough tubes with totals.
+  #
+  # rubocop:disable Metrics/ClassLength
+  class PlateSplitToTubeRacks < Base
+    include SupportParent::PlateOnly
+
+    self.attributes += [{ filters: {} }]
+    self.attributes += %i[sequencing_file contingency_file]
+    self.attributes += %i[child_sequencing_tube_purpose_uuid child_contingency_tube_purpose_uuid]
+    self.page = 'plate_split_to_tube_racks'
+
+    attr_accessor :sequencing_file,
+                  :contingency_file,
+                  :child_sequencing_tube_purpose_uuid,
+                  :child_contingency_tube_purpose_uuid
+
+    validates_nested :well_filter
+
+    # TODO: can add validations for specific metadata fields here if needed
+
+    # contingency file is required, sequencing file is optional
+    validates :contingency_file, presence: true
+
+    # validate files
+    validates_nested :sequencing_csv_file, if: :contingency_file
+    validates_nested :contingency_csv_file, if: :sequencing_file
+
+    # validate there are sufficient tubes in the racks for the number of parent wells
+    validate :sufficient_tubes_in_racks?
+
+    # TODO: check need all this in the includes e.g. metadata
+    PARENT_PLATE_INCLUDES = 'wells.aliquots,wells.aliquots.sample,wells.aliquots.sample.sample_metadata'
+
+    def save
+      super && upload_tube_rack_files && true
+    end
+
+    def parent
+      @parent ||= Sequencescape::Api::V2.plate_with_custom_includes(PARENT_PLATE_INCLUDES, uuid: parent_uuid)
+    end
+
+    def parent_v1
+      @parent_v1 ||= api.plate.find(parent_uuid)
+    end
+
+    def filters=(filter_parameters)
+      well_filter.assign_attributes(filter_parameters)
+    end
+
+    # Fetches the unfiltered list of wells from the parent plate
+    def labware_wells
+      parent.wells
+    end
+
+    def create_labware!
+      @sequencing_child_tubes = create_sequencing_child_tubes
+      @contingency_child_tubes = create_contingency_child_tubes
+      perform_transfers
+      true
+    end
+
+    def create_sequencing_child_tubes
+      return [] if require_contingency_tubes_only?
+
+      create_tubes(sequencing_tube_purpose_uuid, parent_wells_for_sequencing.length, contingency_tube_attributes)
+    end
+
+    def create_contingency_child_tubes
+      create_tubes(parent_wells_for_contingency.length)
+    end
+
+    def perform_transfers
+      api.transfer_request_collection.create!(user: user_uuid, transfer_requests: transfer_request_attributes)
+    end
+
+    # We may create multiple tubes, so redirect to the parent plate
+    def redirection_target
+      parent
+    end
+
+    def anchor
+      'children_tab'
+    end
+
+    private
+
+    def well_filter
+      # filters failed and cancelled wells by default
+      @well_filter ||= WellFilter.new(creator: self)
+    end
+
+    def num_parent_wells
+      @num_parent_wells ||= well_filter.filtered.length
+    end
+
+    def ancestor_stock_tubes
+      @ancestor_stock_tubes ||= locate_ancestor_tubes
+    end
+
+    #
+    # Identify the originally supplied ancestor source tubes from their purpose name
+    # and create a hash with sample uuid as the key
+    # @return [Hash] e.g.
+    # {
+    #   <sample 1 uuid>: <tube 1>,
+    #   <sample 2 uuid>: <tube 2>,
+    #   etc.
+    # }
+    def locate_ancestor_tubes
+      purpose_name = purpose_config[:ancestor_stock_tube_purpose_name]
+
+      ancestor_results = parent.ancestors.where(purpose_name: purpose_name)
+      return {} if ancestor_results.blank?
+
+      ancestor_results.each_with_object({}) do |ancestor_result, tube_list|
+        tube = Sequencescape::Api::V2::Tube.find_by(uuid: ancestor_result.uuid)
+        tube_sample_uuid = tube.aliquots.first.sample.uuid
+        tube_list[tube_sample_uuid] = tube if tube_sample_uuid.present?
+      end
+    end
+
+    def parent_uniq_sample_uuids
+      @parent_uniq_sample_uuids ||=
+        well_filter
+          .filtered
+          .each_with_object({}) do |(well, _ignore), unique_sample_uuids|
+            sample_uuid = well.aliquots.first.sample.uuid
+            unique_sample_uuids[sample_uuid] = true unless sample_uuid.in?(unique_sample_uuids)
+          end
+    end
+
+    # Parent will contain several wells per sample
+    def num_parent_unique_samples
+      @num_parent_unique_samples ||= parent_uniq_sample_uuids.keys.length
+    end
+
+    def num_sequencing_tubes
+      # TODO: check this doesn't count NO READ or empty positions
+      @num_sequencing_tubes ||= sequencing_csv_file&.position_details&.length || 0
+    end
+
+    def num_contingency_tubes
+      @num_contingency_tubes ||= contingency_csv_file&.position_details&.length || 0
+    end
+
+    # Validation that there are sufficient tubes in the racks for this parent plate.
+    # This depends on the number of unique samples in the parent plate, and the number of parent wells,
+    # as well as whether they are using both sequencing tubes and contingency tubes or just contingency.
+    def sufficient_tubes_in_racks?
+      if require_contingency_tubes_only?
+        num_contingency_tubes >= num_parent_wells
+      else
+        (num_sequencing_tubes >= num_parent_unique_samples) &&
+          (num_contingency_tubes >= (num_parent_wells - num_parent_unique_samples))
+      end
+    end
+
+    #
+    # Upload the tube rack csv files onto the plate via api v1
+    #
+    def upload_tube_rack_files
+      parent_v1.qc_files.create_from_file!(sequencing_file, 'scrna_core_sequencing_tube_rack_scan.csv')
+      parent_v1.qc_files.create_from_file!(contingency_file, 'scrna_core_contingency_tube_rack_scan.csv')
+    end
+
+    #
+    # Create class that will parse and validate the uploaded file
+    def sequencing_csv_file
+      @sequencing_csv_file ||= CsvFile.new(sequencing_file, parent.human_barcode)
+    end
+
+    def contingency_csv_file
+      @contingency_csv_file ||= CsvFile.new(contingency_file, parent.human_barcode)
+    end
+
+    def require_contingency_tubes_only?
+      sequencing_file.blank?
+    end
+
+    def parent_wells_for_sequencing
+      @parent_wells_for_sequencing ||= find_parent_wells_for_sequencing
+    end
+
+    def find_parent_wells_for_sequencing
+      unique_sample_uuids = []
+      parent_wells = []
+
+      well_filter.filtered.each do |well, _ignore|
+        sample_uuid = well.aliquots.first.sample.uuid
+        next if sample_uuid.in?(unique_sample_uuids)
+
+        unique_sample_uuids << sample_uuid
+        parent_wells << well
+      end
+
+      parent_wells
+    end
+
+    def parent_wells_for_contingency
+      @parent_wells_for_contingency ||=
+        well_filtered.filtered.filter_map { |well, _ignore| well unless parent_wells_for_sequencing.include?(well) }
+    end
+
+    def create_tubes(tube_purpose_uuid, number_of_tubes, tube_attributes)
+      api
+        .specific_tube_creation
+        .create!(
+          user: user_uuid,
+          parent: parent_uuid,
+          child_purposes: [tube_purpose_uuid] * number_of_tubes,
+          tube_attributes: tube_attributes
+        )
+        .children
+        .index_by(&:name)
+    end
+
+    def sequencing_tube_purpose_name
+      @sequencing_tube_purpose_name ||= purpose_config.dig(:creator_class, :args, :child_sequencing_purpose)
+    end
+
+    def sequencing_tube_purpose_uuid
+      raise "Missing purpose configuration argument 'child_sequencing_purpose'" unless sequencing_tube_purpose_name
+
+      Settings.purpose_uuids[sequencing_tube_purpose_name]
+    end
+
+    def contingency_tube_purpose_name
+      @contingency_tube_purpose_name ||= purpose_config.dig(:creator_class, :args, :child_contingency_purpose)
+    end
+
+    def contingency_tube_purpose_uuid
+      raise "Missing purpose configuration argument 'child_contingency_purpose'" unless contingency_tube_purpose_name
+
+      Settings.purpose_uuids[contingency_tube_purpose_name]
+    end
+
+    def ancestor_tube_barcode(sample_uuid)
+      sample_ancestor_tube = ancestor_stock_tubes[sample_uuid]
+      if sample_ancestor_tube.blank?
+        raise StandardError, "Failed to identify ancestor (supplier) source tube for sample uuid #{sample_uuid}"
+      end
+
+      sample_ancestor_tube.human_barcode
+    end
+
+    #
+    # Create the tube attributes to send for the tubes creation in Sequencescape.
+    # Passes the name for each tube.
+    # Passes the foreign barcode extracted from the tube rack scan upload for each tube,
+    # which on the Sequencescape side sets that barcode as the primary.
+    #
+    # returns [Array of hashes] e.g.
+    # [
+    #   {
+    #     prefix: SPARE,
+    #     name: NT11111111:A1,
+    #     foreign_barcode: FD11111111
+    #   },
+    #   {
+    #     prefix: SPARE,
+    #     name: NT22222222:B1,
+    #     foreign_barcode: FD22222222
+    #   },
+    #   etc.
+    # ]
+    #
+    # rubocop:disable Metrics/AbcSize
+    def sequencing_tube_attributes
+      # fetch the available tube positions (i.e. locations of scanned tubes for which we
+      # have the barcodes) e.g. ["A1", "B1", "D1"]
+      available_tube_positions = sequencing_csv_file.position_details.keys
+
+      parent_wells_for_sequencing.each_with_index do |well, well_index|
+        tube_posn = available_tube_positions[well_index]
+        sample_uuid = well.aliquots.first.sample.uuid
+
+        # TODO: could get prefixes from config
+        name_for_details = {
+          prefix: 'SEQ',
+          stock_tube_bc: ancestor_tube_barcode[sample_uuid],
+          dest_tube_posn: tube_posn
+        }
+        {
+          name: name_for(name_for_details),
+          foreign_barcode: sequencing_csv_file.position_details[tube_posn]['barcode']
+        }
+      end
+    end
+
+    # rubocop:enable Metrics/AbcSize
+
+    #
+    # Create the tube attributes to send for the tubes creation in Sequencescape.
+    # Passes the name for each tube.
+    # Passes the foreign barcode extracted from the tube rack scan upload for each tube,
+    # which on the Sequencescape side sets that barcode as the primary.
+    #
+    # returns [Array of hashes] e.g.
+    # [
+    #   {
+    #     prefix: SPARE,
+    #     name: NT11111111:A1,
+    #     foreign_barcode: FD11111111
+    #   },
+    #   {
+    #     prefix: SPARE,
+    #     name: NT22222222:B1,
+    #     foreign_barcode: FD22222222
+    #   },
+    #   etc.
+    # ]
+    #
+    # rubocop:disable Metrics/AbcSize
+    def contingency_tube_attributes
+      # fetch the available tube positions (i.e. locations of scanned tubes for which we
+      # have the barcodes) e.g. ["A1", "B1", "D1"]
+      available_tube_positions = contingency_csv_file.position_details.keys
+
+      parent_wells_for_contingency.each_with_index do |well, well_index|
+        tube_posn = available_tube_positions[well_index]
+        sample_uuid = well.aliquots.first.sample.uuid
+
+        # TODO: could get prefixes from config
+        name_for_details = {
+          prefix: 'SPARE',
+          stock_tube_bc: ancestor_tube_barcode[sample_uuid],
+          dest_tube_posn: tube_posn
+        }
+        {
+          name: name_for(name_for_details),
+          foreign_barcode: contingency_csv_file.position_details[tube_posn]['barcode']
+        }
+      end
+    end
+
+    # rubocop:enable Metrics/AbcSize
+
+    #
+    # Generates a name for the destination tube.
+    # Comprises a prefix, the ancestor source (stock) tube barcode and the destination tube position
+    # return [String] e.g. 'SEQ:NT12345678:A1'
+    #
+    def name_for(name_for_details)
+      "#{name_for_details[:prefix]}:#{name_for_details[:stock_tube_bc]}:#{name_for_details[:dest_tube_posn]}"
+    end
+
+    # def transfer_material_from_parent!(child_uuid)
+    #   child_plate = Sequencescape::Api::V2::Plate.find_by(uuid: child_uuid)
+    #   api.transfer_request_collection.create!(
+    #     user: user_uuid,
+    #     transfer_requests: transfer_request_attributes(child_plate)
+    #   )
+    # end
+
+    def transfer_request_attributes
+      # pools.each_with_object([]) do |(pool_identifier, pool), transfer_requests|
+      #   # this currently assumes that pool_identifier will be the submission_uuid
+      #   # (it would have always been, historically)
+      #   pool.each do |location|
+      #     transfer_requests <<
+      #       request_hash(
+      #         well_locations.fetch(location).uuid,
+      #         sequencing_child_tubes.fetch(name_for(pool)).uuid,
+      #         pool_identifier
+      #       )
+      #   end
+      # end
+    end
+
+    def request_hash(source_well, child_plate, additional_parameters)
+      # {
+      #   'source_asset' => source_well.uuid,
+      #   'target_asset' => child_plate.wells.detect { |child_well| child_well.location == source_well.location }&.uuid
+      # }.merge(additional_parameters)
+    end
+
+    # Maps well locations to the corresponding uuid
+    #
+    # @return [Hash] Hash with well locations (eg. 'A1') as keys, and uuids as values
+    # def well_locations
+    #   TODO: should get the wells from the well_filter.filtered method
+    #   @well_locations ||= parent.wells.index_by(&:location)
+    # end
+  end
+  # rubocop:enable Metrics/ClassLength
+end
---
+++ spec/models/labware_creators/plate_split_to_tube_racks_spec.rb
@@ -0,0 +1,239 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+require 'labware_creators/base'
+require_relative 'shared_examples'
+
+RSpec.describe LabwareCreators::PlateSplitToTubeRacks, with: :uploader do
+  it_behaves_like 'it only allows creation from plates'
+
+  subject { described_class.new(api, form_attributes) }
+
+  it 'should have a custom page' do
+    expect(described_class.page).to eq 'plate_split_to_tube_racks'
+  end
+
+  let(:user_uuid) { SecureRandom.uuid }
+  let(:child_sequencing_tube_purpose_uuid) { SecureRandom.uuid }
+  let(:child_sequencing_tube_purpose) { json :purpose, uuid: child_sequencing_tube_purpose_uuid }
+  let(:child_contingency_tube_purpose_uuid) { SecureRandom.uuid }
+  let(:child_contingency_tube_purpose) { json :purpose, uuid: child_contingency_tube_purpose_uuid }
+  let(:parent_uuid) { SecureRandom.uuid }
+
+  # The parent plate needs to have several wells containing the same sample
+
+  # samples
+  let(:sample1_uuid) { SecureRandom.uuid }
+  let(:sample2_uuid) { SecureRandom.uuid }
+
+  let(:sample1) { create(:v2_sample, name: 'Sample1', uuid: sample1_uuid) }
+  let(:sample2) { create(:v2_sample, name: 'Sample2', uuid: sample2_uuid) }
+
+  # parent aliquots
+  let(:parent_aliquot_sample1_aliquot1) { create(:v2_aliquot, sample: sample1) }
+  let(:parent_aliquot_sample1_aliquot2) { create(:v2_aliquot, sample: sample1) }
+  let(:parent_aliquot_sample1_aliquot3) { create(:v2_aliquot, sample: sample1) }
+
+  let(:parent_aliquot_sample2_aliquot1) { create(:v2_aliquot, sample: sample2) }
+  let(:parent_aliquot_sample2_aliquot2) { create(:v2_aliquot, sample: sample2) }
+
+  # let(:parent_aliquot_sample2_aliquot3) { create(:v2_aliquot, sample: sample1) }
+
+  # parent wells
+  let(:parent_well_a1) { create(:v2_well, location: 'A1', aliquots: [parent_aliquot_sample1_aliquot1]) }
+  let(:parent_well_a2) { create(:v2_well, location: 'A2', aliquots: [parent_aliquot_sample1_aliquot2]) }
+  let(:parent_well_a3) { create(:v2_well, location: 'A3', aliquots: [parent_aliquot_sample1_aliquot3]) }
+
+  let(:parent_well_b1) { create(:v2_well, location: 'B1', aliquots: [parent_aliquot_sample2_aliquot1]) }
+  let(:parent_well_b2) { create(:v2_well, location: 'B2', aliquots: [parent_aliquot_sample2_aliquot2]) }
+
+  # let(:parent_well_b3) do
+  #   create(:v2_well, location: 'B3', aliquots: [parent_aliquot_sample2_aliquot3])
+  # end
+
+  # parent plate
+  let(:parent_plate) do
+    create(
+      :v2_plate,
+      uuid: parent_uuid,
+      wells: [parent_well_a1, parent_well_a2, parent_well_a3, parent_well_b1, parent_well_b2],
+      barcode_number: 5
+    )
+  end
+
+  let(:form_attributes) do
+    {
+      user_uuid: user_uuid,
+      child_sequencing_tube_purpose_uuid: child_sequencing_tube_purpose_uuid,
+      child_contingency_tube_purpose_uuid: child_contingency_tube_purpose_uuid,
+      parent_uuid: parent_uuid
+    }
+  end
+
+  context 'on new' do
+    has_a_working_api
+
+    it 'can be created' do
+      expect(subject).to be_a LabwareCreators::PlateSplitToTubeRacks
+    end
+  end
+
+  context '#save' do
+    has_a_working_api
+
+    let(:sequencing_file_content) do
+      content = sequencing_file.read
+      sequencing_file.rewind
+      content
+    end
+
+    let(:contingency_file_content) do
+      content = contingency_file.read
+      contingency_file.rewind
+      content
+    end
+
+    let(:form_attributes) do
+      {
+        user_uuid: user_uuid,
+        child_sequencing_tube_purpose_uuid: child_sequencing_tube_purpose_uuid,
+        child_contingency_tube_purpose_uuid: child_contingency_tube_purpose_uuid,
+        parent_uuid: parent_uuid,
+        sequencing_file: sequencing_file,
+        contingency_file: contingency_file
+      }
+    end
+
+    let(:stub_sequencing_file_upload) do
+      stub_request(:post, api_url_for(parent_uuid, 'qc_files'))
+        .with(
+          body: sequencing_file_content,
+          headers: {
+            'Content-Type' => 'sequencescape/qc_file',
+            'Content-Disposition' => 'form-data; filename="scrna_core_seq_tube_rack_scan.csv"'
+          }
+        )
+        .to_return(
+          status: 201,
+          body: json(:qc_file, filename: 'scrna_core_seq_tube_rack_scan.csv'),
+          headers: {
+            'content-type' => 'application/json'
+          }
+        )
+    end
+
+    let(:stub_contingency_file_upload) do
+      stub_request(:post, api_url_for(parent_uuid, 'qc_files'))
+        .with(
+          body: contingency_file_content,
+          headers: {
+            'Content-Type' => 'sequencescape/qc_file',
+            'Content-Disposition' => 'form-data; filename="scrna_core_cont_tube_rack_scan.csv"'
+          }
+        )
+        .to_return(
+          status: 201,
+          body: json(:qc_file, filename: 'scrna_core_cont_tube_rack_scan.csv'),
+          headers: {
+            'content-type' => 'application/json'
+          }
+        )
+    end
+
+    let(:tube_creation_request_uuid) { SecureRandom.uuid }
+
+    let(:tube_creation_request) do
+      stub_api_post(
+        'specific_tube_creations',
+        payload: {
+          specific_tube_creation: {
+            user: user_uuid,
+            parent: parent_uuid,
+            child_purposes: [
+              child_sequencing_tube_purpose_uuid,
+              child_contingency_tube_purpose_uuid,
+              child_sequencing_tube_purpose_uuid,
+              child_contingency_tube_purpose_uuid,
+              child_sequencing_tube_purpose_uuid
+            ],
+            # TODO: how are the tubes named if mutiple racks?
+            tube_attributes: [
+              { name: 'DN5 A1:A1' }, # sample 1 seq tube 1
+              { name: 'DN5 A2:A1' }, # sample 1 cont tube 1
+              { name: 'DN5 A3:A2' }, # sample 1 cont tube 2
+              { name: 'DN5 B1:A2' }, # sample 2 seq tube 1
+              { name: 'DN5 B2:A3' } # sample 2 cont tube 1
+            ]
+          }
+        },
+        body: json(:specific_tube_creation, uuid: tube_creation_request_uuid, children_count: 5)
+      )
+    end
+
+    # Find out what tubes we've just made!
+    let(:tube_creation_children_request) do
+      stub_api_get(
+        tube_creation_request_uuid,
+        'children',
+        body: json(:tube_collection, names: ['DN5 A1:A1', 'DN5 A2:A1', 'DN5 A3:A2', 'DN5 B1:A2', 'DN5 B2:A3'])
+      )
+    end
+
+    # let(:stub_parent_request) do
+    #   stub_api_get(parent_uuid, body: parent_json)
+    #   stub_api_get(parent_uuid, 'wells', body: wells_json)
+    # end
+
+    let(:transfer_creation_request) do
+      stub_api_post(
+        'transfer_request_collections',
+        payload: {
+          transfer_request_collection: {
+            user: user_uuid,
+            transfer_requests: [
+              { 'source_asset' => parent_well_a1.uuid, 'target_asset' => 'tube-0' },
+              { 'source_asset' => parent_well_a2.uuid, 'target_asset' => 'tube-1' },
+              { 'source_asset' => parent_well_a3.uuid, 'target_asset' => 'tube-2' },
+              { 'source_asset' => parent_well_b1.uuid, 'target_asset' => 'tube-3' },
+              { 'source_asset' => parent_well_b2.uuid, 'target_asset' => 'tube-4' }
+            ]
+          }
+        },
+        body: '{}'
+      )
+    end
+
+    before do
+      # stub_parent_request
+      # stub_v2_plate(parent_plate, stub_search: false)
+      stub_v2_plate(
+        parent_plate,
+        stub_search: false,
+        custom_includes: 'wells.aliquots,wells.aliquots.sample,wells.aliquots.sample.sample_metadata'
+      )
+      stub_sequencing_file_upload
+      stub_contingency_file_upload
+      tube_creation_children_request
+      tube_creation_request
+      transfer_creation_request
+    end
+
+    context 'with valid files' do
+      let(:sequencing_file) do
+        fixture_file_upload('spec/fixtures/files/scrna_core_sequencing_tube_rack_scan.csv', 'sequencescape/qc_file')
+      end
+
+      let(:contingency_file) do
+        fixture_file_upload('spec/fixtures/files/scrna_core_contingency_tube_rack_scan.csv', 'sequencescape/qc_file')
+      end
+
+      it 'works' do
+        expect(subject.save).to be_truthy
+        expect(stub_sequencing_file_upload).to have_been_made.once
+        expect(stub_contingency_file_upload).to have_been_made.once
+        expect(tube_creation_request).to have_been_made.once
+        expect(transfer_creation_request).to have_been_made.once
+      end
+    end
+  end
+end
