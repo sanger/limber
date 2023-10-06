eac06379b0600ed055add5fd57786fa6510ececd
yoldas <ay6@sanger.ac.uk>
2023-10-06 02:55:27 +0100

    Added labware creator and driver file download

---
+++ app/models/labware_creators/pooled_wells_by_sample.rb
@@ -0,0 +1,101 @@
+# frozen_string_literal: true
+
+require_dependency 'labware_creators/base'
+
+# This labware creator pools PBMC isolations in pairs (or configured number of
+# source wells per destination) before cell counting to reduce the number runs
+# on Celleca (cell counting). The robot transfer is done from LRC Blood Bank to
+# LRC PBMC Bank plate.
+
+module LabwareCreators
+  class PooledWellsBySample < Base
+    include SupportParent::PlateOnly
+
+    # By default, source wells with the sample are pooled in pairs.
+    DEFAULT_NUMBER_OF_SOURCE_WELLS = 2
+
+    # Number of source wells with the same sample to be pooled.
+    def number_of_source_wells
+      purpose_config.dig(:number_of_source_wells) || DEFAULT_NUMBER_OF_SOURCE_WELLS
+    end
+
+    def well_filter
+      @well_filter ||= WellFilter.new(creator: self)
+    end
+
+    def filters=(filter_parameters)
+      well_filter.assign_attributes(filter_parameters)
+    end
+
+    # Parent plate using SS v2 API
+    def source_plate
+      @source_plate ||= Sequencescape::Api::V2::Plate.find_by(uuid: parent.uuid)
+    end
+
+    # List of passed wells from parent plate
+    def passed_parent_wells
+      source_plate.wells.select { |well| well.state == 'passed' }
+    end
+
+    # List of passed wells from parent plate in column order
+    def parent_wells_in_colums
+      passed_parent_wells.sort_by(&:coordinate)
+    end
+
+    # List of pools to be created; index is the destination pool number; each pool is a list of source wells
+    def build_pools
+      parent_wells_in_colums
+        .group_by { |well| well.aliquots.first.sample.uuid }
+        .flat_map { |uuid, wells| wells.each_slice(number_of_source_wells).to_a }
+    end
+
+    # List of pools built from passed wells from parent plate
+    def pools
+      @pools ||= build_pools
+    end
+
+    # Object mapping source wells to destination wells for
+    def transfer_hash
+      result = {}
+      pools.each_with_index do |pool, index|
+        dest_location = WellHelpers.well_at_column_index(index)
+        pool.each do |source_well|
+          source_location = source_well.location
+          result[source_location] = { dest_locn: dest_location }
+        end
+      end
+      result
+    end
+
+    # Well object at well location on plate
+    def get_well_for_plate_location(plate, well_location)
+      plate.wells.detect { |well| well.location == well_location }
+    end
+
+    # Attributes for transfer request from source_well to dest_plate
+    def request_hash(source_well, dest_plate)
+      dest_location = transfer_hash[source_well.location][:dest_locn]
+      {
+        'source_asset' => source_well.uuid,
+        'target_asset' => get_well_for_plate_location(dest_plate, dest_location)&.uuid,
+        # 'submission' => submission,
+        'merge_equivalent_aliquots' => true
+      }
+    end
+
+    # List of objects mapping source wells to destination wells
+    def transfer_request_attributes(dest_plate)
+      passed_parent_wells.map { |source_well| request_hash(source_well, dest_plate) }
+    end
+
+    # Send the transfer request to SS
+    def transfer_material_from_parent!(dest_uuid)
+      dest_plate = Sequencescape::Api::V2::Plate.find_by(uuid: dest_uuid)
+      api.transfer_request_collection.create!(
+        user: user_uuid,
+        transfer_requests: transfer_request_attributes(dest_plate)
+      )
+      true
+    end
+  end
+end
---
+++ spec/models/labware_creators/pooled_wells_by_sample_spec.rb
@@ -0,0 +1,181 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe LabwareCreators::PooledWellsBySample do
+  has_a_working_api
+
+  let(:user_uuid) { 'user-uuid' }
+  let(:parent_plate_uuid) { 'parent-plate-uuid' }
+  let(:child_purpose_uuid) { 'child-purpose-uuid' }
+  let(:parent_plate) { create(:v2_plate, uuid: parent_plate_uuid, aliquots_without_requests: 1) }
+  let(:form_attributes) { { purpose_uuid: child_purpose_uuid, parent_uuid: parent_plate_uuid, user_uuid: user_uuid } }
+  subject { described_class.new(api, form_attributes) }
+  before do
+    allow(subject).to receive(:parent).and_return(parent_plate)
+    stub_v2_plate(parent_plate, stub_search: false)
+  end
+
+  describe '#number_of_source_wells' do
+    context 'when purpose_config includes number_of_source_wells' do
+      before { create(:purpose_config, uuid: child_purpose_uuid, number_of_source_wells: 3) }
+
+      it 'returns the number of source wells from the purpose_config' do
+        expect(subject.number_of_source_wells).to eq(3)
+      end
+    end
+
+    context 'when purpose_config does not include number_of_source_wells' do
+      before { create(:purpose_config, uuid: child_purpose_uuid) }
+      it 'returns the default number of source wells' do
+        expect(subject.number_of_source_wells).to eq(2)
+      end
+    end
+  end
+
+  describe '#source_plate' do
+    it 'returns the source plate' do
+      expect(subject.source_plate).to eq(parent_plate)
+    end
+  end
+
+  describe '#passed_parent_wells' do
+    before do
+      parent_plate.wells[0..3].map { |well| well.state = 'failed' }
+      parent_plate.wells[4..95].map { |well| well.state = 'passed' }
+    end
+
+    it 'ignores source wells that have been failed' do
+      expect(subject.passed_parent_wells).not_to include(*parent_plate.wells[0..3])
+    end
+
+    it 'returns source wells that have been passed' do
+      expect(subject.passed_parent_wells).to include(*parent_plate.wells[4..95])
+    end
+  end
+
+  describe '#parent_wells_in_colums' do
+    context 'inside columns' do
+      before do
+        # Swap two wells in the column so that they are not in correct order
+        parent_plate.wells.map { |well| well.state = 'failed' }
+        parent_plate.wells[0].state = 'passed'
+        parent_plate.wells[7].state = 'passed'
+        parent_plate.wells[0], parent_plate.wells[7] = parent_plate.wells[7], parent_plate.wells[0]
+      end
+
+      it 'returns passed source wells in correct order' do
+        wells = subject.parent_wells_in_colums
+        expect(wells[0].location).to eq('A1')
+        expect(wells[1].location).to eq('H1')
+      end
+    end
+    context 'between columns' do
+      before do
+        # Swap wells between columns so that they are not in correct order
+        parent_plate.wells.map { |well| well.state = 'failed' }
+        parent_plate.wells[0].state = 'passed'
+        parent_plate.wells[8].state = 'passed'
+        parent_plate.wells[0], parent_plate.wells[8] = parent_plate.wells[8], parent_plate.wells[0]
+      end
+      it 'returns passed source wells in correct order' do
+        wells = subject.parent_wells_in_colums
+        expect(wells[0].location).to eq('A1')
+        expect(wells[1].location).to eq('A2')
+      end
+    end
+  end
+
+  describe '#build_pools' do
+    let(:sample_uuids) do
+      # If well E1 with sample_3_uuid is failed and number_of_source_wells is 2, pools will be:
+      # ["A1", "B1"]
+      # ["C1"]
+      # ["D1"]
+      # ["F1", "G1"]
+      # ["H1", "A2"]
+      %w[
+        sample_1_uuid
+        sample_1_uuid
+        sample_1_uuid
+        sample_2_uuid
+        sample_3_uuid
+        sample_4_uuid
+        sample_4_uuid
+        sample_5_uuid
+        sample_5_uuid
+      ]
+    end
+    before do
+      parent_plate.wells[0..(sample_uuids.size - 1)].each_with_index do |well, index|
+        well.aliquots.first.sample.uuid = sample_uuids[index]
+        well.state = 'passed'
+      end
+      parent_plate.wells[4].state = 'failed' # fail well E1 with sample_3_uuid
+    end
+
+    it 'builds correct number of pools' do
+      expect(subject.build_pools.size).to eq(5)
+    end
+
+    it 'builds up to the number of source wells' do
+      pools = subject.build_pools
+      expect(pools[0].size).to eq(2)
+      expect(pools[0][0].location).to eq('A1')
+      expect(pools[0][0].aliquots.first.sample.uuid).to eq('sample_1_uuid')
+      expect(pools[0][1].location).to eq('B1')
+      expect(pools[0][1].aliquots.first.sample.uuid).to eq('sample_1_uuid')
+    end
+
+    it 'builds with remaining source wells' do
+      pools = subject.build_pools
+      expect(pools[1].size).to eq(1)
+      expect(pools[1][0].location).to eq('C1')
+      expect(pools[1][0].aliquots.first.sample.uuid).to eq('sample_1_uuid') # third well with sample_1_uuid
+    end
+
+    it 'builds with one source well' do
+      pools = subject.build_pools
+      expect(pools[2].size).to eq(1)
+      expect(pools[2][0].location).to eq('D1')
+      expect(pools[2][0].aliquots.first.sample.uuid).to eq('sample_2_uuid') # only well with sample_2_uuid
+    end
+
+    it 'builds without failed source wells' do
+      locations = subject.build_pools.flatten(1).map(&:location)
+      expect(locations).not_to include('E1')
+    end
+
+    it 'builds in column order' do
+      pools = subject.build_pools
+      expect(pools[4].size).to eq(2)
+      expect(pools[4][1].location).to eq('A2')
+      expect(pools[4][1].aliquots.first.sample.uuid).to eq('sample_5_uuid')
+    end
+  end
+
+  describe '#transfer_hash' do
+    before do
+      parent_plate.wells.map { |well| well.state = 'failed' }
+      parent_plate.wells[0..2].map { |well| well.state = 'passed' }
+      parent_plate.wells[0].aliquots.first.sample.uuid = 'sample_1_uuid'
+      parent_plate.wells[1].aliquots.first.sample.uuid = 'sample_1_uuid'
+      parent_plate.wells[2].aliquots.first.sample.uuid = 'sample_2_uuid'
+    end
+
+    it 'returns a mapping of source wells to destination wells' do
+      result = subject.transfer_hash
+      expect(result.size).to eq(3)
+      expect(result['A1']).to eq(dest_locn: 'A1')
+      expect(result['B1']).to eq(dest_locn: 'A1')
+      expect(result['C1']).to eq(dest_locn: 'B1')
+    end
+  end
+
+  describe '#get_well_for_plate_location' do
+    it 'returns well for well location on plate' do
+      well = subject.get_well_for_plate_location(parent_plate, 'A1')
+      expect(well.location).to eq('A1')
+    end
+  end
+end
