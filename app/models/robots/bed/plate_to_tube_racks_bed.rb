775799d73bdd3356191b2a24216898f1da9debf2
yoldas <ay6@sanger.ac.uk>
2023-10-26 02:12:42 +0100

    Robot finds labware for beds

--- app/models/robots/bed/plate_to_tube_racks_bed.rb
+++ app/models/robots/bed/plate_to_tube_racks_bed.rb
@@ -6,42 +6,23 @@ module Robots::Bed
     # TODO: Do I need this accessor?
     attr_accessor :parents
 
-    PLATE_INCLUDES = 'purpose,wells,wells.downstream_tubes,wells.downstream_tubes.custom_metadatum_collection'
-
-    def set_created_with_robot(robot_barcode); end
-
-    def find_all_labware
-      Sequencescape::Api::V2::Plate.find_all({ barcode: @barcodes }, includes: PLATE_INCLUDES)
-    end
-
-    def load_labware_from_parents(parents)
-      return if labware.present?
-      @labware = parents.flat_map(&:child_labware).select { |labware| labware.barcode.human == barcode }
+    def labware_created_with_robot(robot_barcode)
+      labware.tubes.each do |tube|
+        LabwareMetadata
+          .new(api: api, user: user_uuid, barcode: tube.barcode.machine)
+          .update!(created_with_robot: robot_barcode)
+      end
     end
 
     def child_labware
-      return [] if labware.nil?
+      return [] if labware.blank?
 
-      @child_labware ||= child_labware_of_plate
+      @child_labware ||= robot.child_labware(labware)
     end
 
-    def child_labware_of_plate
-      labware
-        .wells
-        .sort_by(&well_order)
-        .each_with_object([]) do |well, racks|
-          next if well.downstream_tubes.empty?
-
-          well.downstream_tubes.each do |tube|
-            barcode = tube.custom_metadatum_collection.metadata[:tube_rack_barcode]
-            rack = racks.detect { |rack| rack.barcode.human == barcode }
-            if rack.nil?
-              labware_barcode = LabwareBarcode.new(human: barcode, machine: barcode)
-              rack = racks.push(TubeRackWrapper.new(labware_barcode)).last
-            end
-            rack.tubes << tube
-          end
-        end
+    def load(barcodes)
+      @barcodes = Array(barcodes).filter_map(&:strip).uniq
+      @labware = @barcodes.present? ? robot.find_bed_labware(@barcodes) : []
     end
   end
 end
