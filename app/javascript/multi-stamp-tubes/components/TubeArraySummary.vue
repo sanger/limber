a080ba9b98f2e41fb127ce634c67c84f3e93f895
Andrew Sparkes <as28@sanger.ac.uk>
2023-11-20 12:16:24 +0000

    refactored after review

--- app/javascript/multi-stamp-tubes/components/TubeArraySummary.vue
+++ app/javascript/multi-stamp-tubes/components/TubeArraySummary.vue
@@ -14,18 +14,18 @@
       </tr>
     </thead>
     <tbody>
-      <tr v-for="(row, rowIndex) in tubeSummary" :key="row.barcode">
+      <tr v-for="(value, machine_barcode, rowIndex) in tubesDict" :key="rowIndex">
         <th class="first-col">
           {{ (rowIndex + 1).toString() + '.' }}
         </th>
         <td :id="`row_human_barcode_index_${rowIndex}`" class="summarycell">
-          {{ row.human_barcode }}
+          {{ value.human_barcode }}
         </td>
         <td :id="`row_machine_barcode_index_${rowIndex}`" class="summarycell">
-          {{ row.machine_barcode }}
+          {{ machine_barcode }}
         </td>
         <td :id="`row_replicates_index_${rowIndex}`" class="replicate_cell">
-          {{ row.replicates }}
+          {{ value.replicates }}
         </td>
       </tr>
     </tbody>
@@ -36,25 +36,33 @@
 export default {
   name: 'TubeArraySummary',
   props: {
+    // Array of tube objects from the parent component.
+    // Represents the tubes scanned into the MultiStampTubes arraying component.
+    // Each tube object contains the following:
+    //  - index: the index of the tube in the array, related to the position in the tube rack
+    //  - labware: the labware object scanned into the parent screen, null if position is empty
+    //  - state: the state of the tube at this index, or 'empty' if nothing scanned yet
     tubes: {
       type: Array,
       required: true,
     },
   },
   computed: {
+    // Create a dictionary of tube barcodes and their numbers of replicates for use
+    // in the summary table
     tubesDict() {
       var summary_dict = {}
       this.tubes.forEach(function (tube) {
         var tube_machine_barcode = 'Empty'
         var tube_human_barcode = 'Empty'
 
-        // skip empty locations
+        // extract the labware barcodes where the labware has been scanned
         if (tube.labware != null) {
           tube_machine_barcode = tube.labware.labware_barcode.machine_barcode
           tube_human_barcode = tube.labware.labware_barcode.human_barcode
         }
 
-        // add to dict of summary counts
+        // build up the dictionary of summary table replicates by barcode (and empties)
         if (tube_machine_barcode in summary_dict) {
           summary_dict[tube_machine_barcode]['replicates'] += 1
         } else {
@@ -65,17 +73,6 @@ export default {
         }
       })
       return summary_dict
-    },
-    tubeSummary() {
-      var summary_array = []
-      for (var tube_barcode in this.tubesDict) {
-        summary_array.push({
-          machine_barcode: tube_barcode,
-          human_barcode: this.tubesDict[tube_barcode]['human_barcode'],
-          replicates: this.tubesDict[tube_barcode]['replicates'],
-        })
-      }
-      return summary_array
     },
   },
 }
