c243f77513ff733b544dc7b3a6445d770a9218da
Stephen Hulme <sh51@sanger.ac.uk>
2023-11-28 11:47:42 +0000

    style: lint

--- app/javascript/multi-stamp-tubes/components/MultiStampTubes.vue
+++ app/javascript/multi-stamp-tubes/components/MultiStampTubes.vue
@@ -53,7 +53,6 @@
 import LabwareScan from 'shared/components/LabwareScan'
 import LoadingModal from 'shared/components/LoadingModal'
 import Plate from 'shared/components/Plate'
-import { validScanMessage } from 'shared/components/scanValidators'
 import { checkDuplicates, checkState } from 'shared/components/tubeScanValidators'
 import devourApi from 'shared/devourApi'
 import { handleFailedRequest } from 'shared/requestHelpers'
