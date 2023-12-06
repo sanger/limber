a26f88e850121dbecbdfa5f4ae9868e6a470db5a
yoldas <ay6@sanger.ac.uk>
2023-12-06 11:35:45 +0000

    Fixed the test for the first line of PCR 2 Pool tube label

--- spec/models/labels/tube_label_traction_compatible_spec.rb
+++ spec/models/labels/tube_label_traction_compatible_spec.rb
@@ -23,11 +23,11 @@ RSpec.describe Labels::TubeLabelTractionCompatible, type: :model do
   end
 
   context 'when labware name contains plate and well range' do
-    let(:labware) { build :v2_tube, name: 'SQPT-12345-H A1:P24', parents: [build(:v2_tube)] }
+    let(:labware) { build :v2_tube, name: 'SQPT-12345-H A1:P24', parents: [build(:v2_plate)] }
 
     it 'has the correct attributes' do
       attributes = label.attributes
-      expect(attributes[:first_line]).to eq labware.name
+      expect(attributes[:first_line]).to eq "#{labware.parents[0].barcode.human} #{labware.name.split.last}"
       expect(attributes[:second_line]).to match(/, P/)
       expect(attributes[:third_line]).to eq labware.purpose.name
       expect(attributes[:fourth_line]).to eq Time.zone.today.strftime('%e-%^b-%Y')
