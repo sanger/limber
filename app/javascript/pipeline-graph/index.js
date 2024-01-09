356992ba9d624ec6c324ab612c7530524cde7f18
Stephen Hulme <sh51@sanger.ac.uk>
2024-01-09 15:55:17 +0000

    fix: wrap text labels

--- app/javascript/pipeline-graph/index.js
+++ app/javascript/pipeline-graph/index.js
@@ -172,6 +172,8 @@ const renderPipelines = function (data) {
           'background-color': '#666',
           label: 'data(id)',
           color: 'white',
+          'text-wrap': 'wrap',
+          'text-max-width': '90',
         },
       },
       {
