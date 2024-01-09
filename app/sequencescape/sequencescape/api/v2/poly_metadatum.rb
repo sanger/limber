40f6c45b50d54f9bcc39903dc0afd02de27fa585
Andrew Sparkes <as28@sanger.ac.uk>
2024-01-09 13:54:58 +0000

    change to has_one to avoid deprecation error for has_many

--- app/sequencescape/sequencescape/api/v2/poly_metadatum.rb
+++ app/sequencescape/sequencescape/api/v2/poly_metadatum.rb
@@ -2,7 +2,7 @@
 
 # Represents a poly metadatum in Limber via the Sequencescape v2 API
 class Sequencescape::Api::V2::PolyMetadatum < Sequencescape::Api::V2::Base
-  belongs_to :metadatable, polymorphic: true, shallow_path: true
+  has_one :metadatable, polymorphic: true, shallow_path: true
 
   property :key, type: :string
   property :value, type: :string
