bc7a94a09546560d8e5e1bd6d4ca24879fd70fdd
Andrew Sparkes <as28@sanger.ac.uk>
2024-01-16 12:36:36 +0000

    updated fields array and removed unneeded comments

--- app/models/labware_creators/pcr_cycles_binned_plate/csv_file_for_t_nano_seq.rb
+++ app/models/labware_creators/pcr_cycles_binned_plate/csv_file_for_t_nano_seq.rb
@@ -11,16 +11,6 @@ module LabwareCreators
   # This version of the csv file is for Targeted NanoSeq.
   #
   class PcrCyclesBinnedPlate::CsvFileForTNanoSeq < PcrCyclesBinnedPlate::CsvFile
-    # include ActiveModel::Validations
-    # extend NestedValidation
-
-    # validate :correctly_parsed?
-    # validates :plate_barcode_header_row, presence: true
-    # validates_nested :plate_barcode_header_row
-    # validates :well_details_header_row, presence: true
-    # validates_nested :well_details_header_row
-    # validates_nested :transfers, if: :correctly_formatted?
-
     delegate :well_column,
              :concentration_column,
              :sanger_sample_id_column,
@@ -33,18 +23,6 @@ module LabwareCreators
              :hyb_panel_column,
              to: :well_details_header_row
 
-    #
-    # Passing in the file to be parsed, the configuration that holds validation range thresholds, and
-    # the parent plate barcode for validation that we are processing the correct file.
-    # def initialize(file, config, parent_barcode)
-    #   initialize_variables(file, config, parent_barcode)
-    # rescue StandardError => e
-    #   reset_variables
-    #   @parse_error = e.message
-    # ensure
-    #   file.rewind
-    # end
-
     def initialize_variables(file, config, parent_barcode)
       @config = Utility::PcrCyclesForTNanoSeqCsvFileUploadConfig.new(config)
       @parent_barcode = parent_barcode
@@ -53,54 +31,12 @@ module LabwareCreators
       @parsed = true
     end
 
-    # def reset_variables
-    #   @config = nil
-    #   @parent_barcode = nil
-    #   @data = []
-    #   @parsed = false
-    # end
-
-    #
-    # Extracts well details from the uploaded csv file
-    #
-    # @return [Hash] eg. { 'A1' => { 'sample_volume' => 5.0, 'diluent_volume' => 25.0,
-    # 'pcr_cycles' => 14, 'submit_for_sequencing' => 'Y', 'sub_pool' => 1, 'coverage' => 15 }, etc. }
-    #
-    # def well_details
-    #   @well_details ||= generate_well_details_hash
-    # end
-
-    # def correctly_parsed?
-    #   return true if @parsed
-
-    #   errors.add(:base, "Could not read csv: #{@parse_error}")
-    #   false
-    # end
-
-    # def plate_barcode_header_row
-    #   @plate_barcode_header_row ||= PlateBarcodeHeader.new(@parent_barcode, @data[0]) if @data[0]
-    # end
-
     # Returns the contents of the header row for the well detail columns
     def well_details_header_row
       @well_details_header_row ||= WellDetailsHeaderForTNanoSeq.new(@data[2]) if @data[2]
     end
 
     private
-
-    # remove byte order marker if present
-    # def remove_bom
-    #   return unless @data.present? && @data[0][0].present?
-
-    #   # byte order marker will appear at beginning of in first string in @data array
-    #   s = @data[0][0]
-
-    #   # NB. had to make byte order marker string mutable here otherwise get frozen string error
-    #   bom = +"\xEF\xBB\xBF"
-    #   s_mod = s.gsub!(bom.force_encoding(Encoding::BINARY), '')
-
-    #   @data[0][0] = s_mod unless s_mod.nil?
-    # end
 
     def transfers
       @transfers ||=
@@ -109,16 +45,19 @@ module LabwareCreators
         end
     end
 
-    # Gates looking for wells if the file is invalid
-    # def correctly_formatted?
-    #   correctly_parsed? && plate_barcode_header_row.valid? && well_details_header_row.valid?
-    # end
-
     # Create the hash of well details from the file upload values
     def generate_well_details_hash
       return {} unless valid?
 
-      fields = %w[sample_volume diluent_volume pcr_cycles hyb_panel]
+      fields = %w[
+        concentration
+        input_amount_available
+        input_amount_desired
+        sample_volume
+        diluent_volume
+        pcr_cycles
+        hyb_panel
+      ]
       transfers.each_with_object({}) do |row, well_details_hash|
         next if row.empty?
 
