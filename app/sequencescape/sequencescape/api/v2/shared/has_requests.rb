22084d2601334121cf59115137e441a08c06f6c9
as28 <as28@sanger.ac.uk>
2024-02-16 16:10:41 +0000

    refactor to make easier to read

--- app/sequencescape/sequencescape/api/v2/shared/has_requests.rb
+++ app/sequencescape/sequencescape/api/v2/shared/has_requests.rb
@@ -57,18 +57,22 @@ module Sequencescape::Api::V2::Shared
     end
 
     # Finding 'in_progress' requests
+    def requests_in_progress(request_type_to_complete: nil)
+      requests = aliquots.flat_map(&:request).compact
 
-    def requests_in_progress(request_type_key: nil)
-      aliquots
-        .flat_map(&:request)
-        .compact
-        .select { |r| request_type_key.nil? || r.request_type_key == request_type_key }
+      if request_type_to_complete.present?
+        requests.select do |r|
+          r.request_type_key == request_type_to_complete
+        end
+      else
+        requests
+      end
     end
 
     # Based on in_progress requests
 
-    def in_progress_submission_uuids(request_type_key: nil)
-      requests_in_progress(request_type_key: request_type_key).flat_map(&:submission_uuid).uniq
+    def in_progress_submission_uuids(request_type_to_complete: nil)
+      requests_in_progress(request_type_to_complete: request_type_to_complete).flat_map(&:submission_uuid).uniq
     end
 
     def all_requests
