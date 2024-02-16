8be10b91fd861b2056f45579b86da475f057d5f3
Stephen Hulme <sh51@sanger.ac.uk>
2024-02-16 11:08:02 +0000

    test: create test component to measure coverage of

---
+++ app/javascript/scratch/example.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: 'Hello, World!',
+    }
+  },
+}
+</script>
+
+<style scoped>
+h1 {
+  color: blue;
+}
+</style>
