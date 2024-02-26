cb861521c156043a9369ba2f6f04e2ce1ceadbbb
Katy Taylor <kt17@sanger.ac.uk>
2024-02-26 15:02:42 +0000

    refactor tests into a shared example file to reduce code duplication

--- spec/sequencescape/api/v2/tube_spec.rb
+++ spec/sequencescape/api/v2/tube_spec.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
+require_relative 'shared_examples'
 
 class SomeStockPlates
   def initialize(stock_plates)
@@ -14,6 +15,7 @@ end
 
 RSpec.describe Sequencescape::Api::V2::Tube do
   subject(:tube) { create :v2_tube, barcode_number: 12_345 }
+  let(:the_labware) { tube }
 
   it { is_expected.to_not be_plate }
   it { is_expected.to be_tube }
@@ -31,74 +33,5 @@ RSpec.describe Sequencescape::Api::V2::Tube do
     end
   end
 
-  describe '#workline_identifier' do
-    it 'displays the barcode of the workline_reference element' do
-      tube1 = create :v2_tube
-      allow(tube).to receive(:workline_reference).and_return(tube1)
-      expect(tube.workline_identifier).to eq(tube1.barcode.human)
-    end
-
-    it 'does not break if there is no workline reference' do
-      allow(tube).to receive(:workline_reference).and_return(nil)
-      expect(tube.workline_identifier).to eq(nil)
-    end
-  end
-
-  describe '#workline_reference' do
-    let(:stock_plate_names) { ['Stock stuff', 'Some other stock stuff'] }
-    let(:ancestors_scope) { double('ancestors_scope') }
-    before do
-      allow(tube).to receive(:ancestors).and_return(ancestors_scope)
-      allow(tube).to receive(:stock_plate).and_return(stock_plate)
-      allow(SearchHelper).to receive(:stock_plate_names).and_return(stock_plate_names)
-      allow(ancestors_scope).to receive(:where).with(purpose_name: stock_plate_names).and_return(stock_plates)
-    end
-
-    context 'when the plate has no stock plates' do
-      let(:stock_plates) { [] }
-      let(:stock_plate) { nil }
-      it 'returns nil' do
-        expect(tube.workline_reference).to be_nil
-      end
-    end
-    context 'when the plate has one stock plate' do
-      let(:stock_plates) { create_list :v2_plate, 1 }
-      let(:stock_plate) { stock_plates.last }
-      it 'returns the stock plate' do
-        expect(tube.workline_reference).to eq(stock_plates.last)
-      end
-    end
-    context 'when the plate has more than one stock plate' do
-      let(:stock_plates) { create_list :v2_plate, 2 }
-      let(:stock_plate) { stock_plates.last }
-
-      context 'when there are no alternative workline purpose references' do
-        before do
-          allow(SearchHelper).to receive(:alternative_workline_reference_name).with(tube).and_return(nil)
-          allow(ancestors_scope).to receive(:where).with(purpose_name: []).and_return([])
-        end
-        it 'returns the last stock plate' do
-          expect(tube.workline_reference).to eq(stock_plates.last)
-        end
-      end
-
-      context 'when there is a list of alternative workline purpose references' do
-        let(:alternative_workline_reference_plates) { create_list :v2_plate, 2 }
-        let(:alternative_workline_name) { 'Some other plate with some stuff inside' }
-
-        before do
-          allow(SearchHelper).to receive(:alternative_workline_reference_name)
-            .with(tube)
-            .and_return(alternative_workline_name)
-          allow(ancestors_scope).to receive(:where)
-            .with(purpose_name: alternative_workline_name)
-            .and_return(alternative_workline_reference_plates)
-        end
-
-        it 'returns the last alternative workline reference' do
-          expect(tube.workline_reference).to eq(alternative_workline_reference_plates.last)
-        end
-      end
-    end
-  end
+  it_behaves_like 'a labware with a workline identifier'
 end
