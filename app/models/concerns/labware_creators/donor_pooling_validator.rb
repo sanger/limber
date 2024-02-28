c8ffdb9c47343e21cd6de807ce139b5c8b03e28a
yoldas <ay6@sanger.ac.uk>
2024-02-28 04:49:59 +0000

    Add comments to validation methods

--- app/models/concerns/labware_creators/donor_pooling_validator.rb
+++ app/models/concerns/labware_creators/donor_pooling_validator.rb
@@ -22,24 +22,42 @@ module LabwareCreators::DonorPoolingValidator
       '(%s) configured. Please check you have scanned the correct set of ' \
       'source plates.'
 
+  # Validates that at least one source barcode has been entered. If no barcodes
+  # are entered, an error is added to the :source_barcodes attribute.
+  #
+  # @return [void]
   def source_barcodes_must_be_entered
     return if minimal_barcodes.size >= 1
 
     errors.add(:source_barcodes, SOURCE_BARCODES_MUST_BE_ENTERED)
   end
 
+  # Validates that all source barcodes are unique. If any barcodes are
+  # duplicated, an error is added to the :source_barcodes attribute.
+  #
+  # @return [void]
   def source_barcodes_must_be_different
     return if minimal_barcodes.size == minimal_barcodes.uniq.size
 
     errors.add(:source_barcodes, SOURCE_BARCODES_MUST_BE_DIFFERENT)
   end
 
+  # Validates that all source plates corresponding to the minimal barcodes exist.
+  # If the number of source plates does not match the number of minimal barcodes,
+  # an error is added to the :source_plates attribute.
+  #
+  # @return [void]
   def source_plates_must_exist
     return if source_plates.size == minimal_barcodes.size
 
     errors.add(:source_plates, SOURCE_PLATES_MUST_EXIST)
   end
 
+  # Validates that the number of calculated pools does not exceed the configured
+  # number of pools. If the number of calculated pools is greater, an error is
+  # added to the :source_plates attribute.
+  #
+  # @return [void]
   def number_of_pools_must_not_exceed_configured
     return if pools.size <= number_of_pools
 
