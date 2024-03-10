68b1e83d6ad2eb1b88de10153ff830a2980926a9
yoldas <ay6@sanger.ac.uk>
2024-03-10 07:42:40 +0000

    Prettier

--- app/models/labware_creators/donor_pooling_plate.rb
+++ app/models/labware_creators/donor_pooling_plate.rb
@@ -189,9 +189,12 @@ module LabwareCreators
     #
     # @return [Hash] A hash where keys are wells and values are tag depths.
     def tag_depth_hash
-      @tag_depth_hash ||= pools.each_with_index.with_object({}) do |(pool, _pool_index), hash|
-        pool.each_with_index { |well, index| hash[well] = (index + 1).to_s }
-      end
+      @tag_depth_hash ||=
+        pools
+          .each_with_index
+          .with_object({}) do |(pool, _pool_index), hash|
+            pool.each_with_index { |well, index| hash[well] = (index + 1).to_s }
+          end
     end
 
     # Builds the pools for the destination plate. The wells are first grouped
