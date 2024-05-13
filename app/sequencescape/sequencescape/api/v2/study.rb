39da6f87f61ca5fa2d177e7e38ae0bc0c3196c9d
yoldas <ay6@sanger.ac.uk>
2024-05-13 14:23:08 +0100

    Add Study method to return poly_metadatum by key

--- app/sequencescape/sequencescape/api/v2/study.rb
+++ app/sequencescape/sequencescape/api/v2/study.rb
@@ -1,5 +1,15 @@
 # frozen_string_literal: true
 
+# Represents a Study in Limber via the Sequencescape API
 class Sequencescape::Api::V2::Study < Sequencescape::Api::V2::Base
   include Sequencescape::Api::V2::Shared::HasPolyMetadata
+
+  # Returns the PolyMetadatum object with a matching key from poly_metadata
+  # of this Study.
+  #
+  # @param key [String] the key of the PolyMetadatum to find
+  # @return [PolyMetadatum, nil] the found PolyMetadatum object, or nil if no match is found
+  def poly_metadatum_by_key(key)
+    poly_metadata.find { |pm| pm.key == key }
+  end
 end
