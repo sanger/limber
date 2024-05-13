1029df5662cc2148a0ff555a18e11abd2282a8b7
yoldas <ay6@sanger.ac.uk>
2024-05-13 14:25:44 +0100

    Add study_with_poly_metadata factory

--- spec/factories/study_factories.rb
+++ spec/factories/study_factories.rb
@@ -10,5 +10,23 @@ FactoryBot.define do
     name { 'Test Study' }
 
     uuid { SecureRandom.uuid }
+
+    factory :study_with_poly_metadata do
+      transient { poly_metadata { [] } }
+
+      after(:build) do |study, evaluator|
+        # initialise the poly_metadata array
+        study.poly_metadata = []
+
+        # add each polymetadatum to the study
+        evaluator.poly_metadata.each do |pm|
+          # set the relationship between the polymetadatum and the study
+          pm.relationships.metadatable = study
+
+          # link the polymetadatum to the study
+          study.poly_metadata.push(pm)
+        end
+      end
+    end
   end
 end
