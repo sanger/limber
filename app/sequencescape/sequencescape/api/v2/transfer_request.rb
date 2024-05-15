d58b2c23d7de91e77ce220186ab974b1695db824
yoldas <ay6@sanger.ac.uk>
2024-05-15 11:12:33 +0100

    Removed the has_one association with Receptacle as it broke cases when a Well is expected, particularly the name attribute turned out missing

--- app/sequencescape/sequencescape/api/v2/transfer_request.rb
+++ app/sequencescape/sequencescape/api/v2/transfer_request.rb
@@ -2,8 +2,4 @@
 
 # Transfer Requests track transfers between two assets
 class Sequencescape::Api::V2::TransferRequest < Sequencescape::Api::V2::Base
-
-  # @!attribute [r] source_asset
-  #   @return [Sequencescape::Api::V2::Receptacle] the source Receptacle
-  has_one :source_asset, class_name: 'Sequencescape::Api::V2::Receptacle'
 end
