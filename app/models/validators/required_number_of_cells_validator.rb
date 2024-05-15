e321170714bac16e8cff34da9e518fbeedf3668d
yoldas <ay6@sanger.ac.uk>
2024-05-15 15:48:15 +0100

    Add guard to limit the validation to pending state

--- app/models/validators/required_number_of_cells_validator.rb
+++ app/models/validators/required_number_of_cells_validator.rb
@@ -18,11 +18,22 @@ module Validators
       'The required number of cells is not configured for all studies ' \
         'going into pooling on this plate. If not provided, the default ' \
         'value 5000 will be used for the samples of the following studies: ' \
-        '%s. This value can be configured by study in Sequencescape. After ' \
-        'editing the study with the appropriate value, please download ' \
-        'the driver file again to get the updated volumes.'
+        '%s. This value can be configured by study in Sequencescape. If you ' \
+        'edit the study with an appropriate value, please download the ' \
+        'driver file again to get the updated volumes.'
 
+    # Checks if the required number of cells is configured for all studies
+    # going into pooling. If not, it adds an error message to the presenter,
+    # which is displayed to the user as a warning. It does not prevent the
+    # user proceeding with the pooling. The validation is applied only when
+    # the labware is in the pending state.
+    #
+    # @param presenter [Object] the presenter object
+    # @return [void]
     def validate(presenter)
+      # Only validate in the pending state which is where pooling is relevant.
+      return unless presenter.labware.state == 'pending'
+
       studies = source_studies_without_required_number_of_cells(presenter)
       return if studies.empty?
 
