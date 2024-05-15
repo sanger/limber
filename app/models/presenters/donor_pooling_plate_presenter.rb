403b7ec3486872831a84bbcfebd10730f84eef77
yoldas <ay6@sanger.ac.uk>
2024-05-15 11:09:01 +0100

    Add comment for donor pooling presenter

--- app/models/presenters/donor_pooling_plate_presenter.rb
+++ app/models/presenters/donor_pooling_plate_presenter.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 module Presenters
+  # Presenter for the scRNA Core donor pooling plate to validate the required
+  # number of cells by study. If other features are necessary in the presenter,
+  # they can be added here or the validation can be moved to the new one.
   class DonorPoolingPlatePresenter < StandardPresenter
     validates_with Validators::RequiredNumberOfCellsValidator
   end
