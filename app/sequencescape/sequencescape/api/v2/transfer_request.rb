2787a199f237d0d64feb1e937cf2d1d0d5f8502b
yoldas <ay6@sanger.ac.uk>
2024-05-15 07:46:51 +0100

    Add source_asset association on TransferRequest resource

--- app/sequencescape/sequencescape/api/v2/transfer_request.rb
+++ app/sequencescape/sequencescape/api/v2/transfer_request.rb
@@ -2,4 +2,8 @@
 
 # Transfer Requests track transfers between two assets
 class Sequencescape::Api::V2::TransferRequest < Sequencescape::Api::V2::Base
+
+  # @!attribute [r] source_asset
+  #   @return [Sequencescape::Api::V2::Receptacle] the source Receptacle
+  has_one :source_asset, class_name: 'Sequencescape::Api::V2::Receptacle'
 end
