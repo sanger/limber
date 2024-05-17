7f5b0f4745273e19865e8701fdb174dec82a8dbf
Andrew Sparkes <as28@sanger.ac.uk>
2024-05-17 11:41:26 +0100

    added presenter for pooled wells in scRNA, with new samples tab contents

---
+++ app/models/presenters/pooled_wells_plate_presenter.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+module Presenters
+  #
+  # The PooledWellsPlatePresenter overrides the way the pooling tab is displayed.
+  #
+  #
+  class PooledWellsPlatePresenter < StandardPresenter
+    # The pooling tab is not relevant for this presenter as the wells are already pooled (tab shows future pooling
+    # by submission strategy)
+    self.pooling_tab = nil
+
+    # Override the samples tab to display additional sample information for the pooled wells
+    self.samples_partial = 'plates/pooled_wells_samples_tab'
+  end
+end
