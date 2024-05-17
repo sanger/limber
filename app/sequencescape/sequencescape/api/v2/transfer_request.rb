1761f09b9963270594638c7d61b1c009f134f6ae
Andrew Sparkes <as28@sanger.ac.uk>
2024-05-17 14:35:23 +0100

    reverted specifying relationships as caused test failures

--- app/sequencescape/sequencescape/api/v2/transfer_request.rb
+++ app/sequencescape/sequencescape/api/v2/transfer_request.rb
@@ -2,7 +2,4 @@
 
 # Transfer Requests track transfers between two assets
 class Sequencescape::Api::V2::TransferRequest < Sequencescape::Api::V2::Base
-  has_one :source_asset, class_name: 'Sequencescape::Api::V2::Receptacle'
-  has_one :target_asset, class_name: 'Sequencescape::Api::V2::Receptacle'
-  has_one :submission, class_name: 'Sequencescape::Api::V2::Submission'
 end
