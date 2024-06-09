c466042a552e272847f5a2e8db722b1cfb86b3dc
yoldas <ay6@sanger.ac.uk>
2024-06-10 00:05:34 +0100

    Add labware creator tests for columns to rows reordering

---
+++ spec/models/labware_creators/stamped_plate_reordering_columns_to_rows_spec.rb
@@ -0,0 +1,116 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe LabwareCreators::StampedPlateReorderingColumnsToRows do
+  has_a_working_api
+
+  let(:user_uuid) { 'user-uuid' }
+
+  let(:paren_purpose_uuid) { 'parent-purpose' }
+  let(:parent_purpose_name) { 'Parent Purpose' }
+
+  let(:child_purpose_uuid) { 'child-purpose' }
+  let(:child_purpose_name) { 'Child Purpose' }
+
+  let(:parent_uuid) { 'parent-uuid' }
+
+  let(:parent_wells) do
+    locations = WellHelpers.column_order(96, rows: 8, columns: 12)
+    locations.take(8).map { |location| create(:v2_well, location: location, aliquot_count: 2, state: 'passed') }
+  end
+
+  let(:parent) { create(:v2_plate, size: 96, uuid: parent_uuid, wells: parent_wells) }
+
+  let(:child_wells) do
+    locations = WellHelpers.row_order(8, rows: 1, columns: 8)
+    locations.take(8).map { |location| create(:v2_well, location: location, aliquot_count: 0) }
+  end
+
+  let(:child_uuid) { 'child-uuid' }
+
+  let(:child_plate) do
+    create(:v2_plate, size: 8, number_of_rows: 1, number_of_columns: 8, wells: child_wells, uuid: child_uuid)
+  end
+
+  let(:form_attributes) { { purpose_uuid: child_purpose_uuid, parent_uuid: parent_uuid, user_uuid: user_uuid } }
+
+  subject { described_class.new(api, form_attributes) }
+
+  before do
+    allow(Sequencescape::Api::V2::Plate).to receive(:find_by).with(uuid: parent_uuid).and_return(parent)
+
+    allow(api).to receive_message_chain(:plate_creation, :create!)
+      .with(parent: parent_uuid, child_purpose: child_purpose_uuid, user: user_uuid)
+      .and_return(double(child: child_plate))
+
+    allow(Sequencescape::Api::V2::Plate).to receive(:find_by).with(uuid: child_uuid).and_return(child_plate)
+  end
+
+  describe '#labware_wells' do
+    it 'returns the parent wells in column order' do
+      expect(subject.labware_wells).to eq(parent.wells_in_columns)
+    end
+  end
+
+  describe '#request_hash' do
+    it 'returns request hash by reordering columns to rows' do
+      parent.wells_in_columns.each_with_index do |source_well, index|
+        submission_id = source_well.aliquots.first.request.submission_id # Same for all aliquots in the well
+        additional_parameters = { 'submission_id' => submission_id }
+
+        request_hash = subject.request_hash(source_well, child_plate, additional_parameters)
+
+        expect(request_hash['source_asset']).to eq(source_well.uuid)
+        expect(request_hash['target_asset']).to eq(child_plate.wells_in_rows[index].uuid)
+        expect(request_hash['submission_id']).to eq(submission_id)
+      end
+    end
+  end
+
+  describe '#save' do
+    # Expected transferred source wells
+    let(:transferred_wells) { parent_wells }
+
+    # Expected transfer requests from source wells to child wells
+    let(:transfer_requests) do
+      transferred_wells.map.with_index do |source_well, index|
+        submission_id = source_well.aliquots.first.request.submission_id
+        {
+          'source_asset' => source_well.uuid,
+          'target_asset' => child_plate.wells_in_rows[index].uuid,
+          'submission_id' => submission_id
+        }
+      end
+    end
+
+    let(:expect_transfer_requests) do
+      expect(api).to receive_message_chain(:transfer_request_collection, :create!).with(
+        user: user_uuid,
+        transfer_requests: transfer_requests
+      )
+      subject.save!
+    end
+
+    context 'with all source wells' do
+      it 'creates transfer requests by ordering columns to rows' do
+        # A1 -> A1, B1 -> A2, C1 -> A3, D1 -> A4, E1 -> A5, F1 -> A6, G1 -> A7, H1 -> A8
+        expect_transfer_requests
+      end
+    end
+
+    context 'with some source wells failed' do
+      let(:transferred_wells) do
+        # Fail 1st, 5th, and last wells
+        failed_wells = [parent_wells[0], parent_wells[4], parent_wells[-1]]
+        failed_wells.each { |well| well.state = 'failed' }
+        parent_wells - failed_wells # Exclude the failed wells
+      end
+
+      it 'compresses the wells in the child plate' do
+        # B1 -> A1, C1 -> A2, D1 -> A3, F1 -> A4, G1 -> A5
+        expect_transfer_requests
+      end
+    end
+  end
+end
