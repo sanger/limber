31e049f568cc0ce1ac739907619727702f7c99de
yoldas <ay6@sanger.ac.uk>
2024-06-20 01:03:05 +0100

    Add concern method to check if a feature is enabled in configured states

---
+++ app/models/concerns/presenters/statemachine/feature_in_states.rb
@@ -0,0 +1,77 @@
+# frozen_string_literal: true
+
+module Presenters::Statemachine
+  # This module provides a method to determine if a feature can be enabled
+  # based on the current state of the instance.
+  module FeatureInStates
+    extend ActiveSupport::Concern
+
+    included do
+      # Determines if a feature can be enabled based on provided states.
+      #
+      # @param states [String, Symbol, Array, Hash] The states to check against.
+      # This can be a single state as a String or Symbol, an Array of states,
+      # or a Hash with includes and excludes keys for states.
+      # @param default [Boolean] The default return value if no specific
+      # condition is met.
+      # @return [Boolean] Returns true if the feature can be enabled based on
+      # the states parameter, otherwise returns the default.
+      #
+      # :reek:BooleanParameter
+      # :reek:ManualDispatch
+      # :reek:TooManyStatements
+      def can_be_enabled?(states, default: true)
+        return default unless respond_to?(:state) # No state method.
+        return default if states.blank? # No states to check.
+
+        includes, excludes = parse_states(states)
+
+        current_state = state.to_s
+
+        # Exludes take precedence over includes.
+        return false if excludes.present? && excludes.include?(current_state)
+
+        # If includes is present, the current state must be in the list.
+        includes.present? ? includes.include?(current_state) : default
+      end
+
+      private
+
+      # Parses the states parameter into includes and excludes arrays.
+      #
+      # @param states [String, Symbol, Array, Hash] The states to check against.
+      # @return [Array] Returns an array with two elements: includes and excludes.
+      #
+      # :reek:TooManyStatements
+      def parse_states(states)
+        case states
+        when Hash
+          states = states.stringify_keys
+          includes = parse_tokens(states['includes'])
+          excludes = parse_tokens(states['excludes'])
+        else
+          includes = parse_tokens(states)
+          excludes = []
+        end
+        [includes, excludes]
+      end
+
+      # Parses the state name tokens into an array of strings.
+      #
+      # @param tokens [String, Symbol, Array] The tokens to parse.
+      # @return [Array] Returns an array of strings.
+      #
+      # :reek:UtilityFunction { public_methods_only: true}
+      def parse_tokens(tokens)
+        case tokens
+        when String, Symbol
+          [tokens.to_s] # single
+        when Array
+          tokens.map(&:to_s) # multiple
+        else
+          [] # none
+        end
+      end
+    end
+  end
+end
