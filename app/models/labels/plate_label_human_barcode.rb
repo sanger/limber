2ff59f8015209df7b03dad976246bacce8fa9df8
Shiv <44001656+SHIV5T3R@users.noreply.github.com>
2024-06-24 10:50:24 +0100

    Added a new label class to scan human readable barcodes

---
+++ app/models/labels/plate_label_human_barcode.rb
@@ -0,0 +1,26 @@
+
+# frozen_string_literal: true
+
+class Labels::PlateLabelHumanBarcode < Labels::Base # rubocop:todo Style/Documentation
+  def attributes
+    {
+      top_left: date_today,
+      bottom_left: labware.barcode.human,
+      top_right: workline_identifier,
+      bottom_right: [labware.role, labware.purpose_name].compact.join(' '),
+      barcode: labware.barcode.human
+    }
+  end
+
+  def default_printer_type
+    default_printer_type_for(:plate_96_2d)
+  end
+
+  def default_label_template
+    default_label_template_for(:plate_96_2d)
+  end
+
+  def default_sprint_label_template
+    default_sprint_label_template_for(:plate_96_2d)
+  end
+end
