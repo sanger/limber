06e58ecf403f76b5f7caf0bfe8e3f97457beeb21
yoldas <ay6@sanger.ac.uk>
2024-06-26 14:04:04 +0100

    Remove csv_file_links override from StandardPresenter

--- app/models/presenters/standard_presenter.rb
+++ app/models/presenters/standard_presenter.rb
@@ -11,27 +11,5 @@ module Presenters
 
     validates_with Validators::SuboptimalValidator
     validates_with Validators::ActiveRequestValidator
-
-    # Returns the CSV file links for the plate based on the configured states.
-    #
-    # @return [Array<Array<String, Array>>] the CSV file links
-    def csv_file_links
-      links =
-        purpose_config
-          .fetch(:file_links, [])
-          .select { |link| can_be_enabled?(link&.states) }
-          .map do |link|
-            [
-              link.name,
-              [
-                :limber_plate,
-                :export,
-                { id: link.id, limber_plate_id: human_barcode, format: :csv, **link.params || {} }
-              ]
-            ]
-          end
-      links << ['Download Worksheet CSV', { format: :csv }] if csv.present?
-      links
-    end
   end
 end
