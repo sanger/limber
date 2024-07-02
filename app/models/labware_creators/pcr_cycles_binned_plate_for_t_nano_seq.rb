45985b6a8210514e7685fc6f2959df2351106f76
Andrew Sparkes <as28@sanger.ac.uk>
2024-07-02 11:58:09 +0100

    Modified validations to check that valid rows in file match to requests in submission, and rows with pcr_cycles = 1 are ignored

--- app/models/labware_creators/pcr_cycles_binned_plate_for_t_nano_seq.rb
+++ app/models/labware_creators/pcr_cycles_binned_plate_for_t_nano_seq.rb
@@ -21,7 +21,7 @@ module LabwareCreators
       # to it based on the details in the customer file
       child_wells_by_location = child_v2_plate.wells.index_by(&:location)
 
-      well_details.each do |parent_location, details|
+      filtered_well_details_without_wells_flagged_to_be_ignored.each do |parent_location, details|
         child_well_location = transfer_hash[parent_location]['dest_locn']
         child_well = child_wells_by_location[child_well_location]
 
@@ -70,14 +70,14 @@ module LabwareCreators
 
     private
 
-    # to fetch the expected binning request type from the purpose config
+    # Fetches the expected binning request type from the purpose config
     def expected_binning_request_type
       @expected_binning_request_type ||= purpose_config.fetch('expected_binning_request_type')
     end
 
-    # check the parent plate only contains requests of the expected type and no others
+    # Checks the parent plate only contains requests of the expected type and no others
     def parent_contains_active_requests_of_expected_type
-      # disabled as request_type.key does not work
+      # had to disable rubocop here as the suggested request_type.key does not work
       # rubocop:disable Performance/MapMethodChain
       request_type_keys = filtered_wells.flat_map { |well| well.active_requests.map(&:request_type).map(&:key) }.uniq
 
@@ -94,21 +94,50 @@ module LabwareCreators
       )
     end
 
+    # Filter out those well details rows from the file with pcr_cycles set to 1, customer does
+    # not want these to go forward
+    def filtered_well_details_without_wells_flagged_to_be_ignored
+      well_details.reject { |_key, hash| hash['pcr_cycles'].to_s == '1' }
+    end
+
+    # Returns list of well locations e.g. ['A1', 'B1' etc.], excluding any rows flagged to be ignored
+    def well_locns_from_filtered_well_details
+      @well_locns_from_filtered_well_details ||= filtered_well_details_without_wells_flagged_to_be_ignored.keys.sort
+    end
+
+    # Returns the list of parent wells with submission requests ie. wells submitted to progress
+    def parent_wells_with_requests
+      @parent_wells_with_requests ||= filtered_wells.map(&:location).sort
+    end
+
+    # Validates that the customer file contains values for all the wells with active requests on the parent plate
     def customer_file_contains_values_for_all_active_requests
-      # parent wells with requests
-      parent_wells_with_requests = filtered_wells.map(&:location).sort
+      return if parent_wells_with_requests == well_locns_from_filtered_well_details
 
-      # wells with details in the customer file
-      wells_with_details = well_details.keys.sort
-
-      # check the two arrays match (same elements in same order)
-      return if parent_wells_with_requests == wells_with_details
+      msg_list = generate_msg_list_for_file_not_matching_submission
 
       errors.add(
         :base,
-        "The uploaded customer file does not contain the same number of rows (#{wells_with_details.count}) " \
-          "as there are wells with active requests on the parent plate (#{parent_wells_with_requests.count})"
+        'The uploaded Customer file does not contain the same number of valid rows ' \
+          "(#{well_locns_from_filtered_well_details.count}) " \
+          "as there are wells submitted for work on the parent plate (#{parent_wells_with_requests.count}). " \
+          "Please check the Customer file vs the Submission for missing or extra rows. #{msg_list.join(', ')}"
       )
+    end
+
+    # Generates a list of messages for each well that is present in the submission but not in the customer file
+    # or vice versa
+    def generate_msg_list_for_file_not_matching_submission
+      parent_wells = parent.wells.map(&:location)
+      parent_wells.filter_map do |well_locn|
+        well_in_submission = parent_wells_with_requests.include?(well_locn)
+        valid_well_row_in_file = well_locns_from_filtered_well_details.include?(well_locn)
+
+        next if well_in_submission == valid_well_row_in_file
+
+        "Well #{well_locn} - File: #{valid_well_row_in_file ? 'present' : 'missing'}, " \
+          "Submission: #{well_in_submission ? 'present' : 'missing'}"
+      end
     end
 
     # The well filter will be used to identify the parent wells to be taken forward.
@@ -117,10 +146,12 @@ module LabwareCreators
       @well_filter ||= WellFilterAllowingPartials.new(creator: self, request_state: 'pending')
     end
 
+    # Check for existing metadata on the request object with the same key
     def find_existing_metadata(metadata_key, request_id)
       Sequencescape::Api::V2::PolyMetadatum.find(key: metadata_key, metadatable_id: request_id).first
     end
 
+    # Update the existing metadata value if it is different
     def update_existing_metadata(existing_metadata_v2, metadata_value, metadata_key, child_well_location)
       return if existing_metadata_v2.value == metadata_value
 
@@ -131,6 +162,7 @@ module LabwareCreators
               "could not be updated for request at child well location #{child_well_location}"
     end
 
+    # Create a new metadata for the request object
     def create_new_metadata(metadata_key, metadata_value, request, child_well_location)
       pm_v2 = Sequencescape::Api::V2::PolyMetadatum.new(key: metadata_key, value: metadata_value)
       pm_v2.relationships.metadatable = request
@@ -142,7 +174,7 @@ module LabwareCreators
               "did not save for request at child well location #{child_well_location}"
     end
 
-    # filename for the customer file upload
+    # Filename for the customer file upload (inherits from the base class - cannot reference the constant directly)
     def customer_filename
       CUSTOMER_FILENAME
     end
