c42c16598f0cec3375cb6538d11e39b6c5dc8a12
yoldas <ay6@sanger.ac.uk>
2024-07-08 21:29:29 +0100

    Update test for cell count utility

--- spec/models/utility/cell_count_spot_checking_spec.rb
+++ spec/models/utility/cell_count_spot_checking_spec.rb
@@ -40,123 +40,27 @@ RSpec.describe Utility::CellCountSpotChecking do
     end
   end
 
-  describe '#select_wells' do
-    context 'when the number of ancestor tubes is 4' do
-      it 'selects wells up to count if specified' do
-        # ['A1', 'B1', 'C1', 'D1', 'E1', 'F1', 'G1', 'H1', 'A2', 'B2', 'C2', 'D2']
-        # ->
-        # ["A1", "D1", "G1", "B2"]
-        count = 4
-        result = subject.select_wells(count)
-        expect(result.size).to eq(count)
-        expect(result.map(&:location)).to eq(%w[A1 D1 G1 B2])
-      end
-
-      it 'selects wells up to the number of ancestor tubes if count is not specified' do
-        # ['A1', 'B1', 'C1', 'D1', 'E1', 'F1', 'G1', 'H1', 'A2', 'B2', 'C2', 'D2']
-        # ->
-        # ["A1", "D1", "G1", "B2"]
-        result = subject.select_wells
-        expect(result.size).to eq(ancestor_tubes.size)
-        expect(result.map(&:location)).to eq(%w[A1 D1 G1 B2])
-      end
+  describe '#first_replicates' do
+    it 'returns the first wells for each of all ancestor tubes' do
+      # ['A1', 'B1', 'C1', 'D1', 'E1', 'F1', 'G1', 'H1', 'A2', 'B2', 'C2', 'D2']
+      # ->
+      # ["A1", "D1", "G1", "B2"]
+      result = subject.first_replicates
+      expect(result.size).to eq(ancestor_tubes.size)
+      selected_wells = plate.wells_in_columns.reject(&:empty?).map(&:location).each_slice(3).map(&:first)
+      expect(result.map(&:location)).to eq(selected_wells)
     end
+  end
 
-    context 'when the number of ancestor tubes is 6' do
-      let(:number_of_tubes) { 6 }
-
-      it 'selects wells up to count if specified' do
-        count = 4 # lower than the number of ancestor tubes
-        result = subject.select_wells(count)
-
-        # ['A1', 'B1', 'C1', 'D1', 'E1', 'F1', 'G1', 'H1',
-        #  'A2', 'B2', 'C2', 'D2', 'E2', 'F2', 'G2', 'H2',
-        #  'A3', 'B3']
-        #  ->
-        # ["A1", "G1", "E2", "H2"]
-
-        expect(result.size).to eq(count)
-        expect(result.map(&:location)).to eq(%w[A1 G1 E2 H2])
-      end
-
-      it 'selects wells up to the number of ancestor tubes if count is not specified' do
-        result = subject.select_wells
-
-        # ['A1', 'B1', 'C1', 'D1', 'E1', 'F1', 'G1', 'H1',
-        #  'A2', 'B2', 'C2', 'D2', 'E2', 'F2', 'G2', 'H2',
-        #  'A3', 'B3']
-        #  ->
-        # ["A1", "D1", "G1", "B2", "E2", "H2"]
-
-        expect(result.size).to eq(ancestor_tubes.size)
-        expect(result.map(&:location)).to eq(%w[A1 D1 G1 B2 E2 H2])
-      end
-    end
-
-    context 'when the number of ancestor tubes is 8' do
-      let(:number_of_tubes) { 8 }
-
-      it 'selects wells up to count if specified' do
-        count = 4 # lower than the number of ancestor tubes
-        result = subject.select_wells(count)
-
-        # ['A1', 'B1', 'C1', 'D1', 'E1', 'F1', 'G1', 'H1',
-        #  'A2', 'B2', 'C2', 'D2', 'E2', 'F2', 'G2', 'H2',
-        #  'A3', 'B3']
-        #  ->
-        # ["A1", "B2", "H2", "F3"]
-
-        expect(result.size).to eq(count)
-        expect(result.map(&:location)).to eq(%w[A1 B2 H2 F3])
-      end
-
-      it 'selects wells up to the number of ancestor tubes if count is not specified' do
-        result = subject.select_wells
-
-        # ['A1', 'B1', 'C1', 'D1', 'E1', 'F1', 'G1', 'H1',
-        #  'A2', 'B2', 'C2', 'D2', 'E2', 'F2', 'G2', 'H2',
-        #  'A3', 'B3']
-        #  ->
-        # ["A1", "D1", "G1", "B2", "E2", "H2", "C3", "F3"]
-
-        expect(result.size).to eq(ancestor_tubes.size)
-        expect(result.map(&:location)).to eq(%w[A1 D1 G1 B2 E2 H2 C3 F3])
-      end
-    end
-
-    context 'when the number of ancestor tubes is 12' do
-      let(:number_of_tubes) { 12 }
-
-      it 'selects wells up to count if specified' do
-        count = 6 # lower than the number of ancestor tubes
-        result = subject.select_wells(count)
-
-        # ['A1', 'B1', 'C1', 'D1', 'E1', 'F1', 'G1', 'H1',
-        #  'A2', 'B2', 'C2', 'D2', 'E2', 'F2', 'G2', 'H2',
-        #  'A3', 'B3', 'C3', 'D3', 'E3', 'F3', 'G3', 'H3',
-        #  'A4', 'B4', 'C4', 'D4', 'E4', 'F4', 'G4', 'H4',
-        #  'A5', 'B5', 'C5', 'D5']
-        # ->
-        # ["A1", "B2", "H2", "F3", "D4", "B5"]
-
-        expect(result.size).to eq(count)
-        expect(result.map(&:location)).to eq(%w[A1 B2 H2 F3 D4 B5])
-      end
-
-      it 'selects wells up to the number of ancestor tubes if count is not specified' do
-        result = subject.select_wells
-
-        # ['A1', 'B1', 'C1', 'D1', 'E1', 'F1', 'G1', 'H1',
-        #  'A2', 'B2', 'C2', 'D2', 'E2', 'F2', 'G2', 'H2',
-        #  'A3', 'B3', 'C3', 'D3', 'E3', 'F3', 'G3', 'H3',
-        #  'A4', 'B4', 'C4', 'D4', 'E4', 'F4', 'G4', 'H4',
-        #  'A5', 'B5', 'C5', 'D5']
-        # ->
-        # ["A1", "D1", "G1", "B2", "E2", "H2", "C3", "F3", "A4", "D4", "G4", "B5"]
-
-        expect(result.size).to eq(ancestor_tubes.size)
-        expect(result.map(&:location)).to eq(%w[A1 D1 G1 B2 E2 H2 C3 F3 A4 D4 G4 B5])
-      end
+  describe '#second_replicates' do
+    it 'returns the second wells for each of all ancestor tubes' do
+      # ['A1', 'B1', 'C1', 'D1', 'E1', 'F1', 'G1', 'H1', 'A2', 'B2', 'C2', 'D2']
+      # ->
+      # ["B1", "E1", "H1", "C2"]
+      result = subject.second_replicates
+      expect(result.size).to eq(ancestor_tubes.size)
+      selected_wells = plate.wells_in_columns.reject(&:empty?).map(&:location).each_slice(3).map(&:second)
+      expect(result.map(&:location)).to eq(selected_wells)
     end
   end
 end
