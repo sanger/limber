eb3c0ed4f9017a90183c75b867823044b2681596
Stephen Hulme <sh51@sanger.ac.uk>
2024-07-19 15:57:36 +0100

    build: update javascript directories
    Entrypoints only: app/frontend/entrypoints/ (lint only)
    Source code: app/frontend/javascript/ (lint and test)

--- jest.config.js
+++ jest.config.js
@@ -105,7 +105,7 @@ module.exports = {
   // restoreMocks: false,
 
   // The root directory that Jest should scan for tests and modules within
-  rootDir: 'app/javascript',
+  rootDir: 'app/frontend/javascript',
 
   // A list of paths to directories that Jest should use to search for files in
   // roots: [
