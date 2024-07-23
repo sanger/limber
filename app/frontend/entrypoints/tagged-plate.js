9f7b4a83c558d2f8bad5a3d82ad04105b00666eb
Stephen Hulme <sh51@sanger.ac.uk>
2024-07-23 17:37:50 +0100

    refactor: remove jQuery from tagged-plate script

--- app/frontend/entrypoints/tagged-plate.js
+++ app/frontend/entrypoints/tagged-plate.js
@@ -1,11 +1,9 @@
-import $ from 'jquery'
-
 const configElement = document.getElementById('labware-creator-config')
 const tagPlatesList = JSON.parse(configElement.dataset['tagPlatesList'])
 const dualRequired = configElement.dataset['dual-required'] === 'true'
 const enforceSameTemplateWithinPool = configElement.dataset['enforce-same-template-within-pool'] === 'true'
 
-$.extend(SCAPE, {
+Object.assign(SCAPE, {
   tag_plates_list: tagPlatesList,
   dualRequired: dualRequired,
   enforceSameTemplateWithinPool: enforceSameTemplateWithinPool,
