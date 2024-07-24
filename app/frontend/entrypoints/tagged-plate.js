c3b8b8471af905c95abbbacc4e5646d4b1db3bfe
Stephen Hulme <sh51@sanger.ac.uk>
2024-07-24 11:51:47 +0100

    fix: retrieve dataset attributes using camelcase

--- app/frontend/entrypoints/tagged-plate.js
+++ app/frontend/entrypoints/tagged-plate.js
@@ -1,7 +1,7 @@
 const configElement = document.getElementById('labware-creator-config')
-const tagPlatesList = JSON.parse(configElement.dataset['tagPlatesList'])
-const dualRequired = configElement.dataset['dual-required'] === 'true'
-const enforceSameTemplateWithinPool = configElement.dataset['enforce-same-template-within-pool'] === 'true'
+const tagPlatesList = JSON.parse(configElement.dataset.tagPlatesList)
+const dualRequired = configElement.dataset.dualRequired === 'true'
+const enforceSameTemplateWithinPool = configElement.dataset.enforceSameTemplateWithinPool === 'true'
 
 Object.assign(SCAPE, {
   tag_plates_list: tagPlatesList,
