572c56ea838b3397b5a34709c066ab8137e809a1
Stephen Hulme <sh51@sanger.ac.uk>
2024-07-30 09:40:28 +0100

    style: lint

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ require 'rails'
   'action_mailer/railtie',
   'active_job/railtie',
   'action_cable/engine',
-  'rails/test_unit/railtie',
+  'rails/test_unit/railtie'
 ].each do |railtie|
   require railtie.to_s
 rescue LoadError
