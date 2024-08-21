f3ec8e62f6e3ab341d130d8dc798b4759fd0e1fc
Stephen Hulme <sh51@sanger.ac.uk>
2024-08-21 17:08:57 +0100

    refactor: remove IIFE from state-change-reasons module

--- app/frontend/javascript/state_change_reasons.js
+++ app/frontend/javascript/state_change_reasons.js
@@ -1,19 +1,16 @@
-import jQuery from 'jquery'
-;(function ($, _exports, undefined) {
-  'use strict'
+import $ from 'jquery'
 
-  let updateDisplay = function (new_state, speed) {
-    $('.reason')
-      .not('#' + new_state + '_reasons')
-      .slideUp(speed)
-    $('#' + new_state + '_reasons').slideDown(speed)
-  }
+let updateDisplay = function (new_state, speed) {
+  $('.reason')
+    .not('#' + new_state + '_reasons')
+    .slideUp(speed)
+  $('#' + new_state + '_reasons').slideDown(speed)
+}
 
-  let displayReason = function () {
-    updateDisplay(this.value, 'fast')
-  }
+let displayReason = function () {
+  updateDisplay(this.value, 'fast')
+}
 
-  $(function () {
-    $('#state-changer').find('input[type=radio]').on('change', displayReason)
-  })
-})(jQuery, window)
+$(function () {
+  $('#state-changer').find('input[type=radio]').on('change', displayReason)
+})
