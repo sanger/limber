26f056db9039d051845cd344094e0edafbc91e4c
Stephen Hulme <sh51@sanger.ac.uk>
2024-08-22 15:51:45 +0100

    build: consolidate eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,13 @@
 module.exports = {
   env: {
+    'vitest/env': true,
     browser: true,
-    es6: true,
-    jasmine: true,
     node: true,
   },
-  plugins: ['vue'],
+  plugins: ['vitest', 'vue'],
   extends: ['eslint:recommended', 'plugin:vue/recommended', 'prettier'],
   parserOptions: {
+    ecmaVersion: 'latest',
     sourceType: 'module',
     requireConfigFile: false,
   },
@@ -23,6 +23,9 @@ module.exports = {
       },
     ],
     'no-var': 'error',
+    // We need a proper logging solution (see https://github.com/sanger/limber/issues/836),
+    // but until then:
+    'no-console': ['error', { allow: ['warn', 'error', 'log'] }],
     // Legacy in from the old days. We should remove these:
     'vue/prop-name-casing': ['warn'],
   },
