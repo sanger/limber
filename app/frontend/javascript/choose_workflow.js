e33d15feedfda4f165da5960caaff874b891259c
Stephen Hulme <sh51@sanger.ac.uk>
2024-08-22 15:36:45 +0100

    refactor: remove IIFE from choose-workflow

--- app/frontend/javascript/choose_workflow.js
+++ app/frontend/javascript/choose_workflow.js
@@ -1,10 +1,3 @@
-import jQuery from 'jquery'
-
 import { disableEnterKeySubmit } from '@/javascript/lib/disable_enter_key_submit.js'
-;(function ($, _exports, undefined) {
-  'use strict'
 
-  $(function (_event) {
-    disableEnterKeySubmit('#choose_workflow_card', '#submission_forms')
-  })
-})(jQuery, window)
+disableEnterKeySubmit('#choose_workflow_card', '#submission_forms')
