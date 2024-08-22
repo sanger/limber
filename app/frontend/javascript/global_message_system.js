5e3bf682b4a5ed55dc0ac8752b5a7a48e2a14243
Stephen Hulme <sh51@sanger.ac.uk>
2024-08-22 15:33:16 +0100

    refactor: remove IIFE from global-message-system

--- app/frontend/javascript/global_message_system.js
+++ app/frontend/javascript/global_message_system.js
@@ -1,25 +1,22 @@
-import jQuery from 'jquery'
+import $ from 'jquery'
 // Global SCAPE.message method
-;(function ($, exports, undefined) {
-  'use strict'
 
-  if (exports.SCAPE === undefined) {
-    exports.SCAPE = {}
+if (exports.SCAPE === undefined) {
+  exports.SCAPE = {}
+}
+
+exports.SCAPE.message = function (message, status) {
+  if (message == '') {
+    $('#validation_report').empty()
+    return
   }
 
-  exports.SCAPE.message = function (message, status) {
-    if (message == '') {
-      $('#validation_report').empty()
-      return
-    }
-
-    $('#validation_report')
-      .empty()
-      .append(
-        $(document.createElement('div'))
-          .addClass('alert')
-          .addClass('alert-' + status)
-          .text(message)
-      )
-  }
-})(jQuery, window)
+  $('#validation_report')
+    .empty()
+    .append(
+      $(document.createElement('div'))
+        .addClass('alert')
+        .addClass('alert-' + status)
+        .text(message)
+    )
+}
