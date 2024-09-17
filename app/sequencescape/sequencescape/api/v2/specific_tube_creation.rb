3d096e488460e4814521d907b16f227b0a6eadef
Stuart McHattie <sm49@sanger.ac.uk>
2024-09-17 16:42:22 +0100

    Change parents relationship to Labware instead of Asset

--- app/sequencescape/sequencescape/api/v2/specific_tube_creation.rb
+++ app/sequencescape/sequencescape/api/v2/specific_tube_creation.rb
@@ -3,6 +3,6 @@
 # Represents a specific tube creation in Limber via the Sequencescape API
 class Sequencescape::Api::V2::SpecificTubeCreation < Sequencescape::Api::V2::Base
   has_many :children, class_name: 'Sequencescape::Api::V2::Tube'
-  has_many :parents, class_name: 'Sequencescape::Api::V2::Asset'
+  has_many :parents, class_name: 'Sequencescape::Api::V2::Labware'
   has_one :user, class_name: 'Sequencescape::Api::V2::User'
 end
