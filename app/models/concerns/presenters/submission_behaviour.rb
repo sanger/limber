d15c05ecbca9e6de75e42338985d62d85077f3c0
Stephen Hulme <sh51@sanger.ac.uk>
2024-10-14 15:08:55 +0100

    refactor: pull out submission behviour into a separate module

---
+++ app/models/concerns/presenters/submission_behaviour.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+module Presenters::SubmissionBehaviour
+  def each_submission_option
+    purpose_config.submission_options.each do |button_text, options|
+      submission_options = options.to_hash
+      submission_options[:asset_groups] = asset_groups
+      submission_options[:labware_barcode] = labware.labware_barcode.human
+      yield button_text, SequencescapeSubmission.new(submission_options)
+    end
+  end
+
+  def pending_submissions?
+    submissions.any? { |submission| submission.building_in_progress?(ready_buffer: 20.seconds) }
+  end
+
+  def submissions
+    labware.direct_submissions
+  end
+
+  private
+
+  def asset_groups
+    @asset_groups ||=
+      labware
+        .wells
+        .compact_blank
+        .group_by(&:order_group)
+        .map { |_, wells| { assets: wells.map(&:uuid), autodetect_studies: true, autodetect_projects: true } }
+  end
+end
