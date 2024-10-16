d559628cca28c0dfcce33ae78dbcd7854bf3d5d1
Seena Nair <55585488+seenanair@users.noreply.github.com>
2024-10-16 17:38:40 +0100

    feature: Adds icons and new states

--- app/frontend/javascript/pool-xp-tube-panel/components/PoolXPTubeSubmitPanel.vue
+++ app/frontend/javascript/pool-xp-tube-panel/components/PoolXPTubeSubmitPanel.vue
@@ -1,66 +1,115 @@
 <template>
-  <div :class="{ 'bordered-badge p-1': displayStatus }">
+  <div class="d-flex justify-content-between align-items-center">
+    <label
+      id="pool_xp_tube_submit_status"
+      :class="['p-2', 'mt-2', stateStyles.text]"
+      :style="{ fontSize: '1.0rem', display: 'flex', alignItems: 'center' }"
+    >
+      <b-spinner v-if="displaySpinner" id="progress_spinner" small type="border" class="mr-2" />
+      <component :is="statusIcon" v-else class="mr-2" :color="stateStyles.icon" />
+      {{ statusText }}
+    </label>
+
     <b-button
       id="pool_xp_tube_submit_button"
       name="pool_xp_tube_submit_button"
       :disabled="isButtonDisabled"
-      :variant="buttonStyle"
-      :class="{ 'btn-gray': isButtonDisabled }"
-      size="lg"
-      block
+      :variant="stateStyles.button"
+      :class="['w-50']"
       @click="handleSubmit"
     >
       {{ buttonText }}
     </b-button>
-    <b-badge
-      v-if="displayStatus"
-      size="lg"
-      :class="['p-2', 'mt-2', 'd-block', 'badge-light', 'text-info']"
-      :style="{ fontSize: '1.25 rem' }"
-    >
-      <b-spinner v-show="displaySpinner" small type="border" class="mr-2" />
-      Status: {{ statusText }}
-    </b-badge>
   </div>
 </template>
 
 <script>
+import ReadyIcon from '../../icons/ReadyIcon.vue'
+import SuccessIcon from '../../icons/SuccessIcon.vue'
+import ErrorIcon from '../../icons/ErrorIcon.vue'
+import TubeSearchIcon from '../../icons/TubeSearchIcon.vue'
+const maxPollttempts = 10
+const pollInterval = 1000
+
 /**
  * Enum for the possible states of the component
  */
 const StateEnum = {
   INITIAL: 'initial', // The default state or the initial state when component is first loaded
-  FETCHING_TUBE: 'fetching', // The state when the component is checking if the tube exists in Traction
-  REFETCHING_TUBE: 'refetching', // The state when the component is rechecking if the tube exists in Traction
-  TUBE_NOT_FOUND: 'tube_not_found', // The state when the tube is not found in Traction
-  TUBE_FOUND: 'tube_found', // The state when the tube is found in Traction
-  SUBMITTING: 'submitting', // The state when the component is submitting the tube to Traction
-  WAITING: 'waiting', // The state when the component is waiting for Traction to process the tube
-  SUCCESS: 'success', // The state when the tube is successfully added to Traction
-  FAILURE_FETCH_TUBE: 'failure_fetch_tube', // The state when the component fails to fetch the tube from Traction
+  CHECKING_TUBE_EXISTS: 'fetching', // The state when the component is checking if the tube exists in Traction
+  TUBE_ALREADY_EXPORTED: 'tube_exists', // The state when the component is rechecking if the tube exists in Traction
+  POLLING_TUBE: 'polling', // The state when the component is polling Traction to check if the tube is exported
+  TUBE_EXPORT_SUCESS: 'tube_exported', // The state when the tube is successfully exported to Traction
+  FAILURE_POLL_TUBE: 'failure_poll_tube', // The state when the component fails to poll Traction for the tube
+  FAILURE_EXPORT_TUBE: 'failure_export_tube', // The state when the component fails to export the tube to Traction
   FAILURE_ADD_TUBE: 'failure_add_tube', // The state when the component fails to add the tube to Traction
-  INVALID_TRACTION_URL: 'invalid_traction_url', // The state when the Traction URL is invalid
-  INVALID_SEQUENCESCAPE_URL: 'invalid_sequencescape_url', // The state when the Sequencescape URL is invalid
-  INVALID_BARCODE: 'invalid_barcode', //  The state when the barcode is invalid
-  INVALID_USER_ID: 'invalid_user_id', //  The state when the user ID is invalid
+  FAILURE_EXPORT_TUBE_AFTER_RECHECK: 'failure_export_tube_after_recheck', // The state when the component fails to export the tube to Traction after rechecking
+  INVALID_PROPS: 'invalid_props', // The state when the component receives invalid props
 }
 
-const StatusTexts = {
-  [StateEnum.INITIAL]: '',
-  [StateEnum.FETCHING_TUBE]: 'Retrieving tube information from Traction...',
-  [StateEnum.REFETCHING_TUBE]: 'Retrieving tube information from Traction...',
-  [StateEnum.TUBE_FOUND]: 'Tube found in Traction',
-  [StateEnum.SUBMITTING]: 'Submitting tube to Traction...',
-  [StateEnum.SUCCESS]: 'Tube successfully added',
-  [StateEnum.FAILURE_FETCH_TUBE]: 'Unable to retrieve tube from Traction. Please try again',
-  [StateEnum.FAILURE_ADD_TUBE]: 'Unable to add tube. Please try submitting again.',
-  [StateEnum.INVALID_TRACTION_URL]: 'Invalid Traction URL',
-  [StateEnum.INVALID_SEQUENCESCAPE_URL]: 'Invalid Sequencescape URL',
-  [StateEnum.INVALID_BARCODE]: 'Invalid barcode',
-  [StateEnum.INVALID_USER_ID]: 'Invalid user ID',
+const StateData = {
+  [StateEnum.INITIAL]: {
+    statusText: 'Ready to export.',
+    buttonText: 'Export',
+    styles: { button: 'success', text: 'text-success', icon: 'green' },
+    icon: ReadyIcon,
+  },
+  [StateEnum.CHECKING_TUBE_EXISTS]: {
+    statusText: 'Checking tube is in Traction...',
+    buttonText: 'Please wait',
+    styles: { button: 'success', text: 'text-black', icon: 'black' },
+    icon: TubeSearchIcon,
+  },
+  [StateEnum.TUBE_ALREADY_EXPORTED]: {
+    statusText: 'Tube already exported to Traction.',
+    buttonText: 'Open Traction.',
+    styles: { button: 'success', text: 'text-success', icon: 'green' },
+    icon: SuccessIcon,
+  },
+  [StateEnum.POLLING_TUBE]: {
+    statusText: 'Tube is being exported',
+    buttonText: 'Please wait',
+    styles: { button: 'success', text: 'text-black', icon: 'green' },
+    icon: null,
+  },
+  [StateEnum.TUBE_EXPORT_SUCESS]: {
+    statusText: 'Tube has been exported to Traction.',
+    buttonText: 'Open Traction.',
+    styles: { button: 'success', text: 'text-success', icon: 'green' },
+    icon: SuccessIcon,
+  },
+  [StateEnum.FAILURE_POLL_TUBE]: {
+    statusText: 'Unable to check whether tube is in Traction. Try again?',
+    buttonText: 'Refresh',
+    styles: { button: 'warning', text: 'text-warning', icon: 'orange' },
+    icon: ErrorIcon,
+  },
+  [StateEnum.FAILURE_EXPORT_TUBE]: {
+    statusText: 'Unable to send tube to Traction. Try again?',
+    buttonText: 'Retry',
+    styles: { button: 'warning', text: 'text-warning', icon: 'warning' },
+    icon: ErrorIcon,
+  },
+  [StateEnum.FAILURE_EXPORT_TUBE_AFTER_RECHECK]: {
+    statusText: 'Unable to send tube to Traction',
+    buttonText: 'Export',
+    styles: { button: 'primary', text: 'text-danger', icon: 'red' },
+    icon: ErrorIcon,
+  },
+  [StateEnum.INVALID_PROPS]: {
+    statusText: 'Invalid Traction URL',
+    buttonText: 'Invalid Traction URL',
+    styles: { button: 'danger', text: 'text-danger', icon: 'red' },
+    icon: ErrorIcon,
+  },
+  
 }
+
 export default {
   name: 'PoolXPTubeSubmitPanel',
+  components: {
+    ReadyIcon,
+  },
   props: {
     barcode: {
       type: String,
@@ -78,7 +127,11 @@ export default {
       type: String,
       required: true,
     },
-    tractionUrl: {
+    tractionServiceUrl: {
+      type: String,
+      required: true,
+    },
+    tractionUIUrl: {
       type: String,
       required: true,
     },
@@ -92,53 +145,38 @@ export default {
   },
   computed: {
     statusText() {
-      return StatusTexts[this.state]
-    },
-    buttonStyle() {
-      return (
-        {
-          [StateEnum.INITIAL]: 'primary text-green',
-          [StateEnum.FETCHING_TUBE]: 'outline-primary',
-          [StateEnum.TUBE_FOUND]: 'success',
-          [StateEnum.SUBMITTING]: 'outline-primary',
-          [StateEnum.SUCCESS]: 'success text-green',
-          [StateEnum.FAILURE_FETCH_TUBE]: 'warning',
-          [StateEnum.REFETCHING_TUBE]: 'warning',
-          [StateEnum.FAILURE_ADD_TUBE]: 'warning',
-        }[this.state] || 'danger'
-      )
+      return StateData[this.state].statusText
     },
     buttonText() {
-      return (
-        {
-          [StateEnum.FAILURE_FETCH_TUBE]: 'Retrieve tube from Traction',
-          [StateEnum.REFETCHING_TUBE]: 'Retrieve tube from Traction',
-        }[this.state] || 'Submit Tube to Traction'
-      )
+      return StateData[this.state].buttonText
     },
-    displayStatus() {
-      return this.state !== StateEnum.INITIAL
+    stateStyles() {
+      return StateData[this.state].styles || { button: 'danger', text: 'text-danger' }
     },
+    statusIcon() {
+      return StateData[this.state].icon
+    },
+   
     isButtonDisabled() {
-      return !(
-        this.state === StateEnum.INITIAL ||
-        this.state === StateEnum.FAILURE_ADD_TUBE ||
-        this.state === StateEnum.FAILURE_FETCH_TUBE
+      return (
+        this.state === StateEnum.CHECKING_TUBE_EXISTS ||
+        this.state === StateEnum.POLLING_TUBE ||
+        this.state === StateEnum.INVALID_PROPS 
       )
     },
     displaySpinner() {
-      return (
-        this.state === StateEnum.FETCHING_TUBE ||
-        this.state === StateEnum.SUBMITTING ||
-        this.state === StateEnum.REFETCHING_TUBE
-      )
+      return this.state === StateEnum.POLLING_TUBE
     },
     sequencescapeApiExportUrl() {
       return `${this.sequencescapeApiUrl}/bioscan/export_pool_xp_to_traction`
     },
     tractionCheckTubeUrl() {
-      if (!this.barcode || !this.tractionUrl) return ''
-      return `${this.tractionUrl}/pacbio/tubes?filter[barcode]=${this.testBarcode}`
+      if (!this.barcode || !this.tractionServiceUrl) return ''
+      return `${this.tractionServiceUrl}/pacbio/tubes?filter[barcode]=${this.testBarcode}`
+    },
+    tractionOpenTubeUrl() {
+      if (!this.barcode || !this.tractionUIUrl) return ''
+      return `${this.tractionUIUrl}/pacbio/libraries?filter_value=barcode&filter_input=${this.testBarcode}`
     },
     submitPayload() {
       return {
@@ -153,39 +191,24 @@ export default {
 
   async mounted() {
     this.validateProps()
-    if (this.state !== StateEnum.INITIAL) return
+    if(this.state === StateEnum.INVALID_PROPS) return
+    this.state = StateEnum.CHECKING_TUBE_EXISTS
     const isTubeFound = await this.checkTubeInTraction()
-    if (isTubeFound) {
-      this.state = StateEnum.TUBE_FOUND
-    }
+    this.state = isTubeFound ? StateEnum.TUBE_ALREADY_EXPORTED : StateEnum.INITIAL
   },
   methods: {
     validateProps() {
-      if (!this.barcode) {
-        this.state = StateEnum.INVALID_BARCODE
+      if (!this.barcode || !this.userId || !this.sequencescapeApiUrl || !this.sequencescapeUrl || !this.tractionServiceUrl) {
+        this.state = StateEnum.INVALID_PROPS
         return
       }
-      if (!this.userId) {
-        this.state = StateEnum.INVALID_USER_ID
-        return
-      }
-      if (!this.sequencescapeApiUrl) {
-        this.state = StateEnum.INVALID_SEQUENCESCAPE_URL
-        return
-      }
-      if (!this.sequencescapeUrl) {
-        this.state = StateEnum.INVALID_SEQUENCESCAPE_URL
-        return
-      }
-      if (!this.tractionUrl) {
-        this.state = StateEnum.INVALID_TRACTION_URL
-        return
-      }
+     
     },
     async checkTubeInTraction() {
+      let isTubeFound = false
       try {
         const response = await fetch(this.tractionCheckTubeUrl)
-        let isTubeFound = false
+        
         if (response.ok) {
           const data = await response.json()
           if (data.data && data.data.length > 0) {
@@ -198,17 +221,27 @@ export default {
       }
     },
 
-    handleSubmit() {
-      if (this.state === StateEnum.FAILURE_FETCH_TUBE) {
-        this.state = StateEnum.REFETCHING_TUBE
-        this.testBarcode = 'TRAC-2-50'
-        this.pollTractionForTube()
-      } else {
-        this.submitTube()
+    async handleSubmit() {
+      switch (this.state) {
+        case StateEnum.FAILURE_POLL_TUBE: {
+          this.state = StateEnum.CHECKING_TUBE_EXISTS
+          const isFound = await this.checkTubeInTraction()
+          this.state = isFound ? StateEnum.TUBE_EXPORT_SUCESS : StateEnum.FAILURE_EXPORT_TUBE_AFTER_RECHECK
+          break
+        }
+        case StateEnum.TUBE_EXPORT_SUCESS:
+        case StateEnum.TUBE_ALREADY_EXPORTED: {
+          // Open Traction in a new tab
+          window.open(this.tractionOpenTubeUrl, '_blank')
+          break
+        }
+        default:{
+          await this.submitTube()
+          break
+        }
       }
     },
     async submitTube() {
-      this.state = StateEnum.SUBMITTING
       try {
         const response = await fetch(this.sequencescapeApiExportUrl, {
           method: 'POST',
@@ -219,24 +252,23 @@ export default {
         })
         if (response.ok) {
           this.isSubmitted = true
-          this.state = StateEnum.FETCHING_TUBE
+          this.state = StateEnum.POLLING_TUBE
           this.pollTractionForTube()
         } else {
-          this.state = StateEnum.FAILURE_ADD_TUBE
+          this.state = StateEnum.FAILURE_EXPORT_TUBE
         }
       } catch (error) {
         console.error('Error submitting tube to Traction:', error)
-        this.state = StateEnum.FAILURE_ADD_TUBE
+        this.state = StateEnum.FAILURE_EXPORT_TUBE
       }
     },
 
     async pollTractionForTube() {
-      const maxAttempts = 5
       let attempts = 0
-      const interval = 2000 // 2 seconds
+      
       const poll = async () => {
-        if (attempts > maxAttempts) {
-          this.state = StateEnum.FAILURE_FETCH_TUBE
+        if (attempts > maxPollttempts) {
+          this.state = StateEnum.FAILURE_POLL_TUBE
           return
         }
         // if(attempts ==3) {
@@ -244,10 +276,11 @@ export default {
         // }
         const isTubeFound = await this.checkTubeInTraction()
         if (isTubeFound) {
-          this.state = StateEnum.TUBE_FOUND
+          this.state = StateEnum.TUBE_EXPORT_SUCESS
+          return
         } else {
           attempts++
-          setTimeout(poll, interval)
+          setTimeout(poll, pollInterval)
         }
       }
       poll()
