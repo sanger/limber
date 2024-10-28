4ad139be8c902e2a23b5de47417be463fca8fc08
Seena Nair <55585488+seenanair@users.noreply.github.com>
2024-10-28 15:33:05 +0000

    Update index.js

--- app/frontend/javascript/pool-xp-tube-panel/index.js
+++ app/frontend/javascript/pool-xp-tube-panel/index.js
@@ -10,22 +10,6 @@ import PoolXPTubeSubmitPanel from './components/PoolXPTubeSubmitPanel.vue'
 Vue.use(BootstrapVue)
 Vue.use(BootstrapVueIcons)
 document.addEventListener('DOMContentLoaded', async () => {
-  /*
-   * As we add more components to this page we should
-   * consider switching to proper components and custom tags.
-   * Ran into a problems as I tried to do this at this stage:
-   * 1 - Vue needs to compile the template (ie. our HTML) on the fly
-   #     which means we import a different version of vue above.
-   #     import Vue from 'vue/dist/vue.esm'
-   #     This is slower, and generally recommended against.
-   # 2 - Things didn't appear to be as straight forward as I
-   #     had hoped. I *think* this was because I began wrestling
-   #     vue's expectations with regards to single page applications
-   # 3 - Vue does NOT like our existing templates. The script tags
-   #     seem to upset it.
-   # In general it looks like this is something we should consider
-   # once the majority of our components are vue based.
-   */
   const assetElem = document.getElementById('pool-xp-tube-submit-panel')
   const missingUserIdError = `
     Unfortunately Limber can't find your user id, which is required to add custom metadata.
