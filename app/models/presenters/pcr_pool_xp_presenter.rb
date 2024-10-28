ce86d3c06b43fbfa5ae33efd8b2581f59c8d1288
Seena Nair <55585488+seenanair@users.noreply.github.com>
2024-10-28 10:29:12 +0000

    update(PcrPoolXpPresenter): documentation update

--- app/models/presenters/pcr_pool_xp_presenter.rb
+++ app/models/presenters/pcr_pool_xp_presenter.rb
@@ -1,8 +1,12 @@
 # frozen_string_literal: true
 
 module Presenters
-  # FinalTubePresenter
+  # PcrPoolXpPresenter
+  # This class is responsible for presenting PCR Pool XP tube data.
+  # It inherits from FinalTubePresenter and provides methods
+  # to export data to Traction.
   class PcrPoolXpPresenter < FinalTubePresenter
+    # Enables the export of the PCR Pool XP tube to Traction.
     def export_to_traction
       true
     end
