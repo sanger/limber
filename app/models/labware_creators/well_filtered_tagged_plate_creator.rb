2a94fc334f22b7a654c8837882f451e3d5eb3110
yoldas <ay6@sanger.ac.uk>
2024-10-29 10:03:25 +0000

    Add well filtered tagged plate labware creator

---
+++ app/models/labware_creators/well_filtered_tagged_plate_creator.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module LabwareCreators
+  # Adds well filtering to the TaggedPlate labware creator.
+  class WellFilteredTaggedPlateCreator < TaggedPlate
+    include LabwareCreators::WellFilterBehaviour
+    self.page = 'well_filtered_tagged_plate'
+  end
+end
