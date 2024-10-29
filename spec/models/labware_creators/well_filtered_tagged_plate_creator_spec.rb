0d463f60555678b8f843337d4aeb488a434bfe3d
yoldas <ay6@sanger.ac.uk>
2024-10-29 10:06:26 +0000

    Add spec file placeholder for well filtered tagged plate labware creator

---
+++ spec/models/labware_creators/well_filtered_tagged_plate_creator_spec.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe LabwareCreators::WellFilteredTaggedPlateCreator do
+  context 'when filtering wells' do
+    let(:parent_uuid) { 'example-parent-uuid' }
+    
+  end
+end
