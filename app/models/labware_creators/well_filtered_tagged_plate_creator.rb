79a54cd3503ea2e2c80d18127400e6f4ab8b7757
yoldas <ay6@sanger.ac.uk>
2024-11-06 15:07:42 +0000

    Prevent calling populate_wells_with_pool on V2 plate

--- app/models/labware_creators/well_filtered_tagged_plate_creator.rb
+++ app/models/labware_creators/well_filtered_tagged_plate_creator.rb
@@ -5,5 +5,6 @@ module LabwareCreators
   class WellFilteredTaggedPlateCreator < TaggedPlate
     include LabwareCreators::WellFilterBehaviour
     self.page = 'well_filtered_tagged_plate'
+    self.should_populate_wells_with_pool = false # parent is a V2 plate
   end
 end
