33df21a2a300f7a1874a1d88c26be011576c6a73
Seena Nair <55585488+seenanair@users.noreply.github.com>
2024-11-07 13:03:29 +0000

    update: Removed index files for vue components

--- app/frontend/javascript/tubes-to-rack/index.js
+++ app/frontend/javascript/tubes-to-rack/index.js
@@ -1,39 +1,39 @@
-/* eslint no-console: 0 */
+// /* eslint no-console: 0 */
 
-import Vue from 'vue'
-import BootstrapVue from 'bootstrap-vue'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
-import TubesToRack from './components/TubesToRack.vue'
-import MainContent from '@/javascript/shared/components/MainContent.vue'
-import Page from '@/javascript/shared/components/Page.vue'
-import Sidebar from '@/javascript/shared/components/Sidebar.vue'
-import axios from 'axios'
+// import Vue from 'vue'
+// import BootstrapVue from 'bootstrap-vue'
+// import 'bootstrap/dist/css/bootstrap.css'
+// import 'bootstrap-vue/dist/bootstrap-vue.css'
+// import TubesToRack from './components/TubesToRack.vue'
+// import MainContent from '@/javascript/shared/components/MainContent.vue'
+// import Page from '@/javascript/shared/components/Page.vue'
+// import Sidebar from '@/javascript/shared/components/Sidebar.vue'
+// import axios from 'axios'
 
-Vue.use(BootstrapVue)
+// Vue.use(BootstrapVue)
 
-Vue.component('LbMainContent', MainContent)
-Vue.component('LbPage', Page)
-Vue.component('LbSidebar', Sidebar)
+// Vue.component('LbMainContent', MainContent)
+// Vue.component('LbPage', Page)
+// Vue.component('LbSidebar', Sidebar)
 
-document.addEventListener('DOMContentLoaded', () => {
-  if (document.getElementById('tubes-to-rack-page')) {
-    axios.defaults.headers.common['X-CSRF-Token'] = document
-      .querySelector('meta[name="csrf-token"]')
-      .getAttribute('content')
-    Vue.prototype.$axios = axios
-    new Vue({
-      // Customized render function to pass in properties from our root element
-      // Uses render (h) rather than h => to ensure that `this` is the Vue app.
-      // Essentially allows any data tags to get passed in to matching properties
-      // in App.vue. This lets us mount our app in a variety of contexts, for
-      // example showing events for a particular subject, and toggling various
-      // navigation elements based on appropriateness
-      // h in this case is Vue-shorthand for createElement
-      // https://vuejs.org/v2/guide/render-function.html#createElement-Arguments
-      render(h) {
-        return h(TubesToRack, { props: this.$el.dataset })
-      },
-    }).$mount('#tubes-to-rack-page')
-  }
-})
+// document.addEventListener('DOMContentLoaded', () => {
+//   if (document.getElementById('tubes-to-rack-page')) {
+//     axios.defaults.headers.common['X-CSRF-Token'] = document
+//       .querySelector('meta[name="csrf-token"]')
+//       .getAttribute('content')
+//     Vue.prototype.$axios = axios
+//     new Vue({
+//       // Customized render function to pass in properties from our root element
+//       // Uses render (h) rather than h => to ensure that `this` is the Vue app.
+//       // Essentially allows any data tags to get passed in to matching properties
+//       // in App.vue. This lets us mount our app in a variety of contexts, for
+//       // example showing events for a particular subject, and toggling various
+//       // navigation elements based on appropriateness
+//       // h in this case is Vue-shorthand for createElement
+//       // https://vuejs.org/v2/guide/render-function.html#createElement-Arguments
+//       render(h) {
+//         return h(TubesToRack, { props: this.$el.dataset })
+//       },
+//     }).$mount('#tubes-to-rack-page')
+//   }
+// })
