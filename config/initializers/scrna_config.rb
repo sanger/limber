54a81e859ef41921af09b4e55b724c1cccb59828
Andrew Sparkes <andrewsparkes@users.noreply.github.com>
2024-11-13 09:09:51 +0000

    Update config/initializers/scrna_config.rb
    Co-authored-by: KatyTaylor <kt17@sanger.ac.uk>

--- config/initializers/scrna_config.rb
+++ config/initializers/scrna_config.rb
@@ -24,7 +24,7 @@ Rails.application.config.scrna_config = {
   study_required_number_of_cells_per_sample_in_pool_key: 'scrna_core_pbmc_donor_pooling_required_number_of_cells',
   # Default viability threshold when passing/failing samples (in percent)
   viability_default_threshold: 65,
-  # Default total cell count for samples
+  # Default total cell count threshold when passing/failing samples
   total_cell_count_default_threshold: 50_000,
   # Key for the number of cells per chip well metadata stored on pool wells (in poly_metadata)
   number_of_cells_per_chip_well_key: 'scrna_core_pbmc_donor_pooling_number_of_cells_per_chip_well'
