729e98a7443334c96b1de058d48a4fa1d9ddc784
Stuart McHattie <sm49@sanger.ac.uk>
2024-11-13 17:58:11 +0000

    Create new QcFiles for tubes

--- app/sequencescape/sequencescape/api/v2/qc_file.rb
+++ app/sequencescape/sequencescape/api/v2/qc_file.rb
@@ -1,5 +1,13 @@
 # frozen_string_literal: true
 
+# A QcFile from sequencescape via the V2 API
 class Sequencescape::Api::V2::QcFile < Sequencescape::Api::V2::Base
-  has_one :asset
+  has_one :labware
+
+  # The endpoint requires that the labware relationship is of a Labware type.
+  # Since we create for plates and tubes, not the more generic labware type, we will declare the relationship manually.
+  def self.create_for_labware!(labware:, contents:, filename:)
+    relationships = { labware: { data: { id: labware.id, type: 'labware' } } }
+    create!(contents:, filename:, relationships:)
+  end
 end
