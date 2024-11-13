565facb0a3733d54bbf687ee04223652e7b83148
Andrew Sparkes <as28@sanger.ac.uk>
2024-11-13 09:22:32 +0000

    linted

--- config/initializers/scrna_config.rb
+++ config/initializers/scrna_config.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-# This hash is used to store constants used in pooling and chip loading calculations for samples in the scRNA Core pipeline.
+# Stores constants used in pooling and chip loading calculations for samples in the scRNA Core pipeline.
 Rails.application.config.scrna_config = {
   # Maximum volume to take into the pools plate for each sample (in microlitres)
   maximum_sample_volume: 60.0,
@@ -17,7 +17,7 @@ Rails.application.config.scrna_config = {
   # Desired concentration of cells per microlitre for chip loading
   desired_chip_loading_concentration: 2400,
   # Desired volume in the chip well (in microlitres)
-  desired_sample_volume: 37.5,
+  desired_chip_loading_volume: 37.5,
   # Volume taken for cell counting in microlitres
   volume_taken_for_cell_counting: 10.0,
   # Key for the required number of cells metadata stored on Study (in poly_metadata)
