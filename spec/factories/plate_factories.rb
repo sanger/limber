18346d50987605a2280cac0fea5d5752b7417814
Stuart McHattie <sm49@sanger.ac.uk>
2024-11-20 18:24:52 +0000

    Use Array.new syntax for submission_pools

--- spec/factories/plate_factories.rb
+++ spec/factories/plate_factories.rb
@@ -140,7 +140,7 @@ FactoryBot.define do
       # Set up submission pools on the plate
       submission_pools_count { 0 }
       plates_in_submission { 2 }
-      submission_pools { submission_pools_count.times.map { create :v2_submission_pool, plates_in_submission: } }
+      submission_pools { Array.new(submission_pools_count) { create :v2_submission_pool, plates_in_submission: } }
 
       # Array of pcr_cycles set up for each pool
       pool_pcr_cycles { Array.new(pool_sizes.length, 10) }
