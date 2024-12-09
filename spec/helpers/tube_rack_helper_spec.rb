31f7968f4fca93f5c6190f5d11bbced2567bd530
Stephen Hulme <sh51@sanger.ac.uk>
2024-12-09 17:06:07 +0000

    tests: add tube rack helper spec

---
+++ spec/helpers/tube_rack_helper_spec.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe TubeRackHelper do
+  include TubeRackHelper
+
+  describe '::racked_tube_tooltip' do
+    let(:tube) { build(:v2_tube, name: 'tube-name', labware_barcode: 'tube-barcode') }
+    let(:location) { 'A1' }
+
+    it 'returns the location and tube name and barcode' do
+      expect(racked_tube_tooltip(tube, location)).to eq("A1: tube-name&#010;#{tube.labware_barcode.human}")
+    end
+
+    it 'returns the location if tube is nil' do
+      expect(racked_tube_tooltip(nil, location)).to eq(location)
+    end
+  end
+end
