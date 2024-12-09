c21613eacbbc5d04152bd2c84d37d2cb5ea44b65
Shiv <44001656+SHIV5T3R@users.noreply.github.com>
2024-12-09 17:04:07 +0000

    Added test for duplicate donor IDs

--- spec/models/labware_creators/donor_pooling_plate_spec.rb
+++ spec/models/labware_creators/donor_pooling_plate_spec.rb
@@ -577,6 +577,41 @@ RSpec.describe LabwareCreators::DonorPoolingPlate do
       end
     end
 
+    context 'when the test run has wells with one duplicate donor ID' do
+      let(:study) { create(:v2_study) }
+      let(:project) { create(:v2_project) }
+      let(:donor_ids) { (1..80).to_a }
+      let(:wells) { parent_1_plate.wells[0..79] }
+      let(:expected_number_of_pools) { 4 }
+      let(:number_of_pools) { 4 }
+
+      before do
+        wells.each_with_index do |well, index|
+          well.state = 'passed'
+          well.aliquots.first.study = study
+          well.aliquots.first.project = project
+          well.aliquots.first.request = requests[index]
+          well.aliquots.first.sample.sample_metadata.donor_id = donor_ids[index]
+          well.aliquots.first.request.request_metadata.number_of_pools = number_of_pools
+        end
+        wells[10].aliquots.first.sample.sample_metadata.donor_id = 1
+      end
+
+      it 'returns correct number of pools' do
+        pools = subject.build_pools
+        expect(pools.size).to eq(expected_number_of_pools)
+        expect(pools.flatten).to match_array(wells)
+      end
+
+      it 'returns pools with correct number of donors' do
+        pools = subject.build_pools
+        pools.each do |pool|
+          number_of_unique_donor_ids = pool.map { |well| well.aliquots.first.sample.sample_metadata.donor_id }.uniq.size
+          expect(number_of_unique_donor_ids).to eq(pool.size)
+        end
+      end
+    end
+
     context 'when test run for 24 samples per pool and 8 pools' do
       let(:study) { create(:v2_study) }
       let(:project) { create(:v2_project) }
