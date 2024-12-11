cb356516e545652ae3b3013f33426b7751b52a07
Stuart McHattie <sm49@sanger.ac.uk>
2024-12-11 17:32:03 +0000

    Change the method used to create Submissions over API v2

--- app/models/sequencescape_submission.rb
+++ app/models/sequencescape_submission.rb
@@ -125,15 +125,18 @@ class SequencescapeSubmission
   # rubocop:disable Metrics/AbcSize
   def generate_submissions
     orders = generate_orders
-    submission = api.submission.create!(orders: orders.map(&:uuid), user: user)
-    @submission_uuid = submission.uuid
-    submission.submit!
+    @submission_uuid =
+      Sequencescape::Api::V2::Submission.create!(
+        and_submit: true,
+        order_uuids: orders.map(&:uuid),
+        user_uuid: user
+      ).uuid
     true
-  rescue Sequencescape::Api::ConnectionFactory::Actions::ServerError => e
-    errors.add(:sequencescape_connection, /.+\[([^\]]+)\]/.match(e.message)[1])
+  rescue JsonApiClient::Errors::ConnectionError => e
+    errors.add(:sequencescape_connection, e.message)
     false
-  rescue Sequencescape::Api::ResourceInvalid => e
-    errors.add(:submission, e.resource.errors.full_messages.join('; '))
+  rescue JsonApiClient::Errors::RecordNotSaved => e
+    errors.add(:submission, e.record.errors.full_messages.join('; '))
     false
   end
 
