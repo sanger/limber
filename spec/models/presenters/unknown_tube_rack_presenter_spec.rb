1b885a9f2ed6350b7cf1e524cfdc5269c513e03d
Andrew Sparkes <as28@sanger.ac.uk>
2024-12-17 17:02:14 +0000

    removed inappropriate tests in unknown tube rack presenter

--- spec/models/presenters/unknown_tube_rack_presenter_spec.rb
+++ spec/models/presenters/unknown_tube_rack_presenter_spec.rb
@@ -9,25 +9,5 @@ RSpec.describe Presenters::UnknownTubeRackPresenter do
     expect { |b| subject.default_state_change(&b) }.not_to yield_control
   end
 
-  it 'prevents child creation' do
-    expect { |b| subject.control_additional_creation(&b) }.not_to yield_control
-  end
-
-  it 'prevents well failure' do
-    expect(subject.well_failing_applicable?).to eq false
-  end
-
-  context 'with a well request' do
-    before { stub_api_get(labware.uuid, 'wells', body: json(:well_collection)) }
-
-    it { is_expected.not_to be_valid }
-
-    it 'warns the user' do
-      subject.valid?
-      expect(subject.errors.full_messages).to include(
-        "Tube rack type 'Other tube rack' is not a limber tube rack. " \
-          'Perhaps you are using the wrong pipeline application?'
-      )
-    end
-  end
+  # TODO: unsure what additional tests are needed here
 end
