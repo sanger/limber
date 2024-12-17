43dc23f84c36debc160d97b2056d3cb34c4308d5
Dasun Pubudumal <pubudumald@gmail.com>
2024-12-17 15:19:53 +0530

    Adding docs

--- app/models/print_job.rb
+++ app/models/print_job.rb
@@ -109,30 +109,32 @@ class PrintJob # rubocop:todo Style/Documentation
 
     # TODO: DPL-865 [Limber] Handle sprint client response
     #
-    # This print_to_sprint call fails silently if there is an error. Instead
-    # of returning success, the result of the send_print_request call should
-    # be used to check the response status and response body. Examples:
-    #
-    # The following response body is returned in a 200 response.
-    # {"errors":[{"message":"Variable 'printRequest' has an invalid value: Expected
-    #   type 'Int' but was
-    #   'Double'.","locations":[{"line":1,"column":16}],"extensions":{"classification":"
-    #   ValidationError"}}]}
-    #
-    # The following response body is returned in a 500 response.
-    # {"timestamp": "2023-08-02T14:46:30.160+00:00","status": 500,"error": "Internal
-    #   Server Error","path": "/graphql"}
-    #
-    # When sprint cannot log in to printer, the details are available in response body.
-    #
-    # A successful response has a job id in response body.
-    #
-    # Use errors.add to show proper feedback in the view.
+
   end
 
   # Handles the response from the SPrintClient and checks for success.
   # If the response is successful and contains a job ID, it returns true.
   # Otherwise, it adds an error message to the errors object and returns false.
+  #
+  # The print_to_sprint call fails silently if there is an error. Instead
+  # of returning success, the result of the send_print_request call should
+  # be used to check the response status and response body. Examples:
+  #
+  # The following response body is returned in a 200 response.
+  # {"errors":[{"message":"Variable 'printRequest' has an invalid value: Expected
+  #   type 'Int' but was
+  #   'Double'.","locations":[{"line":1,"column":16}],"extensions":{"classification":"
+  #   ValidationError"}}]}
+  #
+  # The following response body is returned in a 500 response.
+  # {"timestamp": "2023-08-02T14:46:30.160+00:00","status": 500,"error": "Internal
+  #   Server Error","path": "/graphql"}
+  #
+  # When sprint cannot log in to printer, the details are available in response body.
+  #
+  # A successful response has a job id in response body.
+  #
+  # Use errors.add to show proper feedback in the view.
   #
   # @param response [Net::HTTPResponse] The response object from the SPrintClient.
   # @return [Boolean] True if the response is successful and contains a job ID, false otherwise.
