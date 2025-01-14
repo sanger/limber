95b49eec59b1c255c7b5147ab57e9c3220313a2d
Stuart McHattie <sm49@sanger.ac.uk>
2025-01-14 11:27:21 +0000

    Use constant for PER_PAGE

--- app/models/ongoing_plate.rb
+++ app/models/ongoing_plate.rb
@@ -15,7 +15,7 @@ class OngoingPlate < AssetSearchForm
   def pagination
     return {} if page.nil? # No pagination
 
-    { number: page, size: 30 }
+    { number: page, size: PER_PAGE }
   end
 
   def default_purposes
