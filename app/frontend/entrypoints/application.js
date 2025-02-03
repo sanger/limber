af2b79e96e28420d0189574abff1b25aef823793
Andrew Sparkes <as28@sanger.ac.uk>
2025-02-03 13:00:41 +0000

    reverted to UJS

--- app/frontend/entrypoints/application.js
+++ app/frontend/entrypoints/application.js
@@ -29,11 +29,11 @@ console.log('Visit the guide for more information: ', 'https://vite-ruby.netlify
 
 // ^^^ Template from Vite. Below is custom code for Limber ^^^
 
-// Importing Turbo as replacement for Rails UJS in Rails 7.2 and later
-import * as Turbo from '@hotwired/turbo'
+// Import Rails UJS as in Sequencescape
+import Rails from '@rails/ujs'
 
 try {
-  Turbo.start()
+  Rails.start()
 } catch {
   // Nothing
 }
