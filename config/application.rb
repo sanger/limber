bcf173bfa47a83bf76aa6166b4e159163df4c76b
Andrew Sparkes <as28@sanger.ac.uk>
2025-02-06 11:42:28 +0000

    changes after applying rails update - wip

--- config/application.rb
+++ config/application.rb
@@ -1,41 +1,49 @@
 # frozen_string_literal: true
 
-require_relative 'boot'
+require_relative "boot"
 
-require 'rails'
-# Only load what we need:
-# http://guides.rubyonrails.org/initialization.html#railties-lib-rails-all-rb
-[
-  # 'active_record/railtie',
-  'action_controller/railtie',
-  'action_view/railtie',
-  'action_mailer/railtie',
-  'active_job/railtie',
-  'action_cable/engine',
-  'rails/test_unit/railtie'
-].each do |railtie|
-  require railtie.to_s
-rescue LoadError
-  # Do nothing. We're mimicking rails/all here, so
-  # use the same behaviour.
-end
+require "rails"
+
+# Pick the frameworks you want:
+require "active_model/railtie"
+require "active_job/railtie"
+# require "active_record/railtie"
+# require "active_storage/engine"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+# require "action_mailbox/engine"
+# require "action_text/engine"
+require "action_view/railtie"
+require "action_cable/engine"
+require "rails/test_unit/railtie"
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module Limber
-  class Application < Rails::Application # rubocop:todo Style/Documentation
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
     config.active_support.cache_format_version = 7.0
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.label_templates = config_for(:label_templates)
-    config.disable_animations = false
 
+    # Please, add to the `ignore` list any other `lib` subdirectories that do
+    # not contain `.rb` files, or that should not be reloaded or eager loaded.
+    # Common ones are `templates`, `generators`, or `middleware`, for example.
+    config.autoload_lib(ignore: %w[assets tasks])
+
+    # Printer templates
+    config.label_templates = config_for(:label_templates)
+
+    # Logging
     config.logger = Logger.new(Rails.root.join('log', "#{Rails.env}.log"), 5, 10 * 1024 * 1024)
     config.logger.formatter = ::Logger::Formatter.new
+
+    # Bioscan mBrave tag layout templates
     config.mbrave = config_for(:mbrave)
   end
 end
