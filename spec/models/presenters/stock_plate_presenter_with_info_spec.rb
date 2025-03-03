040e7852f30d358ab15e61f7b214225f24c5360f
Seena Nair <55585488+seenanair@users.noreply.github.com>
2025-03-03 17:12:31 +0000

    update tests

--- spec/models/presenters/stock_plate_presenter_with_info_spec.rb
+++ spec/models/presenters/stock_plate_presenter_with_info_spec.rb
@@ -4,6 +4,20 @@ require 'spec_helper'
 require_relative 'shared_labware_presenter_examples'
 
 RSpec.describe Presenters::StockPlatePresenterWithInfo do
+  let(:labware) do
+    build :v2_stock_plate,
+          receptacle: receptacle,
+          purpose: purpose,
+          purpose_name: purpose_name,
+          state: state,
+          barcode_number: 6,
+          created_at: '2016-10-19 12:00:00 +0100'
+  end
+
+  let!(:purpose_config) { create(:stock_plate_with_info_config, uuid: 'stock-plate-purpose-uuid') }
+  let(:purpose) { create :v2_purpose, name: purpose_name, uuid: purpose_uuid }
+  let(:purpose_name) { 'Limber example purpose' }
+  let(:purpose_uuid) { 'example-purpose-uuid' }
   let(:labware) { create :v2_stock_plate }
 
   subject { Presenters::StockPlatePresenterWithInfo.new(labware:) }
@@ -13,9 +27,6 @@ RSpec.describe Presenters::StockPlatePresenterWithInfo do
   it_behaves_like 'a stock presenter'
 
   it 'initializes with informational messages' do
-    expected_messages = [
-      "Please ensure you use the CITE-seq-compatible primer when working with 'LRC GEM-X 5p GEMs Input CITE' plates"
-    ]
-    expect(subject.info_messages).to match_array(expected_messages)
+    expect(subject.info_messages).to match_array(['Test message'])
   end
 end
