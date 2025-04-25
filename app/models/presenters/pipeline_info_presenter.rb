5ec19ca146880d1ad30ef9714a34c3f6b40e921c
Stephen Hulme <sh51@sanger.ac.uk>
2025-04-25 13:44:34 +0100

    fix: improve formatting of pipeline names

--- app/models/presenters/pipeline_info_presenter.rb
+++ app/models/presenters/pipeline_info_presenter.rb
@@ -20,12 +20,15 @@ class Presenters::PipelineInfoPresenter
   end
 
   def pipeline_names
-    Settings
-      .pipelines
-      .select_pipelines_with_purpose(Settings.pipelines.list, @labware.purpose)
-      .map(&:pipeline_group)
-      .uniq
-      .join(', ')
+    if pipeline_groups_by_filter.empty?
+      # if there are no active pipelines, return the pipeline groups by purpose
+      return join_up_to(3, pipeline_groups_by_purpose.sort)
+    end
+    if pipeline_groups_by_purpose.intersect?(pipeline_groups_by_filter)
+      # combine the two arrays to find the common pipeline groups
+      return join_up_to(3, (pipeline_groups_by_purpose & pipeline_groups_by_filter).sort)
+    end
+    'No Pipelines Found'
   end
 
   # Returns true if the labware purpose has any defined parent of grand-parent relationships, false otherwise.
@@ -69,6 +72,24 @@ class Presenters::PipelineInfoPresenter
   end
 
   private
+
+  def pipeline_groups_by_purpose
+    Settings
+      .pipelines
+      .select_pipelines_with_purpose(Settings.pipelines.list, @labware.purpose)
+      .map(&:pipeline_group)
+      .uniq
+  end
+
+  def join_up_to(max_listed, array, separator = ', ')
+    return array.join(separator) if array.size <= max_listed
+
+    "#{array[0..max_listed - 1].join(separator)}, ...(#{array.size - max_listed} more)"
+  end
+
+  def pipeline_groups_by_filter
+    Settings.pipelines.active_pipelines_for(@labware).map(&:pipeline_group).uniq
+  end
 
   def find_plate(barcode)
     Sequencescape::Api::V2::Plate.find_all({ barcode: [barcode] }).first
