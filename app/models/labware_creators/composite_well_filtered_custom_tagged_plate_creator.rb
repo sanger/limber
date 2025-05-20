4027fdfac262642d0faf35521b55a785d98ef00b
Seena Nair <55585488+seenanair@users.noreply.github.com>
2025-05-20 13:32:07 +0100

    Refactor documentation in CompositeWellFilteredCustomTaggedPlateCreator to clarify well filtering capabilities and update references to WellFilterComposite

--- app/models/labware_creators/composite_well_filtered_custom_tagged_plate_creator.rb
+++ app/models/labware_creators/composite_well_filtered_custom_tagged_plate_creator.rb
@@ -2,29 +2,32 @@
 
 module LabwareCreators
   # This class extends the functionality of the `CustomTaggedPlate` labware creator
-  # by adding well filtering capabilities. It allows partial well filtering
-  # using the `WellFilterAllowingPartials` class.
+  # by adding well filtering capabilities using the `WellFilterComposite` class.
   #
   # ## Key Features:
-  # - Associates a `WellFilter` instance of type WellFilterAllowingPartials with the labware creator.
+  # - Associates a `WellFilter` instance of type WellFilterComposite with the labware creator.
   # ## Use Case:
   # This class is particularly useful in scenarios where:
   # - A single well contains multiple requests of different types.
-  # - Some requests are in a 'closed' state, while others are 'pending' (result of partial submissions).
-  # - Only a subset of wells needs to be processed or filtered based on specific criteria.
+  #   - Some requests are in a 'closed' state, while others are 'pending' (result of partial submissions).
+  #   - Only a subset of wells needs to be processed or filtered based on specific criteria.
+  # - A single well contains single request
   #
-  # By applying partial well filtering, this class ensures that only the relevant requests in
-  # well are processed and thereby creating transfer requests only for them, improving accuracy in workflows.
+  # By applying composite well filtering, this class ensures that only the relevant requests in
+  # well are processed and thereby creating transfer requests only for them.
   #
-  # ## Example:
+  # ## Example 1:
   # Suppose a well contains two requests:
   # - Request 1: Type 'request_type_a', State 'pending'
   # - Request 2: Type 'request_type_b', State 'closed'
-  #
   # Using this class, the well filter will allow processing of Request 1 while ignoring Request 2.
-
+  #
+  ## Example 2:
+  # Suppose a well contains one request:
+  # - Request 1: Type 'request_type_a', State 'pending'
+  #  Using this class, the well filter will allow processing of Request 1.
   # @see LabwareCreators::CustomTaggedPlate
-  # @see LabwareCreators::WellFilterAllowingPartials
+  # @see LabwareCreators::WellFilterComposite
   class CompositeWellFilteredCustomTaggedPlateCreator < CustomTaggedPlate
     include LabwareCreators::WellFilterBehaviour
     self.page = 'custom_tagged_plate'
