788c16e7e405b1965a0c3619be758b921c91dbae
Dasun Pubudumal <pubudumald@gmail.com>
2025-06-02 11:48:49 +0100

    Adding tests for tubes_from_plate_well_spec.rb

--- spec/models/labware_creators/tubes_from_plate_well_spec.rb
+++ spec/models/labware_creators/tubes_from_plate_well_spec.rb
@@ -7,6 +7,9 @@ require_relative 'shared_examples'
 RSpec.describe LabwareCreators::TubesFromPlateWell do
   has_a_working_api
 
+  let(:form_attributes) { { purpose_uuid: child_purpose_uuid, parent_uuid: parent_uuid, user_uuid: user_uuid } }
+  let(:tubes_from_plate_attributes) { [{ child_purpose_uuid:, parent_uuid:, user_uuid: }] }
+
   context 'for pre creation' do
     describe '#support_parent?' do
       subject { described_class.support_parent?(parent) }
@@ -17,5 +20,21 @@ RSpec.describe LabwareCreators::TubesFromPlateWell do
         it { is_expected.to be true }
       end
     end
+  end
+
+  context 'for creation' do
+    subject { described_class.new(api, form_attributes) }
+
+    let(:controller) { TubeCreationController.new }
+    let(:child_purpose_uuid) { SecureRandom.uuid }
+    let(:parent_uuid) { SecureRandom.uuid }
+    let(:user_uuid) { SecureRandom.uuid }
+    let(:child_tubes) { create_list(:tube, 2) }
+
+    let(:transfer_attributes) do
+      [{ arguments: { user_uuid: user_uuid, source_uuid: parent_uuid, destination_uuid: child_tube.uuid } }]
+    end
+
+    it_behaves_like 'it has no custom page'
   end
 end
