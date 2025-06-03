da4aa99132f855045aba4faff702eabfd974ec71
Dasun Pubudumal <pubudumald@gmail.com>
2025-06-03 16:30:35 +0100

    Refactoring kinnex well filter

--- app/models/labware_creators/well_filter_kinnex.rb
+++ app/models/labware_creators/well_filter_kinnex.rb
@@ -2,8 +2,6 @@
 
 # This class is used to create a well filter for Kinnex, allowing partial transfers.
 class LabwareCreators::WellFilterKinnex < LabwareCreators::WellFilterAllowingPartials
-  REQUEST_TYPE = %w[kinnex_prep].freeze
-
   # Returns an array of wells along with their filtered requests.
   # This method iterates through the wells and applies filtering logic to determine
   # which wells and their associated requests should be included in the result.
@@ -26,23 +24,6 @@ class LabwareCreators::WellFilterKinnex < LabwareCreators::WellFilterAllowingPar
         end
   end
 
-  def filter_requests(requests, _well)
-    filter_by_request_type(requests)
-  end
-
-  def filter_by_request_type(requests)
-    requests.select { |r| REQUEST_TYPE.include?(r.request_type.key) }
-  end
-
-  # Returns the wells from the parent plate that have requests of the specified type.
-  # This method filters the wells of the parent plate to include only those wells
-  # that have at least one request matching the types defined in `REQUEST_TYPE`.
-  #
-  # @return [Array<Sequencescape::Api::V2::Well>] An array of wells with matching requests.
-  def well_locations
-    wells.select { |well| well.requests_as_source.any? { |request| REQUEST_TYPE.include?(request.request_type.key) } }
-  end
-
   private
 
   def valid_well?(well)
@@ -50,7 +31,6 @@ class LabwareCreators::WellFilterKinnex < LabwareCreators::WellFilterAllowingPar
   end
 
   def filtered_requests_for_well(well)
-    requests = filter_requests(well.active_requests&.uniq, well)
-    filter_by_state(requests)
+    filter_by_state(filter_by_request_type(well.active_requests&.uniq))
   end
 end
