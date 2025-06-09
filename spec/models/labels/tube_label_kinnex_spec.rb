820e1d10947d034f6a76599e5f5318494c5ef82b
Dasun Pubudumal <pubudumald@gmail.com>
2025-06-09 18:39:06 +0100

    Adding tests

---
+++ spec/models/labels/tube_label_kinnex_spec.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Labels::TubeLabelKinnex do
+  it { expect(described_class).to be < Labels::Base }
+
+  context 'when creating the label of a plate' do
+    let(:labware) { create(:v2_tube, name: 'ABCD:A1') }
+    let(:label) { described_class.new(labware) }
+
+    before do
+      allow(labware).to receive(:transfer_requests_as_target).and_return(
+        [create(:v2_transfer_request, source_asset: labware)]
+      )
+    end
+
+    describe '#attributes' do
+      let(:attributes) { label.attributes }
+
+      it 'has the correct top_left attribute' do
+        expect(attributes[:top_left]).to eq Time.zone.today.strftime('%e-%^b-%Y')
+      end
+
+      it 'has the correct bottom_left attribute' do
+        expect(attributes[:bottom_left]).to eq labware.barcode.human
+      end
+
+      it 'has the correct top_right attribute' do
+        expect(attributes[:top_right]).to eq 'ABCD:A1'
+      end
+
+      it 'has the correct bottom_right attribute' do
+        expect(attributes[:bottom_right]).to eq 'WGS example-purpose'
+      end
+
+      it 'has the correct barcode attribute' do
+        expect(attributes[:barcode]).to eq labware.barcode.human
+      end
+    end
+  end
+end
