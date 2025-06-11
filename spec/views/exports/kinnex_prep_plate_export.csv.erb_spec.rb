ddb7b3509457d572e8b90a84b4ff6733b0a33391
Seena Nair <55585488+seenanair@users.noreply.github.com>
2025-06-11 11:47:00 +0100

    tests(kinnex_prep_plate_export_spec): Update source wells and destination tubes for Kinnex Prep export

--- spec/views/exports/kinnex_prep_plate_export.csv.erb_spec.rb
+++ spec/views/exports/kinnex_prep_plate_export.csv.erb_spec.rb
@@ -24,7 +24,8 @@ RSpec.describe 'exports/kinnex_prep_plate_export.csv.erb' do
 
     # source wells
     let(:source_well_a1) do
-      create(:v2_well, location: 'A1', aliquots: [source_aliquot1_s1], downstream_tubes: [dest_tube1])
+      # Multiple downstream tubes as in Kinnex Prep
+      create(:v2_well, location: 'A1', aliquots: [source_aliquot1_s1], downstream_tubes: [dest_tube1, dest_tube7])
     end
     let(:source_well_a2) do
       create(:v2_well, location: 'A2', aliquots: [source_aliquot2_s1], downstream_tubes: [dest_tube2])
@@ -59,6 +60,7 @@ RSpec.describe 'exports/kinnex_prep_plate_export.csv.erb' do
     let(:dest_aliquot4) { create(:v2_aliquot, sample: sample2) }
     let(:dest_aliquot5) { create(:v2_aliquot, sample: sample2) }
     let(:dest_aliquot6) { create(:v2_aliquot, sample: sample2) }
+    let(:dest_aliquot7) { create(:v2_aliquot, sample: sample2) }
 
     # destination tube uuids
     let(:dest_tube1_uuid) { SecureRandom.uuid }
@@ -67,6 +69,7 @@ RSpec.describe 'exports/kinnex_prep_plate_export.csv.erb' do
     let(:dest_tube4_uuid) { SecureRandom.uuid }
     let(:dest_tube5_uuid) { SecureRandom.uuid }
     let(:dest_tube6_uuid) { SecureRandom.uuid }
+    let(:dest_tube7_uuid) { SecureRandom.uuid }
 
     # destination tubes
     let(:dest_tube1) { create(:v2_tube, uuid: dest_tube1_uuid, aliquots: [dest_aliquot1], name: 'SEQ:NT1O:A1') }
@@ -75,12 +78,14 @@ RSpec.describe 'exports/kinnex_prep_plate_export.csv.erb' do
     let(:dest_tube4) { create(:v2_tube, uuid: dest_tube4_uuid, aliquots: [dest_aliquot4], name: 'SEQ:NT2P:B1') }
     let(:dest_tube5) { create(:v2_tube, uuid: dest_tube5_uuid, aliquots: [dest_aliquot5], name: 'SPR:NT2P:C1') }
     let(:dest_tube6) { create(:v2_tube, uuid: dest_tube6_uuid, aliquots: [dest_aliquot6], name: 'SPR:NT2P:D1') }
+    let(:dest_tube7) { create(:v2_tube, uuid: dest_tube7_uuid, aliquots: [dest_aliquot7], name: 'SPR:NT2P:E1') }
 
     # expected file content
     let(:expected_content) do
       [
         ['Source plate barcode', 'Source well position', 'Destination tube barcode'],
         ['DN3U', 'A1', dest_tube1.barcode.human],
+        ['DN3U', 'A1', dest_tube7.barcode.human],
         ['DN3U', 'B1', dest_tube4.barcode.human],
         ['DN3U', 'A2', dest_tube2.barcode.human],
         ['DN3U', 'B2', dest_tube5.barcode.human],
