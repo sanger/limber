fe636f09b0a3e99546c41aaca3057de561f33afa
Stephen Hulme <sh51@sanger.ac.uk>
2025-06-26 15:28:29 +0100

    style: lint Layout/EmptyLineAfterMagicComment

--- app/models/presenters/chromium_chip_presenter.rb
+++ app/models/presenters/chromium_chip_presenter.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 module Presenters
   # Presenter for the Chromium Chip plate to disable the existing pooling tab
   # that assumes a different pooling strategy and a standard plate layout.
--- spec/models/presenters/stock_plate_presenter_with_info_spec.rb
+++ spec/models/presenters/stock_plate_presenter_with_info_spec.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
-#
+
 require 'spec_helper'
 require_relative 'shared_labware_presenter_examples'
 
