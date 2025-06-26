3ad01a8c9426fa9f033c946e548488eb0c5423c0
Stephen Hulme <sh51@sanger.ac.uk>
2025-06-26 15:45:20 +0100

    style: lint Layout/MultilineMethodCallIndentation

--- app/models/presenters/plate_presenter.rb
+++ app/models/presenters/plate_presenter.rb
@@ -112,15 +112,15 @@ module Presenters
           .fetch(:file_links, [])
           .select { |link| can_be_enabled?(link&.states) }
           .map do |link|
+          [
+            link.name,
             [
-              link.name,
-              [
-                :limber_plate,
-                :export,
-                { id: link.id, limber_plate_id: human_barcode, format: :csv, **link.params || {} }
-              ]
+              :limber_plate,
+              :export,
+              { id: link.id, limber_plate_id: human_barcode, format: :csv, **link.params || {} }
             ]
-          end
+          ]
+        end
       links << ['Download Worksheet CSV', { format: :csv }] if csv.present?
       links
     end
