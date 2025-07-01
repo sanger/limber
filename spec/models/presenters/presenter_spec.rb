56b038abd50ca13b59e1990b740074d6c7623d09
Stephen Hulme <sh51@sanger.ac.uk>
2025-07-01 11:44:33 +0100

    test: add test

--- spec/models/presenters/presenter_spec.rb
+++ spec/models/presenters/presenter_spec.rb
@@ -8,6 +8,32 @@ RSpec.describe Presenters::Presenter, type: :model do
 
   let(:presenter) { dummy_class.new }
 
+  describe '#parent_labwares' do
+    context 'when there are no parents' do
+      before { allow(presenter.labware).to receive(:parents).and_return([]) }
+
+      it 'returns an empty array' do
+        expect(presenter.parent_labwares).to eq([])
+      end
+    end
+
+    context 'when there are parents' do
+      let(:parent_labware) { create(:labware) }
+
+      before do
+        allow(presenter.labware).to receive(:parents).and_return([parent_labware])
+        allow(Sequencescape::Api::V2::Labware).to receive(:find_all).with(
+          { uuid: [parent_labware.uuid] },
+          includes: %w[purpose]
+        ).and_return([parent_labware])
+      end
+
+      it 'returns the parent labwares with purposes' do
+        expect(presenter.parent_labwares).to eq([parent_labware])
+      end
+    end
+  end
+
   describe '#show_pooling_tab?' do
     context 'when pooling_tab in the presenter has a value' do
       before { presenter.pooling_tab = 'some_value' }
