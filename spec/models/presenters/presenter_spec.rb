58efe570683486073dab1e32f873b80384eca088
Stephen Hulme <sh51@sanger.ac.uk>
2025-07-01 14:06:27 +0100

    test: add test for children_labwares

--- spec/models/presenters/presenter_spec.rb
+++ spec/models/presenters/presenter_spec.rb
@@ -35,6 +35,33 @@ RSpec.describe Presenters::Presenter, type: :model do
     end
   end
 
+  describe '#children_labwares' do
+    context 'when there are no children' do
+      before { allow(presenter.labware).to receive(:children).and_return([]) }
+
+      it 'returns an empty array' do
+        expect(presenter.children_labwares).to eq([])
+      end
+    end
+
+    context 'when there are children' do
+      let(:child_labware1) { create(:labware) }
+      let(:child_labware2) { create(:labware) }
+
+      before do
+        allow(presenter.labware).to receive(:children).and_return([child_labware1, child_labware2])
+        allow(Sequencescape::Api::V2::Labware).to receive(:find_all).with(
+          { uuid: [child_labware1.uuid, child_labware2.uuid] },
+          includes: %w[purpose]
+        ).and_return([child_labware1, child_labware2])
+      end
+
+      it 'returns the child labwares' do
+        expect(presenter.children_labwares).to eq([child_labware1, child_labware2])
+      end
+    end
+  end
+
   describe '#show_pooling_tab?' do
     context 'when pooling_tab in the presenter has a value' do
       before { presenter.pooling_tab = 'some_value' }
