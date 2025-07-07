eb50f3804b96c8d9a89163fc3119776c096bfac4
Stephen Hulme <sh51@sanger.ac.uk>
2025-07-07 17:56:47 +0100

    fix: limit scope of tag layout styling to remove ✓ in unrelated inputs

--- app/frontend/javascript/custom-tagged-plate/components/TagLayoutManipulations.vue
+++ app/frontend/javascript/custom-tagged-plate/components/TagLayoutManipulations.vue
@@ -290,7 +290,7 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 input:invalid + span:after {
   content: '✖';
   padding-left: 5px;
--- app/frontend/javascript/custom-tagged-plate/components/TagLayoutManipulationsMultiple.vue
+++ app/frontend/javascript/custom-tagged-plate/components/TagLayoutManipulationsMultiple.vue
@@ -104,7 +104,7 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 input:invalid + span:after {
   content: '✖';
   padding-left: 5px;
