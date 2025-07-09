fd91d086f2abcf87ba46b06bada9cd99827859c7
Stephen Hulme <sh51@sanger.ac.uk>
2025-07-09 12:23:13 +0100

    test: repair tag template specs

--- spec/factories/tag_layout_template_factories.rb
+++ spec/factories/tag_layout_template_factories.rb
@@ -21,6 +21,36 @@ FactoryBot.define do
       tag_layout_template._cached_relationship(:tag2_group) { evaluator.tag2_group } if evaluator.tag2_group
     end
 
+    factory :tag_layout_template_by_row do
+      direction { 'row' }
+    end
+
+    factory :tag_layout_template_by_inverse_column do
+      direction { 'inverse_column' }
+    end
+
+    factory :tag_layout_template_by_inverse_row do
+      direction { 'inverse_row' }
+    end
+
+    factory :tag_layout_template_by_quadrant do
+      walking_by { 'quadrants' }
+      direction { 'column then row' }
+
+      factory :tag_layout_template_by_quadrant_in_columns do
+        direction { 'column' }
+      end
+
+      factory :tag_layout_template_by_quadrant_in_columns_then_columns do
+        direction { 'column then column' }
+      end
+    end
+
+    factory :tag_layout_template_combinatorial_by_row do
+      walking_by { 'combinatorial sequential' }
+      direction { 'combinatorial by row' }
+    end
+
     factory :v2_dual_index_tag_layout_template do
       transient { tag2_group { create :v2_tag_group_with_tags } }
     end
