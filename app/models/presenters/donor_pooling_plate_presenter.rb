824fd07937e4269660adae6c95c96c62c4c573dd
Shiv <44001656+SHIV5T3R@users.noreply.github.com>
2025-07-23 12:05:17 +0100

    feat: enhance DonorPoolingPlatePresenter with methods for grouping and sample information

--- app/models/presenters/donor_pooling_plate_presenter.rb
+++ app/models/presenters/donor_pooling_plate_presenter.rb
@@ -7,8 +7,32 @@ module Presenters
   class DonorPoolingPlatePresenter < StandardPresenter
     # The pooling tab is not relevant for this presenter as the wells are already pooled (tab shows future pooling
     # by submission strategy)
+    #
     self.pooling_tab = ''
 
+    def study_project_groups_from_wells
+      groups = Hash.new { |h, k| h[k] = [] }
+
+      wells.compact
+        .reject { |well| well.aliquots.empty? }
+        .each do |well|
+        key = "#{well.aliquots.first.study.name} / #{well.aliquots.first.project.name}"
+        groups[key] << well # Store the well itself
+      end
+
+      groups.to_a
+    end
+
+    def num_samples_per_pool(group)
+      group.map { |well| well&.aliquots&.size }.join(', ')
+    end
+
+    def get_source_wells(group)
+      binding.pry
+      group.map { |well| well.position['name'] || 'Unknown' }.join(', ')
+    end
+
+    self.page = 'show_pooling_info'
     # Override the samples tab to display additional sample information for the pooled wells
     self.samples_partial = 'plates/pooled_wells_samples_tab'
   end
