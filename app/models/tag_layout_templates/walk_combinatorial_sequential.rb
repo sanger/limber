5c7dc89bcfeec4bc70a529472144e85ae6857e95
Stephen Hulme <sh51@sanger.ac.uk>
2025-07-28 15:47:29 +0100

    wip: re-assign module to walkers

--- app/models/tag_layout_templates/walk_combinatorial_sequential.rb
+++ app/models/tag_layout_templates/walk_combinatorial_sequential.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 # Handle Sequencescape "combinatorial sequential" tag layout template walker
-module Limber::TagLayoutTemplate::WalkCombinatorialSequential
-  include Limber::TagLayoutTemplate::TagWalker
+module TagLayoutTemplates::WalkCombinatorialSequential
+  include TagLayoutTemplates::TagWalker
 end
