22cb656bc3cde8eb568041793fc57ba2d52552fd
Stephen Hulme <sh51@sanger.ac.uk>
2025-07-28 15:33:09 +0100

    wip: delete limber models

--- app/models/limber/user.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class Limber::User < Sequencescape::User # rubocop:todo Style/Documentation
-  def name
-    @name ||= first_name || last_name ? "#{first_name} #{last_name}".strip : login
-  end
-end
