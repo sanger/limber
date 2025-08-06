06aa8766495eb493a82d61206c480884aa25df81
Stephen Hulme <sh51@sanger.ac.uk>
2025-08-06 16:49:35 +0100

    docs: add some docstrings before I forget...

--- app/models/presenters/request_info_presenter.rb
+++ app/models/presenters/request_info_presenter.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
+# Presents information about requests associated with a given labware item.
+#
+# This presenter provides methods to summarize and group active requests for a labware,
+# such as grouping by request type and state, and generating human-readable summaries.
 class Presenters::RequestInfoPresenter
   attr_reader :labware
 
@@ -15,6 +19,18 @@ class Presenters::RequestInfoPresenter
 
   delegate :active_requests, to: :@labware
 
+  # Groups active requests by the specified attributes and returns a hash with the count for each group.
+  #
+  # Each group key is an array of attribute values (e.g. [request_type.name, request.state]).
+  # If the attribute is not found on the request, it is looked up on the request's request_type.
+  #
+  # @param by [Array<Symbol>] The list of attributes to group by. Defaults to [:name, :state].
+  # @return [Hash<Array, Integer>] A hash where the key is an array of attribute values and
+  #    the value is the count of requests in that group.
+  #
+  # @example
+  #   grouped_active_requests
+  #   # => {["LCM Triomics WGS", "passed"] => 24, ["LCM Triomics EMSeq", "failed"] => 24}
   def grouped_active_requests(by: %i[name state])
     active_requests.group_by do |request|
       by.map do |attr|
