8aab7a5e0c20a6c7b5a1db53e0365c5d68cd0958
Wendy Yang <wy1@sanger.ac.uk>
2025-08-11 15:26:11 +0100

    refactor to meet linting

--- app/controllers/plates_controller.rb
+++ app/controllers/plates_controller.rb
@@ -8,51 +8,41 @@ class PlatesController < LabwareController
   before_action :check_for_current_user!, only: %i[update fail_wells] # rubocop:todo Rails/LexicallyScopedActionFilter
 
   def fail_wells # rubocop:todo Metrics/AbcSize
-    if selected_wells.empty?
-      redirect_to(limber_plate_path(params[:id]), notice: t('notices.no_wells_selected'))
-    else
-      begin
-        Sequencescape::Api::V2::StateChange.create!(
-          contents: selected_wells,
-          customer_accepts_responsibility: params[:customer_accepts_responsibility],
-          reason: 'Individual Well Failure',
-          target_state: 'failed',
-          target_uuid: params[:id],
-          user_uuid: current_user_uuid
-        )
-      rescue StandardError => e
-        log_plate_error(e)
-        redirect_to limber_plate_path(params[:id]), alert: t('errors.messages.fail_wells_failed')
-        return
-      end
+    
+    return redirect_to(limber_plate_path(params[:id]), notice: t('notices.no_wells_selected')) if selected_wells.empty?
+    
+    begin
+      Sequencescape::Api::V2::StateChange.create!(
+        contents: selected_wells,
+        customer_accepts_responsibility: params[:customer_accepts_responsibility],
+        reason: 'Individual Well Failure',
+        target_state: 'failed',
+        target_uuid: params[:id],
+        user_uuid: current_user_uuid
+      )
       redirect_to(limber_plate_path(params[:id]), notice: t('notices.wells_failed'))
-    end
+    rescue StandardError => e
+      log_plate_error(e)
+      redirect_to limber_plate_path(params[:id]), alert: t('errors.messages.fail_wells_failed')
+      nil
+    end   
   end
 
   def selected_wells
     params.fetch(:plate, {}).fetch(:wells, {}).select { |_, v| v == '1' }.keys
   end
 
-  def process_mark_under_represented_wells
-    if selected_wells.empty?
-      redirect_to(limber_plate_path(params[:id]), notice: t('notices.no_wells_selected'))
-    else
-      begin
-        plate = Sequencescape::Api::V2.plate_with_custom_includes(['wells.aliquots.request'], uuid: params[:id])
-        wells_by_location = plate.wells.index_by(&:location)
+  def process_mark_under_represented_wells # rubocop:todo Metrics/AbcSize
+    return redirect_to(limber_plate_path(params[:id]), notice: no_wells_notice) if selected_wells.empty?
 
-        # for each well, find the aliquot and then the request
-        # create a new poly metadatum for the request
-        selected_wells.each do |well_location|
-          well = wells_by_location[well_location]
-          create_poly_metadatum_for_request(well)
-        end
-      rescue StandardError => e
-        log_plate_error(e)
-        redirect_to limber_plate_path(params[:id]), alert: t('errors.messages.mark_wells_under_represented_failed')
-        return
-      end
+    begin
+      plate = fetch_plate_with_requests(params[:id])
+      mark_selected_wells_under_represented(plate)
       redirect_to(limber_plate_path(params[:id]), notice: t('notices.wells_marked_under_represented'))
+    rescue StandardError => e
+      log_plate_error(e)
+      redirect_to limber_plate_path(params[:id]), alert: t('errors.messages.mark_wells_under_represented_failed')
+      nil
     end
   end
 
@@ -61,6 +51,18 @@ class PlatesController < LabwareController
   def locate_labware_identified_by_id
     Sequencescape::Api::V2.plate_for_presenter(**search_param) ||
       raise(ActionController::RoutingError, "Unknown resource #{search_param}")
+  end
+
+  def fetch_plate_with_requests(plate_id)
+    Sequencescape::Api::V2.plate_with_custom_includes(['wells.aliquots.request'], uuid: plate_id)
+  end
+
+  def mark_selected_wells_under_represented(plate)
+    wells_by_location = plate.wells.index_by(&:location)
+    selected_wells.each do |location|
+      well = wells_by_location[location]
+      create_poly_metadatum_for_request(well)
+    end
   end
 
   def create_poly_metadatum_for_request(well)
