f50a9bb9fe407a99592d68cd9b5fbf34f52d4fa1
Dasun Pubudumal <pubudumald@gmail.com>
2025-08-19 16:18:00 +0100

    Refactor search_helper to remove unnecessary children method calls for accessing purposes

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -2,20 +2,20 @@
 
 module SearchHelper # rubocop:todo Style/Documentation
   def stock_plate_uuids
-    Settings.purposes.children.select { |_uuid, config| config.input_plate }.keys
+    Settings.purposes.select { |_uuid, config| config.input_plate }.keys
   end
 
   def self.stock_plate_names
-    Settings.purposes.children.values.select(&:input_plate).map(&:name)
+    Settings.purposes.values.select(&:input_plate).map(&:name)
   end
 
   # Returns purpose names of stock plates using stock_plate flag instead of input_plate.
   def self.stock_plate_names_with_flag
-    Settings.purposes.children.values.select(&:stock_plate).map(&:name)
+    Settings.purposes.values.select(&:stock_plate).map(&:name)
   end
 
   def self.purpose_config_for_purpose_name(purpose_name)
-    Settings.purposes.children.values.find { |obj| obj[:name] == purpose_name }
+    Settings.purposes.values.find { |obj| obj[:name] == purpose_name }
   end
 
   def self.alternative_workline_reference_name(labware)
@@ -26,13 +26,12 @@ module SearchHelper # rubocop:todo Style/Documentation
   end
 
   def self.merger_plate_names
-    Settings.purposes.children.values.select(&:merger_plate).map(&:name)
+    Settings.purposes.values.select(&:merger_plate).map(&:name)
   end
 
   def purpose_options(type)
     Settings
       .purposes
-      .children
       .select { |_uuid, settings| settings[:asset_type] == type }
       .map { |uuid, settings| [settings[:name], uuid] }
   end
