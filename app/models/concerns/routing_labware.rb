01735f53ecb8a6dad0ccd167254b4ab58dcc52db
Stephen Hulme <sh51@sanger.ac.uk>
2025-08-22 12:08:06 +0100

    docs: add more detail about routing mapping

--- app/models/concerns/routing_labware.rb
+++ app/models/concerns/routing_labware.rb
@@ -2,6 +2,9 @@
 
 # A concern to encapsulate Rails routing proxy behaviour for labware models that use a Sequencescape API.
 # See the route definitions in `config/routes.rb` for the `resources` to `controller` mapping.
+# For the actual mapping to Sequencescape API V2 models, see the model_name method
+# in app/integrations/sequencescape/api/v2/labware.rb.
+#
 module RoutingLabware
   extend ActiveSupport::Concern
 
@@ -15,7 +18,7 @@ module RoutingLabware
   #   plate = Plate.new(api, '123e4567-e89b-12d3-a456-426614174000')
   #   puts plate.to_param # Outputs: '123e4567-e89b-12d3-a456-426614174000'
   def initialize(api, id)
-    @api = api
+    @api = api # DEPRECATED: To be removed when API v1 is fully removed
     @id = id
   end
 
@@ -27,6 +30,9 @@ module RoutingLabware
     @id
   end
 
+  # For routing and form helpers, we need to behave like an ActiveModel instance.
+  # This method returns self to satisfy this interface.
+  # @return [self] The current instance
   def to_model
     self
   end
