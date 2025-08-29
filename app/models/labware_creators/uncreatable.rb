d8f522c39c0c2fafb01df89e8f46fed7c5d8bfd0
Stephen Hulme <sh51@sanger.ac.uk>
2025-08-29 12:12:00 +0100

    refactor: unify uncreatable labware errors
    Extends the existing creator pattern to apply to uncreatable labwares.
    Doesn't create anything, but provides a useful error message.

--- app/models/labware_creators/uncreatable.rb
+++ app/models/labware_creators/uncreatable.rb
@@ -6,13 +6,48 @@ module LabwareCreators
   # will fail to function correctly if created through Limber.
   # In practice this class behaves just like the base creator, but has been
   # sub-classed to better communicate its intent.
+  # Doesn't create anything, but provide a useful error message.
   class Uncreatable < Base
     # This creator is invalid for all parents.
+    self.page = 'uncreatable'
+
     # @note This actually duplicates the behaviour on the base class, so is not
     #       required for correct functionality. However I've decided to include
     #       it here for reasons of clarity.
     def self.creatable_from?(_parent)
       false
+    end
+
+    # Gather information for the view for user and developer debugging purposes
+
+    # Returns the labware type of the parent labware purpose
+    def parent_labware_type
+      Settings.purposes.fetch(parent_purpose.uuid)&.fetch(:asset_type, 'unknown labware type')
+    end
+
+    # Returns the name of the parent purpose, or 'Unknown Purpose' if unavailable.
+    def parent_purpose_name
+      parent_purpose&.name || 'Unknown Purpose'
+    end
+
+    # Returns the name of the child purpose
+    def child_purpose_name
+      purpose_name
+    end
+
+    # Returns the labware type of the child purpose
+    def child_labware_type
+      purpose_config.asset_type
+    end
+
+    private
+
+    def parent
+      @parent ||= Sequencescape::Api::V2::Labware.find(uuid: parent_uuid).first
+    end
+
+    def parent_purpose
+      @parent_purpose ||= parent.purpose
     end
   end
 end
