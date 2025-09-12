aff6b364bd22af5cedaeadab2f20f13d18a90655
Stephen Hulme <sh51@sanger.ac.uk>
2025-09-12 17:17:11 +0100

    fix: update LabwareMetadata to raise error on bad barcode

--- app/models/labware_metadata.rb
+++ app/models/labware_metadata.rb
@@ -4,15 +4,18 @@ class LabwareMetadata # rubocop:todo Style/Documentation
   attr_accessor :user_uuid, :labware, :barcode
 
   def initialize(params = {})
+    # Initialize with either a barcode or a labware object
+    # If both are given, the labware is used
+    # If neither is given, an ArgumentError is raised
+    # If a barcode is given, but no labware is found, a JsonApiClient::Errors::NotFound is raised
     @user_uuid = params.fetch(:user_uuid, nil)
-    @user = Sequencescape::Api::V2::User.find(uuid: @user_uuid).first unless @user_uuid.nil?
+    @labware = params.fetch(:labware, nil)
     @barcode = params.fetch(:barcode, nil)
-    if barcode.present?
-      @labware = Sequencescape::Api::V2::Labware.find(barcode:).first
-    else
-      @labware = params.fetch(:labware, nil)
-      raise ArgumentError, 'Parameters labware or barcode missing' if labware.nil?
-    end
+
+    raise ArgumentError, 'Parameters labware or barcode missing' if @barcode.nil? && @labware.nil?
+
+    @user = Sequencescape::Api::V2::User.find(uuid: @user_uuid).first unless @user_uuid.nil?
+    @labware ||= Sequencescape::Api::V2::Labware.find!(barcode:).first
   end
 
   def update!(metadata)
