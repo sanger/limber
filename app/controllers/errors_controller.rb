c77b669fd2deaad900165831f3afe157b3972cb8
Stephen Hulme <sh51@sanger.ac.uk>
2025-09-25 11:01:24 +0100

    wip: single error page

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -12,27 +12,25 @@
 # All actions provide responses in HTML format, even if the request ends in a
 # different format like .json or .png.
 class ErrorsController < ApplicationController
-  skip_before_action :login_required
-
   layout false
 
   # 404 Not Found
   def not_found
-    render status: :not_found, formats: [:html]
+    render template: 'errors/error', status: :not_found, formats: [:html]
   end
 
   # 406 Not Acceptable
   def not_acceptable
-    render status: :not_acceptable, formats: [:html]
+    render template: 'errors/error', status: :not_acceptable, formats: [:html]
   end
 
   # 500 Internal Server Error
   def internal_server_error
-    render status: :internal_server_error, formats: [:html]
+    render template: 'errors/error', status: :internal_server_error, formats: [:html]
   end
 
   # 503 Service Unavailable
   def service_unavailable
-    render status: :service_unavailable, formats: [:html]
+    render template: 'errors/error', status: :service_unavailable, formats: [:html]
   end
 end
