9073e2947f6590ac2d8e273b19d241ec4d20de83
Andrew Sparkes <as28@sanger.ac.uk>
2025-11-20 15:23:58 +0000

    changes after feedback

--- app/models/concerns/presenters/filter_mx_children_creation_behaviour.rb
+++ app/models/concerns/presenters/filter_mx_children_creation_behaviour.rb
@@ -1,9 +1,15 @@
 # frozen_string_literal: true
 
 # Include in a presenter to add support for filtering out multiplexed child creation options
+# rubocop:disable Metrics/ModuleLength
 module Presenters::FilterMxChildrenCreationBehaviour
-  DESCENDANT_TUBE_INCLUDES =
-    'receptacle,aliquots,aliquots.request,aliquots.request.request_type,receptacle.requests_as_source.request_type'
+  DESCENDANT_TUBE_INCLUDES = [
+    'receptacle',
+    'aliquots',
+    'aliquots.request',
+    'aliquots.request.request_type',
+    'receptacle.requests_as_source.request_type'
+  ].join(',')
 
   # Prevent the multiplexing child labware button from appearing if a valid child labware already exists
   def allow_specific_child_creation?
@@ -19,13 +25,13 @@ module Presenters::FilterMxChildrenCreationBehaviour
   end
 
   def suggested_purpose_options
-    spo = identify_suggested_purpose_options
+    suggested_options = identify_suggested_purpose_options
 
     if allow_specific_child_creation?
-      spo
+      suggested_options
     else
       # filter out the purpose(s) specified in the purpose config if we cannot allow the child creation yet
-      display_warning_and_filter_child_options(spo)
+      display_warning_and_filter_child_options(suggested_options)
     end
   end
 
@@ -65,20 +71,20 @@ module Presenters::FilterMxChildrenCreationBehaviour
   # Check that the tube has a request of the specified type and state
   # Returns true if all checks pass, false otherwise
   def tube_request_matches_requirements?(labware_descendant)
-    child_v2_tube = fetch_child_v2_tube(labware_descendant)
-    return false unless child_v2_tube
+    child_tube = fetch_child_tube(labware_descendant)
+    return false unless child_tube
 
-    child_v2_tube_reqs = fetch_child_v2_tube_requests(child_v2_tube)
-    return false if child_v2_tube_reqs.empty?
+    child_tube_reqs = fetch_child_tube_requests(child_tube)
+    return false if child_tube_reqs.empty?
 
     # look for a request matching the type and allowed state
-    matching_request?(child_v2_tube_reqs)
+    matching_request?(child_tube_reqs)
   end
 
-  def matching_request?(child_v2_tube_reqs)
+  def matching_request?(child_tube_reqs)
     acceptable_request_found = false
-    child_v2_tube_reqs.each do |child_v2_tube_req|
-      tube_req_type = fetch_tube_req_type(child_v2_tube_req)
+    child_tube_reqs.each do |child_tube_req|
+      tube_req_type = fetch_tube_req_type(child_tube_req)
 
       next unless tube_req_type.present? && req_type_for_multiplexing?(tube_req_type)
 
@@ -93,17 +99,17 @@ module Presenters::FilterMxChildrenCreationBehaviour
     child_tube_purposes_to_limit.include?(labware_descendant.purpose.name)
   end
 
-  def fetch_child_v2_tube(labware_descendant)
+  def fetch_child_tube(labware_descendant)
     Sequencescape::Api::V2::Tube.find_by(uuid: labware_descendant.uuid, includes: DESCENDANT_TUBE_INCLUDES)
   end
 
-  def fetch_child_v2_tube_requests(child_v2_tube)
+  def fetch_child_tube_requests(child_tube)
     # assumption that all aliquots will have same request
-    Array(child_v2_tube.aliquots.first.request) || []
+    Array(child_tube.aliquots.first.request) || []
   end
 
-  def fetch_tube_req_type(child_v2_tube_req)
-    child_v2_tube_req.request_type
+  def fetch_tube_req_type(child_tube_req)
+    child_tube_req.request_type
   end
 
   def req_type_for_multiplexing?(tube_req_type)
@@ -111,10 +117,10 @@ module Presenters::FilterMxChildrenCreationBehaviour
   end
 
   # rubocop:disable Metrics/MethodLength
-  def display_warning_and_filter_child_options(spo)
+  def display_warning_and_filter_child_options(suggested_options)
     # NB. uses lazy.reject to return an enumerator, not an array, because that breaks creation behaviour
     # in the construct buttons method where there is a force on the scope
-    spo.lazy.reject do |(_uuid, settings)|
+    suggested_options.lazy.reject do |(_uuid, settings)|
       if child_tube_purposes_to_limit.include?(settings[:name])
         suggested_options_warnings.add(
           :tube,
@@ -146,3 +152,5 @@ module Presenters::FilterMxChildrenCreationBehaviour
       .uniq
   end
 end
+
+# rubocop:enable Metrics/ModuleLength
