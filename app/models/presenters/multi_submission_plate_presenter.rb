095bf26182884d283c2d2e0cf88d3970f5bd1b18
Wendy Yang <wy1@sanger.ac.uk>
2026-02-10 21:50:38 +0000

    check in changes

---
+++ app/models/presenters/multi_submission_plate_presenter.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+module Presenters
+  #
+  # Class PermissivePresenter provides a presenter which allows plate creation
+  # even when the plate is pending
+  #
+  class MultiSubmissionPlatePresenter < PlatePresenter
+    # include Presenters::Statemachine::Permissive
+
+    # validates_with Validators::SuboptimalValidator
+    # validates_with Validators::ActiveRequestValidator
+  end
+end
