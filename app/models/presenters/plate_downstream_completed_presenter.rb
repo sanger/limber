effdf486ef59c5edac7135fd0637dd50a02b6c9b
Katy Taylor <kt17@sanger.ac.uk>
2026-02-10 13:40:22 +0000

    fix query syntax, display message in UI, and refactor/reorder methods

--- app/models/presenters/plate_downstream_completed_presenter.rb
+++ app/models/presenters/plate_downstream_completed_presenter.rb
@@ -25,37 +25,42 @@ module Presenters
     DESCENDANT_TUBE_INCLUDES =
       'receptacle,aliquots,aliquots.request,aliquots.request.request_type,receptacle.requests_as_source.request_type'
 
-    # Messages to display in banner on page load, to inform the user of the status of downstream runs and creation of child labware.
-    # STATUS_CONFIG = {
-    #   unknown: {
-    #     strength: 'error',
-    #     message: "Unable to determine the status of the downstream sequencing run, so creating child labware of the following types is disabled: #{child_tube_purposes}. \
-    #       Please refresh the page or contact PSD."
-    #   },
-    #   not_finished: {
-    #     strength: 'warning',
-    #     message: "The initial sequencing run is not yet complete, so creating child labware of the following types is disabled: #{child_tube_purposes}."
-    #   },
-    #   finished: {
-    #     strength: 'info',
-    #     message: "The initial sequencing run has completed, so creating child labware of the following types is enabled: #{child_tube_purposes}."
-    #   }
-    # }
+    # Not strictly validation, used to display messages relating to the status of downstream runs.
+    # Other presenters use a similar pattern.
+    validate :ultima_run_status
 
-    # Prevents the display of specific child creation buttons unless the first sequencing path has finished
-    def allow_specific_child_creation?
-      # TODO: Display status in UI.
+    private
 
-      @ultima_run_status = find_ultima_run_status
-      return true if @ultima_run_status == :finished
+    # --- parameters from purpose config ---
+    def downstream_seq_tube_purpose
+      @downstream_seq_tube_purpose ||=
+        purpose_config.dig(:presenter_class, :args, :downstream_seq_tube, :purpose)
+    end
 
-      false
+    def child_tube_purposes
+      @child_tube_purposes ||=
+        Array(purpose_config.dig(:presenter_class, :args, :child_tube_purposes))
+    end
+    # --- end parameters from purpose config ---
+
+    def ultima_run_status
+      @ultima_run_status ||= find_ultima_run_status
+
+      case @ultima_run_status
+      when :unknown
+        errors.add(:base, "Unable to determine the status of the downstream sequencing run, so creating child labware of the following types is disabled: #{child_tube_purposes.join(', ')}. Please refresh the page or contact PSD.")
+      when :not_finished
+        errors.add(:base, "The initial sequencing run is not yet complete, so creating child labware of the following types is disabled: #{child_tube_purposes.join(', ')}.")
+      when :finished
+        # No error message if finished, as child creation will be allowed and there will be no issue for the user.
+      end
     end
 
     def find_ultima_run_status
       # TODO: replace 'descendants' call with one like 'descendants_with_requests_as_source'
       # to avoid requerying tubes individually in fetch_tube.
-      downstream_sequenced_tubes = @labware.descendants.all.select{ |d| d.purpose.name == downstream_seq_tube_purpose }
+      # binding.pry
+      downstream_sequenced_tubes = @labware.descendants.all.select{ |d| d.purpose&.name == downstream_seq_tube_purpose }
       return :not_finished if downstream_sequenced_tubes.empty?
 
       return :finished if Limber::Application.config.mock_ultima_run_check
@@ -77,6 +82,13 @@ module Presenters
       end
     end
 
+    def fetch_tube(labware_descendant)
+      Sequencescape::Api::V2::Tube.find_all(
+        { uuid: labware_descendant.uuid },
+        includes: DESCENDANT_TUBE_INCLUDES
+      ).first
+    end
+
     # Use sql2 gem as don't need any Rails ORM functionality
     # for this single query to an external database.
     def mlwh_contains_run_record(wafer_ids)
@@ -87,32 +99,11 @@ module Presenters
         database: Rails.application.config.mlwh_db
       )
 
-      results = client.query("SELECT * FROM useq_wafer WHERE id_wafer_lims IN (#{wafer_ids.join(',')})")
-
+      query_string = "SELECT * FROM useq_wafer WHERE id_wafer_lims IN ('#{wafer_ids.join('\',\'')}')"
+      results = client.query(query_string)
       client.close
 
       results.count > 0
-    end
-
-    private
-
-    # --- parameters from purpose config ---
-    def downstream_seq_tube_purpose
-      @downstream_seq_tube_purpose ||=
-        purpose_config.dig(:presenter_class, :args, :downstream_seq_tube, :purpose)
-    end
-
-    def child_tube_purposes
-      @child_tube_purposes ||=
-        Array(purpose_config.dig(:presenter_class, :args, :child_tube_purposes))
-    end
-    # --- end parameters from purpose config ---
-
-    def fetch_tube(labware_descendant)
-      Sequencescape::Api::V2::Tube.find_all(
-        { uuid: labware_descendant.uuid },
-        includes: DESCENDANT_TUBE_INCLUDES
-      ).first
     end
 
     # Used to decide what suggested child labwares can be created from this labware.
@@ -152,6 +143,11 @@ module Presenters
       spo.reject do |(_uuid, settings)|
         child_tube_purposes.include?(settings[:name])
       end
+    end
+
+    # Prevents the display of specific child creation buttons unless the first sequencing path has finished
+    def allow_specific_child_creation?
+      ultima_run_status == :finished
     end
   end
 end
