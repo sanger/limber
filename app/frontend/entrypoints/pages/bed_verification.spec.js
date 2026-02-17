54fc0858501c2cc1bf3dd153f39c093f656bc14f
Andrew Sparkes <as28@sanger.ac.uk>
2026-02-17 13:59:16 +0000

    removed unneeded console logs

--- app/frontend/entrypoints/pages/bed_verification.spec.js
+++ app/frontend/entrypoints/pages/bed_verification.spec.js
@@ -4,7 +4,10 @@ import SCAPE from '@/javascript/lib/global_message_system.js'
 describe('bed_verification plate scan handler', () => {
   let plateScan, bedScan, robotScan
 
-  beforeEach(async () => {
+  // NB. use of beforeAll is not ideal but could not get beforeEach to work.
+  // Deemed acceptable in this case as the two tests could mimic a sequence that could happen in
+  // real life.
+  beforeAll(async () => {
     // Set up DOM elements
     document.body.innerHTML = `
 			<input id="plate_scan" />
@@ -14,28 +17,29 @@ describe('bed_verification plate scan handler', () => {
     plateScan = $('#plate_scan')
     bedScan = $('#bed_scan')
     robotScan = $('#robot_scan')
+
     // Mock SCAPE.message
     SCAPE.message = vi.fn()
+
     // Import and attach the handler
     await import('./bed_verification.js')
   })
 
   it('shows warning and does not scan plate if bed_scan is empty', () => {
-    console.log('DEBUG: starting bed scan empty test')
     bedScan.val('')
     plateScan.val('PLATE123')
-    console.log('DEBUG: triggering plate scan change')
     plateScan.trigger('change')
+
+    // We expect a message on screen requesting user scan the bed first
     expect(SCAPE.message).toHaveBeenCalledWith('Scan the bed before the plate please!', 'warning')
   })
 
   it('clears warning and scans plate if bed_scan has value', () => {
-    console.log('DEBUG: starting bed scan has value test')
     robotScan.val('ROBOT123')
     bedScan.val('BED123')
     plateScan.val('PLATE123')
-    console.log('DEBUG: triggering plate scan change')
     plateScan.trigger('change')
+
     // We clear the message field if we have a bed barcode, so we expect an empty message to be sent
     expect(SCAPE.message).toHaveBeenCalledWith('', '')
   })
