e536e4385a40725fb59b3cba4476d38648c5828f
Wendy Yang <wy1@sanger.ac.uk>
2026-02-17 10:32:35 +0000

    save changes

---
+++ app/models/presenters/stock_plate_with_submission_presenter.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+module Presenters
+
+    class StockPlateWithSubmissionPresenter < PlatePresenter
+    include Presenters::Statemachine::Submission
+    include Presenters::SubmissionBehaviour
+    include Presenters::StockBehaviour
+    include Presenters::StateChangeless
+
+    self.allow_well_failure_in_states = []
+    self.style_class = 'stock'
+
+    validates_with Validators::SuboptimalValidator
+    validates_with Validators::ActiveRequestValidator
+    end
+end
