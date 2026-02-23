2ed3a513d6cc422b25ccc99a7f1064ed63ca25ef
Wendy Yang <wy1@sanger.ac.uk>
2026-02-23 21:29:27 +0000

    add submissionWhenPassed statemachine to presenter

--- app/models/presenters/stock_plate_with_submission_presenter.rb
+++ app/models/presenters/stock_plate_with_submission_presenter.rb
@@ -6,13 +6,19 @@ module Presenters
         include Presenters::Statemachine::Submission
         include Presenters::SubmissionBehaviour
         include Presenters::StockBehaviour
-        # include Presenters::StateChangeless
+        include Presenters::StateChangeless
         include Presenters::Statemachine::DoesNotAllowLibraryPassing
+        include Presenters::Statemachine::SubmissionWhenPassed
 
         self.allow_well_failure_in_states = []
         self.style_class = 'stock'
 
         validates_with Validators::SuboptimalValidator
         validates_with Validators::ActiveRequestValidator
+
+        def allow_new_submission?
+            Rails.logger.warn "===============ALLOW_NEW_SUBMISSION CALLED"
+            true
+        end
     end
 end
