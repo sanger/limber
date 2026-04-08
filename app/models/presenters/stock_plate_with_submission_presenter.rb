034b0e6a1bcdf42dc704c146d0c4e469c32d8b00
Wendy Yang <wy1@sanger.ac.uk>
2026-04-08 11:58:11 +0100

    add condition for showing project input field

--- app/models/presenters/stock_plate_with_submission_presenter.rb
+++ app/models/presenters/stock_plate_with_submission_presenter.rb
@@ -19,6 +19,10 @@ module Presenters
       true
     end
 
+    def require_project?
+      true
+    end
+
     def active_request_types
       wells.flat_map(&:active_requests)
         .map(&:request_type_key)
