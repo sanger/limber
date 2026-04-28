82c65b394fbb24bdb37343cbef42ac964b84da6d
Stephen Hulme <sh51@sanger.ac.uk>
2026-04-28 17:15:20 +0100

    build: upgrade @vitest/coverage-v8 to 4

--- vite.config.mjs
+++ vite.config.mjs
@@ -32,6 +32,11 @@ export default defineConfig({
     coverage: {
       provider: 'v8',
       reporter: ['lcov', 'text'],
+      // Include covered and uncovered files matching this pattern:
+      include: ['app/frontend/**/**.{js,jsx,ts,tsx}'],
+      // Exclusion is applied for the files that match include pattern above
+      // No need to define root level *.config.ts files or node_modules, as we didn't add those in include
+      // exclude: ['**/some-pattern/**'],
     },
     // This hides the "Download the Vue Devtools extension" message from the console
     onConsoleLog(log) {
