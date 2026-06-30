44ba4210a3b91c9195516d413465fdf66184695e
Katy Taylor <kt17@sanger.ac.uk>
2026-06-30 14:50:14 +0100

    Change test expectation. New version of the code retrieves these barcodes in a different way - through a separate V2 API call, rather than using a redirect to labware#show. The old way rendered barcode.prefix + barcode.number, leaving out the suffix for some reason (see app/views/tubes/show.json.erb, app/views/plates/show.json.erb). The new code just calls .human_barcode, which I think is fine.

--- spec/features/pooling_multiple_tubes_into_one_tube_spec.rb
+++ spec/features/pooling_multiple_tubes_into_one_tube_spec.rb
@@ -139,7 +139,7 @@ RSpec.feature 'Pooling multiple tubes into a tube', :js do
 
       expect(page).to have_text(
         'The scanned tube contains tags that would clash with those in other tubes in the pool. ' \
-        'Tag clashes found between: NT1 (3980000001795) and NT2 (3980000002808)'
+        'Tag clashes found between: NT1O (3980000001795) and NT2P (3980000002808)'
       )
 
       # removes the error message if another scan is made (NB. currently validation and messages relate to
