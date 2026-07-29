a40b660bae4d0bb5a7b13a28d0f3073669a16680
Ben Topping <ben.topping@outlook.com>
2026-07-29 12:04:10 +0100

    feat(submission_behaviour): tweak allow_new_submission to prevent appearing if valid active requests are present

--- app/models/concerns/presenters/submission_behaviour.rb
+++ app/models/concerns/presenters/submission_behaviour.rb
@@ -21,7 +21,9 @@ module Presenters::SubmissionBehaviour
 
   # Determine whether the Choose Workflow buttons should be displayed
   def allow_new_submission?
-    !(pending_submissions? || active_submissions? || labware.active_requests.any?)
+    !(pending_submissions? || active_submissions? || labware.active_requests.any? do |req|
+      %w[pending started passed].include? req.state
+    end)
   end
 
   private
