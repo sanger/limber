0204cc66c2cb7cced18075da16b51a7baa9bf92e
yoldas <ay6@sanger.ac.uk>
2026-09-13 18:35:07 +0100

    chore: allow overriding the Selenium Chrome binary via CHROME_BINARY_PATH to match CI locally

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -68,6 +68,7 @@ Capybara.register_driver :headless_chrome do |app|
   options.add_argument('--window-size=1600,3200')
   options.add_argument('--no-sandbox')
   options.add_preference('profile.password_manager_leak_detection', false)
+  options.binary = ENV['CHROME_BINARY_PATH'] if ENV['CHROME_BINARY_PATH']
 
   Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
 end
